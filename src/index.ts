9802ec7d5851db894fe9fe9eae2fe4984ba7b46d
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-09-13 14:34:24 +0100

    Progress dump for full SDK flow
    This looks like it's working now, which is great. It needs a lot of
    tidying, splitting apart, commenting, testing, and error checking, but
    the base functionality is there.
    
    There's an odd change to `package.json` here. Because we're grabbing the
    `package.json` file, it's shifting all of the files down a directory to
    be relative to that import.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 import { URL } from "url";
+import { Request, Response } from "express";
+import { version } from "../package.json";
+import { z } from "zod";
 
 export declare namespace Inngest {
   /**
@@ -95,9 +98,9 @@ export declare namespace Inngest {
   export interface ClientOptions {
     /**
      * The base Inngest Source API URL to append the Source API Key to.
-     * Defaults to https://inn.gs/e/
+     * Defaults to https://inn.gs/
      */
-    inngestApiUrl?: string;
+    inngestBaseUrl?: string;
   }
 
   /**
@@ -122,19 +125,21 @@ export class Inngest<Events extends Record<string, any>> {
   /**
    * Inngest Source API Key
    */
-  readonly #apiKey: string;
+  private readonly apiKey: string;
 
   /**
    * Full URL for the Inngest Source API
    */
-  private readonly inngestApiUrl: string;
+  private readonly inngestBaseUrl: URL;
+  private readonly inngestApiUrl: URL;
+  private readonly inngestRegisterUrl: URL;
 
   /**
    * Axios configuration for sending events to Inngest
    */
-  readonly #axiosConfig: AxiosRequestConfig;
+  private readonly client: AxiosInstance;
 
-  #fns: InngestFunction<Events>[] = [];
+  private readonly fns: Record<string, InngestFunction<Events>> = {};
 
   /**
    * @param apiKey - An API Key for the Inngest Source API
@@ -142,13 +147,15 @@ export class Inngest<Events extends Record<string, any>> {
   constructor(
     name: string,
     apiKey: string,
-    { inngestApiUrl = "https://inn.gs/e/" }: Inngest.ClientOptions = {}
+    { inngestBaseUrl = "https://inn.gs/" }: Inngest.ClientOptions = {}
   ) {
     this.name = name;
-    this.#apiKey = apiKey;
-    this.inngestApiUrl = new URL(this.#apiKey, inngestApiUrl).toString();
+    this.apiKey = apiKey;
+    this.inngestBaseUrl = new URL(inngestBaseUrl);
+    this.inngestApiUrl = new URL(`e/${this.apiKey}`, this.inngestBaseUrl);
+    this.inngestRegisterUrl = new URL("x/register", this.inngestBaseUrl);
 
-    this.#axiosConfig = {
+    this.client = axios.create({
       timeout: 0,
       headers: {
         "Content-Type": "application/json",
@@ -156,10 +163,10 @@ export class Inngest<Events extends Record<string, any>> {
       },
       validateStatus: () => true, // all status codes return a response
       maxRedirects: 0,
-    };
+    });
   }
 
-  private getResponseError(response: AxiosResponse): Error {
+  #getResponseError(response: AxiosResponse): Error {
     let errorMessage = "Unknown error";
     switch (response.status) {
       case 401:
@@ -200,20 +207,16 @@ export class Inngest<Events extends Record<string, any>> {
       | Omit<Inngest.EventPayload<Events[Event]>, "name">
       | Omit<Inngest.EventPayload<Events[Event]>, "name">[]
   ): Promise<boolean> {
-    const response = await axios.post(
-      this.inngestApiUrl,
-      {
-        ...payload,
-        name,
-      },
-      this.#axiosConfig
-    );
+    const response = await this.client.post(this.inngestApiUrl.href, {
+      ...payload,
+      name,
+    });
 
     if (response.status >= 200 && response.status < 300) {
       return true;
     }
 
-    throw this.getResponseError(response);
+    throw this.#getResponseError(response);
   }
 
   /**
@@ -240,26 +243,59 @@ export class Inngest<Events extends Record<string, any>> {
     fn: Fn
   ): InngestFunction<Events> {
     return this.#addFunction(
-      new InngestFunction(
-        this,
-        typeof opts === "string" ? { name: opts } : opts,
-        event,
-        {
-          step: new InngestStep(this, fn),
-        }
-      )
+      typeof opts === "string" ? { name: opts } : opts,
+      event,
+      {
+        step: new InngestStep(this, fn),
+      }
     );
   }
 
-  #addFunction(fn: InngestFunction<Events>): typeof fn {
-    this.#fns.push(fn);
+  #addFunction(
+    opts: Inngest.FunctionOptions,
+    trigger: keyof Events,
+    steps: Inngest.Steps<Events>
+  ): InngestFunction<Events> {
+    if (Object.prototype.hasOwnProperty.call(this.fns, opts.name)) {
+      throw new Error(
+        `Cannot create two functions with the same name: "${opts.name}`
+      );
+    }
+
+    const fn = new InngestFunction(this, opts, trigger, steps);
+    this.fns[opts.name] = fn;
+
     return fn;
   }
 
   /**
-   * Register any functions under this Inngest instance.
+   * Finds a function by `functionId` and runs the relevant step with the given
+   * `stepId`.
    */
-  public register() {}
+  private async runStep(
+    functionId: string,
+    stepId: string,
+    data: any
+  ): Promise<StepRunResponse> {
+    try {
+      const fn = this.fns[functionId];
+      if (!fn) {
+        throw new Error(`Could not find function with ID "${functionId}"`);
+      }
+
+      const body = await fn["runStep"](stepId, data);
+
+      return {
+        status: 200,
+        body: JSON.stringify(body),
+      };
+    } catch (err: any) {
+      return {
+        status: 500,
+        error: err.stack || err.message,
+      };
+    }
+  }
 }
 
 class InngestFunction<Events extends Record<string, any>> {
@@ -280,7 +316,54 @@ class InngestFunction<Events extends Record<string, any>> {
     this.#steps = steps;
   }
 
-  private getConfig() {}
+  public get name() {
+    return this.#opts.name;
+  }
+
+  /**
+   * Retrieve the Inngest config for this function.
+   */
+  private getConfig(
+    /**
+     * Must be provided a URL that will be used to trigger the step. This
+     * function can't be expected to know how it will be accessed, so relies on
+     * an outside method providing context.
+     */
+    url: URL
+  ): FunctionConfig {
+    return {
+      id: this.#opts.name,
+      name: this.#opts.name,
+      triggers: [{ event: this.#trigger as string }],
+      steps: Object.keys(this.#steps).reduce<FunctionConfig["steps"]>(
+        (acc, stepId) => {
+          return {
+            ...acc,
+            [stepId]: {
+              id: stepId,
+              name: stepId,
+              runtime: {
+                type: "remote",
+                url: url.href,
+              },
+            },
+          };
+        },
+        {}
+      ),
+    };
+  }
+
+  private runStep(stepId: string, data: any): Promise<unknown> {
+    const step = this.#steps[stepId];
+    if (!step) {
+      throw new Error(
+        `Could not find step with ID "${stepId}" in function "${this.name}"`
+      );
+    }
+
+    return step["run"](data);
+  }
 }
 
 class InngestStep<
@@ -289,8 +372,232 @@ class InngestStep<
   Output
 > {
   readonly #inngest: Inngest<Events>;
+  readonly #fn: (...args: any) => Output;
 
   constructor(inngest: Inngest<Events>, fn: (...args: Input) => Output) {
     this.#inngest = inngest;
+    this.#fn = fn;
+  }
+
+  private async run(data: any): Promise<unknown> {
+    return this.#fn(data);
   }
 }
+
+export interface FunctionConfig {
+  name: string;
+  id: string;
+  triggers: (
+    | {
+        event: string;
+        expression?: string;
+      }
+    | {
+        cron: string;
+      }
+  )[];
+  steps: Record<
+    string,
+    {
+      id: string;
+      name: string;
+      runtime: {
+        type: "remote";
+        url: string;
+      };
+    }
+  >;
+}
+
+export const register = (
+  inngestOrHandler: Inngest<any> | InngestCommHandler
+) => {
+  const handler =
+    inngestOrHandler instanceof Inngest
+      ? new InngestCommHandler(inngestOrHandler)
+      : inngestOrHandler;
+
+  return handler.createHandler();
+};
+
+export class InngestCommHandler {
+  protected readonly frameworkName: string = "default";
+  protected readonly inngest: Inngest<any>;
+
+  constructor(inngest: Inngest<any>) {
+    this.inngest = inngest;
+  }
+
+  public createHandler(): any {
+    return async (req: Request, res: Response) => {
+      console.log("Something hit the default handler!");
+
+      const reqUrl = new URL(req.originalUrl, req.hostname);
+
+      switch (req.method) {
+        case "PUT":
+          console.log("It was a PUT request");
+          // Push config to Inngest.
+          await this.register(reqUrl);
+          return void res.sendStatus(200);
+
+        case "GET":
+          console.log("It was a GET request");
+          // Inngest is asking for config; confirm signed and send.
+          this.validateSignature(); //TODO
+          const pingRes = this.pong(reqUrl);
+          this.signResponse(); // TODO
+          return void res.json(pingRes);
+
+        case "POST":
+          console.log("It was a POST request");
+          // Inngest is trying to run a step; confirm signed and run.
+          const { fnId, stepId } = z
+            .object({
+              fnId: z.string().min(1),
+              stepId: z.string().min(1),
+            })
+            .parse({
+              fnId: req.query.fnId,
+              stepId: req.query.stepId,
+            });
+
+          const stepRes = await this.runStep(fnId, stepId, req.body);
+
+          return void res.json(stepRes);
+
+        default:
+          return void res.sendStatus(405);
+      }
+    };
+  }
+
+  protected runStep(
+    functionId: string,
+    stepId: string,
+    data: any
+  ): Promise<StepRunResponse> {
+    console.log(
+      "Trying to run step",
+      stepId,
+      "in function",
+      functionId,
+      "with data",
+      data
+    );
+
+    return this.inngest["runStep"](functionId, stepId, data);
+  }
+
+  protected configs(url: URL): FunctionConfig[] {
+    return Object.values(this.inngest["fns"]).map((fn) => fn["getConfig"](url));
+  }
+
+  protected async register(url: URL): Promise<void> {
+    const body = {
+      url: url.href,
+      hash: "TODO",
+    };
+
+    const config: AxiosRequestConfig = {
+      headers: {
+        Authorization: `Bearer ${this.inngest["apiKey"]}`,
+      },
+    };
+
+    const res = await this.inngest["client"].post(
+      this.inngest["inngestRegisterUrl"].href,
+      body,
+      config
+    );
+
+    console.log(
+      "hit the register URL",
+      this.inngest["inngestRegisterUrl"].href,
+      "with:",
+      body,
+      "and",
+      config,
+      "and got back:",
+      res.status,
+      res.data
+    );
+  }
+
+  protected pong(url: URL): RegisterPingResponse {
+    return {
+      ctx: {
+        deployType: "ping",
+        framework: this.frameworkName,
+        name: this.inngest.name,
+      },
+      functions: this.configs(url),
+      sdk: version,
+      v: "0.1",
+    };
+  }
+
+  protected validateSignature(): boolean {
+    return true;
+  }
+
+  protected signResponse(): string {
+    return "";
+  }
+}
+
+class NextCommHandler extends InngestCommHandler {
+  public override frameworkName = "nextjs";
+
+  public override createHandler() {
+    // this.
+  }
+}
+
+interface RegisterPingResponse {
+  /**
+   * Response version, allowing Inngest to change any top-level field.
+   */
+  v: string;
+
+  /**
+   * SDK version from `package.json` for our internal metrics and to warn users
+   * they need to upgrade.
+   */
+  sdk: string;
+
+  ctx: {
+    /**
+     * The name of this particular app, used for grouping and easier viewing in
+     * the UI.
+     */
+    name: string;
+
+    /**
+     * The name of the framework being used for this instance, e.g. "nextjs",
+     * "vercel", "netlify", "lambda", etc. Uses the `framework` specified when
+     * creating a new `InngestCommHandler`.
+     */
+    framework: string;
+
+    /**
+     * The method used to deploy these functions.
+     */
+    deployType: "ping";
+  };
+
+  /**
+   * The functions available at this particular handler.
+   */
+  functions: FunctionConfig[];
+}
+
+type StepRunResponse =
+  | {
+      status: 500;
+      error?: string;
+    }
+  | {
+      status: 200;
+      body?: string;
+    };
