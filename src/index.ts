bb415598cc7135a441e9a118ecff1a1c1ecea9b8
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-09-13 11:16:58 +0100

    Progress for creating and maintaining `InngestFunction` instances

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,116 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { URL } from "url";
 
-/**
- * An HTTP-like, standardised response format that allows Inngest to help
- * orchestrate steps and retries.
- */
-export interface InngestResponse {
+export declare namespace Inngest {
   /**
-   * A step response must contain an HTTP status code.
-   *
-   * A `2xx` response indicates success; this is not a failure and no retry is
-   * necessary.
-   *
-   * A `4xx` response indicates a bad request; this step will not be retried as
-   * it is deemed irrecoverable. Examples of this might be an event with
-   * insufficient data or concerning a user that no longer exists.
-   *
-   * A `5xx` status indicates a temporary internal error; this will be retried
-   * according to the step and function's retry policy (3 times, by default).
-   *
-   * @link https://www.inngest.com/docs/functions/function-input-and-output#response-format
-   * @link https://www.inngest.com/docs/functions/retries
+   * An HTTP-like, standardised response format that allows Inngest to help
+   * orchestrate steps and retries.
    */
-  status: number;
-
-  /**
-   * The output of the function - the `body` - can be any arbitrary
-   * JSON-compatible data. It is then usable by any future steps.
-   *
-   * @link https://www.inngest.com/docs/functions/function-input-and-output#response-format
-   */
-  body?: any;
-}
-
-/**
- * A single step within a function.
- */
-export type InngestStep<Context = any> = (
-  /**
-   * The context for this step, including the triggering event and any previous
-   * step output.
-   */
-  context: Context
-) => Promise<InngestResponse> | InngestResponse;
-
-/**
- * The event payload structure for sending data to Inngest
- */
-export interface EventPayload<Data = Record<string, any>> {
-  /**
-   * A unique identifier for the event
-   */
-  name: string;
-  /**
-   * Any data pertinent to the event
-   */
-  data: Data;
-  /**
-   * Any user data associated with the event
-   * All fields ending in "_id" will be used to attribute the event to a particular user
-   */
-  user?: {
+  export interface Response {
     /**
-     * Your user's unique id in your system
+     * A step response must contain an HTTP status code.
+     *
+     * A `2xx` response indicates success; this is not a failure and no retry is
+     * necessary.
+     *
+     * A `4xx` response indicates a bad request; this step will not be retried as
+     * it is deemed irrecoverable. Examples of this might be an event with
+     * insufficient data or concerning a user that no longer exists.
+     *
+     * A `5xx` status indicates a temporary internal error; this will be retried
+     * according to the step and function's retry policy (3 times, by default).
+     *
+     * @link https://www.inngest.com/docs/functions/function-input-and-output#response-format
+     * @link https://www.inngest.com/docs/functions/retries
      */
-    external_id?: string;
-    /**
-     * Your user's email address
-     */
-    email?: string;
-    /**
-     * Your user's phone number
-     */
-    phone?: string;
-    [key: string]: any;
-  };
-  /**
-   * A specific event schema version
-   * (optional)
-   */
-  v?: string;
-  /**
-   * An integer representing the milliseconds since the unix epoch at which this event occurred.
-   * Defaults to the current time.
-   * (optional)
-   */
-  ts?: number;
-}
+    status: number;
 
-/**
- * A set of options for configuring the Inngest client
- */
-export interface InngestClientOptions {
+    /**
+     * The output of the function - the `body` - can be any arbitrary
+     * JSON-compatible data. It is then usable by any future steps.
+     *
+     * @link https://www.inngest.com/docs/functions/function-input-and-output#response-format
+     */
+    body?: any;
+  }
+
   /**
-   * The base Inngest Source API URL to append the Source API Key to.
-   * Defaults to https://inn.gs/e/
+   * A single step within a function.
    */
-  inngestApiUrl?: string;
+  export type Step<Context = any> = (
+    /**
+     * The context for this step, including the triggering event and any previous
+     * step output.
+     */
+    context: Context
+  ) => Promise<Response> | Response;
+
+  /**
+   * The event payload structure for sending data to Inngest
+   */
+  export interface EventPayload<Data = Record<string, any>> {
+    /**
+     * A unique identifier for the event
+     */
+    name: string;
+    /**
+     * Any data pertinent to the event
+     */
+    data: Data;
+    /**
+     * Any user data associated with the event
+     * All fields ending in "_id" will be used to attribute the event to a particular user
+     */
+    user?: {
+      /**
+       * Your user's unique id in your system
+       */
+      external_id?: string;
+      /**
+       * Your user's email address
+       */
+      email?: string;
+      /**
+       * Your user's phone number
+       */
+      phone?: string;
+      [key: string]: any;
+    };
+    /**
+     * A specific event schema version
+     * (optional)
+     */
+    v?: string;
+    /**
+     * An integer representing the milliseconds since the unix epoch at which this event occurred.
+     * Defaults to the current time.
+     * (optional)
+     */
+    ts?: number;
+  }
+
+  /**
+   * A set of options for configuring the Inngest client
+   */
+  export interface ClientOptions {
+    /**
+     * The base Inngest Source API URL to append the Source API Key to.
+     * Defaults to https://inn.gs/e/
+     */
+    inngestApiUrl?: string;
+  }
+
+  /**
+   * A set of options for configuring an Inngest function.
+   */
+  export interface FunctionOptions {
+    name: string;
+  }
+
+  export type Steps<Events extends Record<string, any>> = Record<
+    string,
+    InngestStep<Events, any[], any>
+  >;
 }
 
 /**
@@ -128,7 +142,7 @@ export class Inngest<Events extends Record<string, any>> {
   constructor(
     name: string,
     apiKey: string,
-    { inngestApiUrl = "https://inn.gs/e/" }: InngestClientOptions = {}
+    { inngestApiUrl = "https://inn.gs/e/" }: Inngest.ClientOptions = {}
   ) {
     this.name = name;
     this.#apiKey = apiKey;
@@ -183,8 +197,8 @@ export class Inngest<Events extends Record<string, any>> {
   public async send<Event extends keyof Events>(
     name: Event,
     payload:
-      | Omit<EventPayload<Events[Event]>, "name">
-      | Omit<EventPayload<Events[Event]>, "name">[]
+      | Omit<Inngest.EventPayload<Events[Event]>, "name">
+      | Omit<Inngest.EventPayload<Events[Event]>, "name">[]
   ): Promise<boolean> {
     const response = await axios.post(
       this.inngestApiUrl,
@@ -207,16 +221,76 @@ export class Inngest<Events extends Record<string, any>> {
    */
   public on<
     Event extends keyof Events,
-    Fn extends (arg: { event: EventPayload<Events[Event]> }) => any
-  >(name: string, event: Event, fn: Fn): InngestFunction<Events> {
-    return new InngestFunction(this);
+    Fn extends (arg: { event: Inngest.EventPayload<Events[Event]> }) => any
+  >(name: string, event: Event, fn: Fn): InngestFunction<Events>;
+  public on<
+    Event extends keyof Events,
+    Fn extends (arg: { event: Inngest.EventPayload<Events[Event]> }) => any
+  >(
+    opts: Inngest.FunctionOptions,
+    event: Event,
+    fn: Fn
+  ): InngestFunction<Events>;
+  public on<
+    Event extends keyof Events,
+    Fn extends (arg: { event: Inngest.EventPayload<Events[Event]> }) => any
+  >(
+    opts: string | Inngest.FunctionOptions,
+    event: Event,
+    fn: Fn
+  ): InngestFunction<Events> {
+    return this.#addFunction(
+      new InngestFunction(
+        this,
+        typeof opts === "string" ? { name: opts } : opts,
+        event,
+        {
+          step: new InngestStep(this, fn),
+        }
+      )
+    );
   }
+
+  #addFunction(fn: InngestFunction<Events>): typeof fn {
+    this.#fns.push(fn);
+    return fn;
+  }
+
+  /**
+   * Register any functions under this Inngest instance.
+   */
+  public register() {}
 }
 
 class InngestFunction<Events extends Record<string, any>> {
   readonly #inngest: Inngest<Events>;
+  readonly #opts: Inngest.FunctionOptions;
+  readonly #trigger: keyof Events;
+  readonly #steps: Inngest.Steps<Events>;
 
-  constructor(inngest: Inngest<Events>) {
+  constructor(
+    inngest: Inngest<Events>,
+    opts: Inngest.FunctionOptions,
+    trigger: keyof Events,
+    steps: Inngest.Steps<Events>
+  ) {
+    this.#inngest = inngest;
+    this.#opts = opts;
+    this.#trigger = trigger;
+    this.#steps = steps;
+  }
+
+  private getConfig() {}
+}
+
+class InngestStep<
+  Events extends Record<string, any>,
+  Input extends any[],
+  Output
+> {
+  readonly #inngest: Inngest<Events>;
+
+  constructor(inngest: Inngest<Events>, fn: (...args: Input) => Output) {
     this.#inngest = inngest;
   }
 }
