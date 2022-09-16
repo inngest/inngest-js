710f937e6ea291bf96b239cfecf460fdeb511aca
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-09-16 14:22:10 +0100

    Add CORS to default Express example

--- src/handlers/default.ts
+++ src/handlers/default.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-import { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 import { Inngest } from "../components/Inngest";
 import { InngestFunction } from "../components/InngestFunction";
-import { fnIdParam, stepIdParam } from "../helpers/consts";
+import { corsOrigin, fnIdParam, stepIdParam } from "../helpers/consts";
 import {
   EventPayload,
   FunctionConfig,
@@ -149,7 +149,19 @@ export class InngestCommHandler {
   }
 
   public createHandler(): any {
-    return async (req: Request, res: Response) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      /**
+       * Specifically for CORS (browser->site requests), only allow PUT requests
+       * from the dashboard.
+       */
+      if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Origin", corsOrigin);
+        res.header("Access-Control-Allow-Methods", "PUT");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+
+        return void next();
+      }
+
       console.log("Something hit the default handler!");
 
       const reqUrl = new URL(req.originalUrl, req.hostname);
