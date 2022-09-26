d1af3cd7ccb57fab362c61de01daf29c1e9cd983
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-09-26 15:12:13 +0100

    More polyfill juggling

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,13 +1,17 @@
+if (typeof global === "undefined") {
+  (global as any) = { crypto: window.crypto };
+}
+
+if (typeof window !== "undefined") {
+  window.global = global;
+}
+
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import cryptoIsomorphic from "isomorphic-crypto";
 import type { createHash as createHashT } from "crypto";
 
-if (typeof global !== "undefined") {
-  global.crypto = cryptoIsomorphic;
-} else {
-  (global as any) = { crypto: cryptoIsomorphic };
-}
+global.crypto = cryptoIsomorphic;
 
 if (typeof window !== "undefined") {
   window.crypto = cryptoIsomorphic;
