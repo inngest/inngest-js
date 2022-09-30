b9975d5c449f4e1929b8ce7854543adf21cb8260
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-09-30 16:05:49 +0100

    Clean up wrapping for desktop and mobile views

--- landing/src/components/Container.tsx
+++ landing/src/components/Container.tsx
@@ -7,3 +7,11 @@ interface Props {
 export const Container = ({ children }: Props) => {
   return <div class="w-full">{children}</div>;
 };
+
+export const Wrapper = ({ children }: Props) => {
+  return (
+    <div class="w-full flex justify-center">
+      <div class="w-[70rem] max-w-full px-8">{children}</div>
+    </div>
+  );
+};
--- landing/src/components/Content.tsx
+++ landing/src/components/Content.tsx
@@ -1,4 +1,5 @@
 import type { FunctionConfig } from "../../../src/types";
+import { Wrapper } from "./Container";
 import { FunctionBlock } from "./FunctionBlock";
 import { Spinner } from "./Loading";
 
@@ -64,15 +65,23 @@ export const Content = () => {
 
   return (
     <>
-      <div class="flex flex-col gap-4 p-20 bg-gray-100">
-        <div class="text-3xl">✅ Your functions are set up correctly</div>
-        <div>
-          <code>inngest@v0.55.1</code>
-        </div>
+      <div class="flex flex-col gap-4 py-20 bg-gray-100">
+        <Wrapper>
+          <div class="text-3xl">✅ Your functions are set up correctly</div>
+          <div class="ml-12 opacity-75">
+            <code>inngest@v0.55.1</code>
+          </div>
+        </Wrapper>
       </div>
       <div class="w-full flex items-center justify-center mt-8 p-4">
-        <div class="w-[60rem] max-w-full">
-          <div class="mb-8">Found 4 functions</div>
+        <Wrapper>
+          <div class="flex flex-row justify-between">
+            <div>Found 4 functions</div>
+            <a class="mb-8" href="#">
+              Don't see your function?
+            </a>
+          </div>
+
           <div class="flex flex-col">
             <div class="w-full grid grid-cols-[1fr_1fr_1fr] font-semibold border-b-2 border-slate-300 pb-1">
               <div>Name</div>
@@ -83,7 +92,7 @@ export const Content = () => {
               <FunctionBlock config={fn} altBg={i % 2 === 0} />
             ))}
           </div>
-        </div>
+        </Wrapper>
       </div>
     </>
   );
