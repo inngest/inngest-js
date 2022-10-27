8550e904718876bd947d36b2d54c4edc93deb451
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-10-27 14:33:05 -0400

    Add cron trigger button to landing page

--- landing/src/components/Button.tsx
+++ landing/src/components/Button.tsx
@@ -5,13 +5,25 @@ export interface ButtonProps {
   type?: string;
   onClick?: any;
   disabled?: boolean;
+  className?: string;
 }
 
-export const Button = ({ children, onClick, type, disabled }: ButtonProps) => {
+export const Button = ({
+  children,
+  onClick,
+  type,
+  disabled,
+  className = "",
+}: ButtonProps) => {
   const color = disabled ? "bg-slate-400" : "bg-blue-500";
 
   return (
-    <button class={`${color} text-white px-5 py-1 font-semibold rounded`} onClick={onClick} type={type} disabled={disabled}>
+    <button
+      className={`${color} text-white px-5 py-1 font-semibold rounded ${className}`}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
