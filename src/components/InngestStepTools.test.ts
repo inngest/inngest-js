46c012b293919d0de3e4498bab975d1228955448
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-11-01 17:16:30 +0000

    Add todo tests for `waitForEvent`, `step`, and `sleep`

---
+++ src/components/InngestStepTools.test.ts
@@ -0,0 +1,15 @@
+describe("createStepTools", () => {
+  test.todo("returns a toolset");
+});
+
+describe("waitForEvent", () => {
+  test.todo("...");
+});
+
+describe("step", () => {
+  test.todo("...");
+});
+
+describe("sleep", () => {
+  test.todo("...");
+});
