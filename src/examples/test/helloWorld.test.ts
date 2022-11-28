d51920a849c17b7119abdeb926360fd44d87172e
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-11-28 12:34:12 +0000

    Add integration tests using dev server to `helloWorld` example

--- src/examples/test/helloWorld.test.ts
+++ src/examples/test/helloWorld.test.ts
@@ -1,32 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import fetch from "cross-fetch";
-import { z } from "zod";
-
-const introspectionSchema = z.object({
-  functions: z.array(
-    z.object({
-      name: z.string(),
-      id: z.string(),
-      triggers: z.array(
-        z.object({ event: z.string() }).or(
-          z.object({
-            cron: z.string(),
-          })
-        )
-      ),
-      steps: z.object({
-        step: z.object({
-          id: z.literal("step"),
-          name: z.literal("step"),
-          runtime: z.object({
-            type: z.literal("http"),
-            url: z.string().url(),
-          }),
-        }),
-      }),
-    })
-  ),
-});
+import {
+  eventRunWithName,
+  introspectionSchema,
+  runHasTimeline,
+  sendEvent,
+} from "../../test/helpers";
 
 describe("introspection", () => {
   const specs = [
@@ -39,28 +18,47 @@ describe("introspection", () => {
       const res = await fetch(url);
       const data = introspectionSchema.parse(await res.json());
 
-    expect(data.functions).toContainEqual({
-      name: "Hello World",
-      id: expect.stringMatching(/^.*-hello-world$/),
-      triggers: [{ event: "demo/event.sent" }],
-      steps: {
-        step: {
-          id: "step",
-          name: "step",
-          runtime: {
-            type: "http",
+      expect(data.functions).toContainEqual({
+        name: "Hello World",
+        id: expect.stringMatching(/^.*-hello-world$/),
+        triggers: [{ event: "demo/event.sent" }],
+        steps: {
+          step: {
+            id: "step",
+            name: "step",
+            runtime: {
+              type: "http",
               url: expect.stringMatching(
                 /^http:\/\/(localhost|127\.0\.0\.1):3000\/api\/inngest\?fnId=.+-hello-world&stepId=step$/
-            ),
+              ),
+            },
           },
         },
-      },
-    });
+      });
     });
   });
 });
 
 describe("run", () => {
-  test.todo("runs in response to 'demo/event.sent'");
-  test.todo("returns 'Hello, Inngest!'");
+  let eventId: string;
+  let runId: string;
+
+  beforeAll(async () => {
+    eventId = await sendEvent("demo/event.sent");
+  });
+
+  test("runs in response to 'demo/event.sent'", async () => {
+    runId = await eventRunWithName(eventId, "Hello World");
+    expect(runId).toEqual(expect.any(String));
+  });
+
+  test("returns 'Hello, Inngest!'", async () => {
+    await expect(
+      runHasTimeline(runId, {
+        __typename: "StepEvent",
+        stepType: "COMPLETED",
+        output: JSON.stringify({ body: '"Hello, Inngest!"', status: 200 }),
+      })
+    ).resolves.toBe(true);
+  });
 });
