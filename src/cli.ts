914b109524445c2f804f257ba195561d81cb26d0
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-12-01 12:24:21 +0000

    Revert "Add proxy script `npx inngest` to go to `npx inngest-cli@latest` (#49)"
    This reverts commit 7220d2d1e21cf93e3addee9522cd9e425504c3e6.

--- src/cli.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { spawn } from "child_process";
-
-const [, , ...args] = process.argv;
-
-spawn("npx", ["--quiet", "inngest-cli@latest", ...args], {
-  stdio: "inherit",
-});
