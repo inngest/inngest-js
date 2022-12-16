385fe4a593a0822b00b0827bfb99f8617a89f688
Jack Williams <jack@inngest.com>
2022-12-16 15:27:14 +0000

    Deno integration testing (#51)
    ## Summary
    
    Adds some tooling for Deno support, allowing folks to use
    `"npm:inngest"` imports.
    
    Most of the changes here (all of the `deno_compat/` directory) are for
    integration testing purposes; we shouldn't have to update those files as
    we add features or fix bugs.
    
    This also includes the addition of a new handler,
    `"inngest/deno/fresh"`, which can be used with the
    [Fresh](https://fresh.deno.dev/) framework to deploy to [Deno
    Deploy](https://deno.com/deploy). Integration tests have also been added
    here for the
    [inngest/sdk-example-fresh-deno-deploy](https://github.com/inngest/sdk-example-fresh-deno-deploy)
    example repo, with unit tests for the handler coming in #52, as this
    refactors handlers some.
    
    At time of writing, [Deno Deploy doesn't support `npm:`
    specifiers](https://deno.com/blog/npm-and-deno-anywhere#:~:text=npm%20specifiers%20aren%27t%20currently%20supported%20in%20Deploy%2C%20but%20will%20be%20soon.),
    but it will soon™️.
    
    ## Related
    
    - #52

---
+++ deno_compat/inngest.ts
@@ -0,0 +1,10 @@
+import { createRequire } from "https://deno.land/std@0.167.0/node/module.ts";
+
+const require = createRequire(import.meta.url);
+
+const inngest = require("../dist");
+
+export const Inngest = inngest.Inngest;
+export const createFunction = inngest.createFunction;
+export const createScheduledFunction = inngest.createScheduledFunction;
+export const createStepFunction = inngest.createStepFunction;
