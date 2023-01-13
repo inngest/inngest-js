48912741dc7c2c70aa25d0e6b79fde0adae7434d
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-01-13 13:08:51 +0000

    Make DO handler types less verbose

--- src/digitalocean.ts
+++ src/digitalocean.ts
@@ -1,9 +1,7 @@
+import type { ServeHandler } from "./components/InngestCommHandler";
 import { InngestCommHandler } from "./components/InngestCommHandler";
 import { queryKeys } from "./helpers/consts";
 import { allProcessEnv } from "./helpers/env";
-import { RegisterOptions } from "./types";
-import type { Inngest } from "./components/Inngest";
-import type { InngestFunction } from "./components/InngestFunction";
 
 type HTTP = {
   headers: {
@@ -19,65 +17,12 @@ type Main = {
   [data: string]: any;
 };
 
-/**
- * Opts extends RegisterOptions to make `servePath` and `serveHost` required.
- */
-type Opts = RegisterOptions & {
-  /**
-   * The path to the Inngest serve endpoint. e.g.:
-   *
-   *     "/some/long/path/to/inngest/endpoint"
-   *
-   * By default, the library will try to infer this using request details such
-   * as the "Host" header and request path, but sometimes this isn't possible
-   * (e.g. when running in a more controlled environments such as AWS Lambda or
-   * when dealing with proxies/rediects).
-   *
-   * Provide the custom path (excluding the hostname) here to ensure that the
-   * path is reported correctly when registering functions with Inngest.
-   *
-   * To also provide a custom hostname, use `serveHost`.
-   */
-  servePath?: string;
-
-  /**
-   * The host used to access the Inngest serve endpoint, e.g.:
-   *
-   *     "https://myapp.com"
-   *
-   * By default, the library will try to infer this using request details such
-   * as the "Host" header and request path, but sometimes this isn't possible
-   * (e.g. when running in a more controlled environments such as AWS Lambda or
-   * when dealing with proxies/rediects).
-   *
-   * Provide the custom hostname here to ensure that the path is reported
-   * correctly when registering functions with Inngest.
-   *
-   * To also provide a custom path, use `servePath`.
-   */
-  serveHost: string;
-};
-
-type ServeHandler = (
-  /**
-   * The name of this app, used to scope and group Inngest functions, or
-   * the `Inngest` instance used to declare all functions.
-   */
-  nameOrInngest: string | Inngest<any>,
-
-  /**
-   * An array of the functions to serve and register with Inngest.
-   */
-  functions: InngestFunction<any>[],
-
-  /**
-   * A set of options to further configure the registration of Inngest
-   * functions.
-   */
-  opts: Opts
-) => any;
-
-export const serve: ServeHandler = (name, fns, opts: Opts) => {
+export const serve = (
+  name: Parameters<ServeHandler>[0],
+  fns: Parameters<ServeHandler>[1],
+  opts: Parameters<ServeHandler>[2] &
+    Required<Pick<NonNullable<Parameters<ServeHandler>[2]>, "serveHost">>
+) => {
   const handler = new InngestCommHandler(
     "digitalocean",
     name,
@@ -101,7 +46,7 @@ export const serve: ServeHandler = (name, fns, opts: Opts) => {
       // for the SDK to properly register and run functions.
       //
       // DigitalOcean provides no hostname or path in its arguments during execution.
-      const url = new URL(opts?.serveHost + (opts?.servePath || "/"));
+      const url = new URL(`${opts.serveHost}${opts?.servePath || "/"}`);
 
       return {
         register: () => {
