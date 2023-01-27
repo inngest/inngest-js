b0ab085272e32f38c4421e966c776088cd81da4e
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-01-27 19:40:04 +0000

    Add more examples - parallel work + reduce, sequential reduce, polling

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,5 +1,17 @@
 import helloWorld from "./helloWorld";
+import parallelReduce from "./parallelReduce";
+import parallelWork from "./parallelWork";
+import polling from "./polling";
 import promiseAll from "./Promise.all";
 import promiseRace from "./Promise.race";
+import sequentialReduce from "./sequentialReduce";
 
-export default [helloWorld, promiseAll, promiseRace];
+export default [
+  helloWorld,
+  promiseAll,
+  promiseRace,
+  parallelWork,
+  parallelReduce,
+  sequentialReduce,
+  polling,
+];
---
+++ src/examples/sequentialReduce.ts
@@ -0,0 +1,29 @@
+import { inngest } from "./client";
+
+const scoresDb: Record<string, number> = {
+  blue: 50,
+  red: 25,
+  green: 75,
+};
+
+export default inngest.createFunction(
+  { name: "Parallel Reduce" },
+  { event: "demo/sequential.reduce" },
+  async ({ step }) => {
+    const teams = Object.keys(scoresDb);
+
+    // Fetch every team's score sequentially and add them up
+    const totalScores = await teams.reduce(async (score, team) => {
+      const currentScore = await score;
+
+      const teamScore = await step.run(
+        `Get ${team} team score`,
+        () => scoresDb[team]
+      );
+
+      return currentScore + teamScore;
+    }, Promise.resolve(0));
+
+    return totalScores;
+  }
+);
