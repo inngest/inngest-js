01104184415fda78914a33289cbf9865472826da
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-01-27 16:57:25 +0000

    Add `Promise.all` and `Promise.race` examples to exported functions

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,3 +1,5 @@
 import helloWorld from "./helloWorld";
+import promiseAll from "./Promise.all";
+import promiseRace from "./Promise.race";
 
-export default [helloWorld];
+export default [helloWorld, promiseAll, promiseRace];
