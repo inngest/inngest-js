a3109506416dda89f3fad0e71f5f689e2a1890d1
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-02-17 17:31:33 +0000

    Use `json-stringify-deterministic` for any hashing

--- src/components/InngestCommHandler.ts
+++ src/components/InngestCommHandler.ts
@@ -1,4 +1,5 @@
 import { hmac, sha256 } from "hash.js";
+import stringify from "json-stringify-deterministic";
 import { z } from "zod";
 import { envKeys, headerKeys, queryKeys } from "../helpers/consts";
 import { devServerAvailable, devServerUrl } from "../helpers/devserver";
@@ -756,7 +757,7 @@ export class InngestCommHandler<H extends Handler, TransformedRes> {
     };
 
     // Calculate the checksum of the body... without the checksum itself being included.
-    body.hash = sha256().update(JSON.stringify(body)).digest("hex");
+    body.hash = sha256().update(stringify(body)).digest("hex");
     return body;
   }
 
@@ -851,9 +852,31 @@ export class InngestCommHandler<H extends Handler, TransformedRes> {
     sig: string | undefined,
     body: Record<string, any>
   ): void {
-    /**
-     * Disabled until this has an integration test using raw buffers.
-     */
+    if (this.isProd && !sig) {
+      throw new Error(`No ${headerKeys.Signature} provided`);
+    }
+
+    if (!this.isProd && !this.signingKey) {
+      return;
+    }
+
+    if (!this.signingKey) {
+      console.warn(
+        "No signing key provided to validate signature.  Find your dev keys at https://app.inngest.com/test/secrets"
+      );
+      return;
+    }
+
+    if (!sig) {
+      console.warn(`No ${headerKeys.Signature} provided`);
+      return;
+    }
+
+    new RequestSignature(sig).verifySignature({
+      body,
+      allowExpiredSignatures: this.allowExpiredSignatures,
+      signingKey: this.signingKey,
+    });
   }
 
   protected signResponse(): string {
@@ -899,7 +922,7 @@ class RequestSignature {
     }
 
     // Calculate the HMAC of the request body ourselves.
-    const encoded = typeof body === "string" ? body : JSON.stringify(body);
+    const encoded = typeof body === "string" ? body : stringify(body);
     // Remove the /signkey-[test|prod]-/ prefix from our signing key to calculate the HMAC.
     const key = signingKey.replace(/signkey-\w+-/, "");
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
