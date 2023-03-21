30790566175dedaca5af12b8758c8aa6ba53e7e7
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-03-21 13:28:56 +0000

    Add `"inngest/lambda"` serve handler for AWS Lambda environments

---
+++ src/lambda.test.ts
@@ -0,0 +1,29 @@
+import type { APIGatewayProxyResult } from "aws-lambda";
+import * as LambdaHandler from "./lambda";
+import { testFramework } from "./test/helpers";
+
+testFramework("AWS Lambda", LambdaHandler, {
+  transformReq: (req, _res, _env) => {
+    return [
+      {
+        path: req.path,
+        headers: req.headers,
+        httpMethod: req.method,
+        queryStringParameters: req.query,
+        body:
+          typeof req.body === "string" ? req.body : JSON.stringify(req.body),
+      },
+      {},
+    ];
+  },
+
+  transformRes: async (_res, retP: Promise<APIGatewayProxyResult>) => {
+    const ret = await retP;
+
+    return {
+      status: ret.statusCode,
+      body: ret.body || "",
+      headers: (ret.headers || {}) as Record<string, string>,
+    };
+  },
+});
