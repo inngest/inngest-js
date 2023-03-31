55ef357de464d80360053809180a4e7d18cfeedf
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-03-31 13:59:42 +0100

    Replace `onFailure` TODO comment with user-facing instructions

--- src/components/Inngest.ts
+++ src/components/Inngest.ts
@@ -387,10 +387,12 @@ export class Inngest<
           fns?: TFns;
 
           /**
-           * Leaving commented out; the feature can be added in a small PR
-           * when ready.
+           * Provide a function to be called if your function fails, meaning
+           * that it ran out of retries and was unable to complete successfully.
            *
-           * TODO Add user-facing comments here.
+           * This is useful for sending warning notifications or cleaning up
+           * after a failure and supports all the same functionality as a
+           * regular handler.
            */
           onFailure?: Handler<
             Events,
