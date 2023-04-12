5889d1342a9c08447f926df69d5da08627fe66f6
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-04-12 14:35:11 +0100

    Add `.fromUnion()` tests for `EventSchemas`

--- src/components/EventSchemas.test.ts
+++ src/components/EventSchemas.test.ts
@@ -115,6 +115,109 @@ describe("EventSchemas", () => {
     });
   });
 
+  describe("fromUnion", () => {
+    type TestEvent = {
+      name: "test.event";
+      data: { a: string };
+      user: { b: number };
+    };
+
+    type TestEvent2 = {
+      name: "test.event2";
+      data: { c: string };
+      user: { d: number };
+    };
+
+    test("sets types based on input", () => {
+      const schemas = new EventSchemas().fromUnion<TestEvent>();
+
+      assertType<Schemas<typeof schemas>["test.event"]["name"]>("test.event");
+      assertType<Schemas<typeof schemas>["test.event"]["data"]>({ a: "" });
+      assertType<Schemas<typeof schemas>["test.event"]["user"]>({ b: 0 });
+    });
+
+    test("can concatenate types with multiple calls", () => {
+      const schemas = new EventSchemas()
+        .fromUnion<TestEvent>()
+        .fromUnion<TestEvent2>();
+
+      assertType<Schemas<typeof schemas>["test.event"]["name"]>("test.event");
+      assertType<Schemas<typeof schemas>["test.event"]["data"]>({ a: "" });
+      assertType<Schemas<typeof schemas>["test.event"]["user"]>({ b: 0 });
+
+      assertType<Schemas<typeof schemas>["test.event2"]["name"]>("test.event2");
+      assertType<Schemas<typeof schemas>["test.event2"]["data"]>({ c: "" });
+      assertType<Schemas<typeof schemas>["test.event2"]["user"]>({ d: 0 });
+    });
+
+    test("can overwrite types with multiple calls", () => {
+      const schemas = new EventSchemas().fromUnion<TestEvent>().fromUnion<{
+        name: "test.event";
+        data: { c: string };
+        user: { d: number };
+      }>();
+
+      assertType<Schemas<typeof schemas>["test.event"]["name"]>("test.event");
+      assertType<Schemas<typeof schemas>["test.event"]["data"]>({ c: "" });
+      assertType<Schemas<typeof schemas>["test.event"]["user"]>({ d: 0 });
+    });
+
+    test.todo("cannot set extra properties");
+
+    test("can set 'any' type for data", () => {
+      const schemas = new EventSchemas().fromUnion<{
+        name: "test.event";
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        data: any;
+      }>();
+
+      assertType<IsAny<Schemas<typeof schemas>["test.event"]["data"]>>(true);
+    });
+
+    test("can set 'any' type for user", () => {
+      const schemas = new EventSchemas().fromUnion<{
+        name: "test.event";
+        data: { foo: string };
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        user: any;
+      }>();
+
+      assertType<IsAny<Schemas<typeof schemas>["test.event"]["user"]>>(true);
+    });
+
+    test("cannot set non-object type for data", () => {
+      // @ts-expect-error Data must be object type or any
+      new EventSchemas().fromUnion<{ name: "test.event"; data: string }>();
+    });
+
+    test("cannot set non-object type for user", () => {
+      // @ts-expect-error User must be object type or any
+      new EventSchemas().fromUnion<{
+        name: "test.event";
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        data: any;
+        user: string;
+      }>();
+    });
+
+    test("fills in missing properties with default values", () => {
+      const schemas = new EventSchemas().fromUnion<{
+        name: "test.event";
+        data: { foo: string };
+      }>();
+
+      assertType<Schemas<typeof schemas>["test.event"]["name"]>("test.event");
+      assertType<Schemas<typeof schemas>["test.event"]["data"]>({ foo: "" });
+      assertType<IsAny<Schemas<typeof schemas>["test.event"]["user"]>>(true);
+      assertType<
+        IsEqual<Schemas<typeof schemas>["test.event"]["ts"], number | undefined>
+      >(true);
+      assertType<
+        IsEqual<Schemas<typeof schemas>["test.event"]["v"], string | undefined>
+      >(true);
+    });
+  });
+
   describe("fromZod", () => {
     test("sets types based on input", () => {
       const schemas = new EventSchemas().fromZod({
