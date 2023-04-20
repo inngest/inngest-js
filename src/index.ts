05df6ee09493a7057e4d28f6868ee7e6acd318d3
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-04-20 16:53:05 +0100

    Export `isEdgeRuntime()` function for use with custom serve handlers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { InngestCommHandler } from "./components/InngestCommHandler";
 export type { ServeHandler } from "./components/InngestCommHandler";
 export { NonRetriableError } from "./components/NonRetriableError";
 export { headerKeys, internalEvents, queryKeys } from "./helpers/consts";
+export { isEdgeRuntime } from "./helpers/env";
 export type {
   ClientOptions,
   EventNameFromTrigger,
