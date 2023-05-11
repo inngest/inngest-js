0f7f07fd048b2aa15cc0e79ffa0e75811e2fca8b
Darwin D Wu <darwin67@users.noreply.github.com>
2023-05-11 15:45:19 -0700

    INN-1342: Add LogBuffer and ProxyLogger classes for temporary log storage

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -39,3 +39,62 @@ export class DefaultLogger implements Logger {
     console.debug(...args);
   }
 }
+
+/**
+ * LogBuffer hold the args that will be passed on to the actual
+ * logger when attempting to flush.
+ */
+class LogBuffer {
+  public readonly level: string;
+  public readonly args: LogArg[];
+
+  constructor(level: string, ...args: LogArg[]) {
+    this.level = level;
+    this.args = args;
+  }
+}
+
+/**
+ * ProxyLogger attempts to temporarily hold user's logs
+ * during the function run, and attempt to flush it through
+ * the provided logger when the function returns.
+ *
+ * The expected usage of this class is,
+ * 1. store the log attempt with its arguments and level
+ * 2. if function hits a step that has been memorized, clear the _buffer
+ * 3. on function return, flush all buffers
+ *
+ * And it should be invisible to the user as much as possible.
+ */
+export class ProxyLogger implements Logger {
+  private readonly _logger: Logger;
+  private _buffer: LogBuffer[] = [];
+
+  constructor(logger: Logger) {
+    this._logger = logger;
+  }
+
+  info(...args: LogArg[]) {
+    this._buffer.push(new LogBuffer("info", ...args));
+  }
+
+  warn(...args: LogArg[]) {
+    this._buffer.push(new LogBuffer("warn", ...args));
+  }
+
+  error(...args: LogArg[]) {
+    this._buffer.push(new LogBuffer("error", ...args));
+  }
+
+  debug(...args: LogArg[]) {
+    this._buffer.push(new LogBuffer("debug", ...args));
+  }
+
+  reset() {
+    this._buffer = [];
+  }
+
+  flush() {
+    throw new Error("TO BE IMPLEMENTED");
+  }
+}
