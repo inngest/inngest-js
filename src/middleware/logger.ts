7ee6f76da4c38f6615d02bf2bf2036d5651a586c
Darwin D Wu <darwin67@users.noreply.github.com>
2023-05-11 17:55:29 -0700

    Embed types in Logger interface for now since there's no need to expose it

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,20 @@
-import { type LogArg } from "../helpers/types";
+/**
+ * All kinds of arguments can come through
+ *
+ * Examples seen are
+ * - string
+ * - object / hash
+ * - values used for string interpolation <== basically anything
+ *
+ * See https://linear.app/inngest/issue/INN-1342/flush-logs-on-function-exitreturns for more details
+ */
+type LogArg = object;
 
-// Based on https://datatracker.ietf.org/doc/html/rfc5424#autoid-11
-// it's pretty reasonable to expect a logger to have the following interfaces
-// available.
+/**
+ * Based on https://datatracker.ietf.org/doc/html/rfc5424#autoid-11
+ * it's pretty reasonable to expect a logger to have the following interfaces
+ * available.
+ */
 export interface ILogger {
   info(...args: LogArg[]): void;
   warn(...args: LogArg[]): void;
