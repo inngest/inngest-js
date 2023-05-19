4a2e9d25b8cf8639b6ed87918634505a9c2f3de4
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-05-19 15:19:55 +0100

    Add and export `InngestMiddleware` and typing

---
+++ src/components/InngestMiddleware.ts
@@ -0,0 +1,319 @@
+import { type Simplify } from "type-fest";
+import {
+  type Await,
+  type MaybePromise,
+  type ObjectAssign,
+  type SendEventPayload,
+} from "../helpers/types";
+import {
+  type BaseContext,
+  type ClientOptions,
+  type EventPayload,
+  type IncomingOp,
+  type OutgoingOp,
+} from "../types";
+
+/**
+ * A middleware that can be registered with Inngest to hook into various
+ * lifecycles of the SDK and affect input and output of Inngest functionality.
+ *
+ * TODO Add docs and shortlink.
+ *
+ * See {@link https://innge.st/middleware}
+ *
+ * @example
+ *
+ * ```ts
+ * export const inngest = new Inngest({
+ *   middleware: [
+ *     new InngestMiddleware({
+ *       name: "My Middleware",
+ *       register: () => {
+ *         // ...
+ *       }
+ *     })
+ *   ]
+ * });
+ * ```
+ *
+ * @public
+ */
+export class InngestMiddleware<const TOpts extends MiddlewareOptions> {
+  /**
+   * The name of this middleware. Used primarily for debugging and logging
+   * purposes.
+   */
+  public readonly name: TOpts["name"];
+
+  /**
+   * This function is used to initialize your middleware and register any hooks
+   * you want to use. It will be called once when the SDK is initialized, and
+   * should be used to store any state you want to use in other parts of your
+   * middleware.
+   *
+   * It can be synchronous or asynchronous, in which case the client will wait
+   * for it to resolve before continuing to initialize the next middleware.
+   *
+   * Multiple clients could be used in the same application with differing
+   * middleware, so do not store state in global variables or assume that your
+   * middleware will only be used once.
+   *
+   * Must return an object detailing the hooks you want to register.
+   */
+  public readonly register: TOpts["register"];
+
+  /**
+   * TODO Create runtime waterfall functions for every hook, ready to be called.
+   */
+  constructor({ name, register }: TOpts) {
+    this.name = name;
+    this.register = register;
+  }
+}
+
+/**
+ * Options passed to new {@link InngestMiddleware} instance.
+ *
+ * @public
+ */
+export interface MiddlewareOptions {
+  /**
+   * The name of this middleware. Used primarily for debugging and logging
+   * purposes.
+   */
+  name: string;
+
+  /**
+   * This function is used to initialize your middleware and register any hooks
+   * you want to use. It will be called once when the SDK is initialized, and
+   * should be used to store any state you want to use in other parts of your
+   * middleware.
+   *
+   * It can be synchronous or asynchronous, in which case the client will wait
+   * for it to resolve before continuing to initialize the next middleware.
+   *
+   * Multiple clients could be used in the same application with differing
+   * middleware, so do not store state in global variables or assume that your
+   * middleware will only be used once.
+   *
+   * Must return an object detailing the hooks you want to register.
+   */
+  register: MiddlewareRegisterFn;
+}
+
+export type MiddlewareRegisterFn = () => MaybePromise<{
+  /**
+   * This hook is called for every function execution and allows you to hook
+   * into various stages of a run's lifecycle. Use this to store any state you
+   * want to use for the entirety of a particular run.
+   *
+   * It can be synchronous or asynchronous, in which case the client will wait
+   * for it to resolve before continuing to initialize the next middleware.
+   *
+   * Must return an object detailing the hooks you want to register.
+   */
+  run?: (ctx: InitialRunInfo) => MaybePromise<{
+    /**
+     * The `input` hook is called once the input for the function has been
+     * properly set up. This is where you can modify the input before the
+     * function starts to memoize or execute by returning an object containing
+     * changes to the context.
+     *
+     * For example, to add `foo` to the context, you'd return
+     * `{ ctx: { foo: "bar" } }`.
+     *
+     * @param ctx - The context for the incoming request.
+     * @param steps - The step data in state. Does not include internal IDs.
+     */
+    input?: MiddlewareRunInput;
+
+    /**
+     * The `beforeMemoization` hook is called before the function starts to
+     * memoize.
+     */
+    beforeMemoization?: BlankHook;
+
+    /**
+     * The `afterMemoization` hook is called after the function has finished
+     * memoizing.
+     */
+    afterMemoization?: BlankHook;
+
+    /**
+     * The `beforeExecution` hook is called before the function starts to
+     * execute. Execution here means that new code is being seen/run for the
+     * first time.
+     */
+    beforeExecution?: BlankHook;
+
+    /**
+     * The `afterExecution` hook is called after the function has finished
+     * executing.
+     */
+    afterExecution?: BlankHook;
+
+    /**
+     * The `output` hook is called after the function has finished executing
+     * and before the response is sent back to Inngest. This is where you
+     * can modify the output before it's sent back to Inngest by returning
+     * an object containing changes.
+     *
+     * @param err - The raw error that was thrown by the function, if any, so
+     * that you can capture exact error messages and stack traces.
+     *
+     * @param data - The prepared-but-unserialized data that was returned by
+     * the function, if any, so that you can modify the output.
+     */
+    output?: MiddlewareRunOutput;
+
+    /**
+     * The `beforeResponse` hook is called after the output has been set and
+     * before the response is sent back to Inngest. This is where you can
+     * perform any final actions before the response is sent back to Inngest.
+     */
+    beforeResponse?: BlankHook;
+  }>;
+
+  /**
+   * The `sendEvent` hook is called every time an event is sent to Inngest.
+   */
+  sendEvent?: () => MaybePromise<{
+    /**
+     * The `input` hook is called before the event is sent to Inngest. This
+     * is where you can modify the event before it's sent to Inngest by
+     * returning an object containing changes.
+     */
+    input?: MiddlewareSendEventInput;
+
+    /**
+     * The `output` hook is called after the event has been sent to Inngest.
+     * This is where you can perform any final actions after the event has
+     * been sent to Inngest and can modify the output the SDK sees.
+     */
+    output?: () => MaybePromise<void | unknown>;
+  }>;
+}>;
+
+/**
+ * A blank, no-op hook that passes nothing and expects nothing in return.
+ *
+ * @internal
+ */
+type BlankHook = () => MaybePromise<void>;
+
+/**
+ * Arguments sent to some `run` lifecycle hooks of a middleware.
+ *
+ * @internal
+ */
+type MiddlewareRunArgs = Readonly<{
+  /**
+   * The context object that will be passed to the function. This contains
+   * event data, some contextual data such as the run's ID, and step tooling.
+   */
+  ctx: Readonly<
+    BaseContext<
+      ClientOptions,
+      string,
+      Record<string, (...args: unknown[]) => unknown>
+    >
+  >;
+
+  /**
+   * The step data that will be passed to the function.
+   */
+  steps: Readonly<Omit<IncomingOp, "id">>;
+}>;
+
+/**
+ * The specific arguments sent to the `run` hook when an execution has begun.
+ * Differs from {@link MiddlewareRunArgs} in that we don't have a complete
+ * context yet.
+ *
+ * @internal
+ */
+type InitialRunInfo = Readonly<
+  Simplify<
+    Omit<MiddlewareRunArgs, "ctx"> & {
+      /**
+       * A partial context object that will be passed to the function. Does not
+       * necessarily contain all the data that will be passed to the function.
+       */
+      ctx: Pick<MiddlewareRunArgs["ctx"], "event" | "runId">;
+    }
+  >
+>;
+
+/**
+ * The shape of an `input` hook within a `run`, optionally returning change to
+ * the context or steps.
+ *
+ * @internal
+ */
+type MiddlewareRunInput = (ctx: MiddlewareRunArgs) => {
+  ctx?: Record<string, unknown>;
+  steps?: Pick<IncomingOp, "data">[];
+  // We need these in the future to allow users to specify their own complex
+  // types for transforming data above using just inference. e.g. every field
+  // ending with "_at" is transformed to a Date.
+  //
+  // transformEvent?: (event: EventPayload) => unknown;
+  // transformStep?: (data: unknown) => unknown;
+} | void;
+
+/**
+ * Arguments sent to some `sendEvent` lifecycle hooks of a middleware.
+ *
+ * @internal
+ */
+type MiddlewareSendEventArgs = Readonly<{
+  payloads: ReadonlyArray<SendEventPayload<Record<string, EventPayload>>>;
+}>;
+
+/**
+ * The shape of an `input` hook within a `sendEvent`, optionally returning
+ * change to the payloads.
+ *
+ * @internal
+ */
+type MiddlewareSendEventInput = (ctx: MiddlewareSendEventArgs) => {
+  payloads?: SendEventPayload<Record<string, EventPayload>>[];
+} | void;
+
+/**
+ * @internal
+ */
+type MiddlewareRunOutput = (
+  ctx: Readonly<Pick<OutgoingOp, "error" | "data">>
+) => Partial<Pick<OutgoingOp, "data">> | void;
+
+/**
+ * @internal
+ */
+type GetMiddlewareRunInputMutation<
+  TMiddleware extends InngestMiddleware<MiddlewareOptions>
+> = TMiddleware extends InngestMiddleware<infer TOpts>
+  ? TOpts["register"] extends MiddlewareRegisterFn
+    ? Await<Await<Await<TOpts["register"]>["run"]>["input"]> extends {
+        ctx: infer TCtx;
+      }
+      ? TCtx
+      : // eslint-disable-next-line @typescript-eslint/ban-types
+        {}
+    : // eslint-disable-next-line @typescript-eslint/ban-types
+      {}
+  : // eslint-disable-next-line @typescript-eslint/ban-types
+    {};
+
+/**
+ * @internal
+ */
+export type MiddlewareStackRunInputMutation<
+  TContext,
+  TMiddleware extends InngestMiddleware<MiddlewareOptions>[]
+> = ObjectAssign<
+  {
+    [K in keyof TMiddleware]: GetMiddlewareRunInputMutation<TMiddleware[K]>;
+  },
+  TContext
+>;
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ export { Inngest } from "./components/Inngest";
 export type { EventsFromOpts } from "./components/Inngest";
 export { InngestCommHandler } from "./components/InngestCommHandler";
 export type { ServeHandler } from "./components/InngestCommHandler";
+export { InngestMiddleware } from "./components/InngestMiddleware";
+export type { MiddlewareOptions } from "./components/InngestMiddleware";
 export { NonRetriableError } from "./components/NonRetriableError";
 export { headerKeys, internalEvents, queryKeys } from "./helpers/consts";
 export type { IsStringLiteral } from "./helpers/types";
