8cf031bc2678ebc08eae8a8c669b0d1d53dba239
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-05-23 15:00:06 +0100

    Add `waterfall` helper function for middleware hook stacks

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,3 +1,5 @@
+import { type Await } from "./types";
+
 /**
  * Wraps a function with a cache. When the returned function is run, it will
  * cache the result and return it on subsequent calls.
@@ -15,4 +17,34 @@ export const cacheFn = <T extends (...args: unknown[]) => unknown>(
 
     return cache.get(key);
   }) as T;
+};
+
+/**
+ * Given an array of functions, return a new function that will run each
+ * function in series and return the result of the final function. Regardless of
+ * if the functions are synchronous or asynchronous, they'll be made into an
+ * async promise chain.
+ *
+ * If an error is thrown, the waterfall will stop and return the error.
+ *
+ * Because this needs to support both sync and async functions, it only allows
+ * functions that accept a single argument.
+ *
+ * TODO Add a second function that decides how to merge results from prev and current results.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const waterfall = <TFns extends ((arg: any) => any)[]>(
+  fns: TFns
+): ((
+  ...args: Parameters<TFns[number]>["length"] extends 0
+    ? []
+    : Parameters<TFns[number]>[0]
+) => Promise<Await<TFns[number]>>) => {
+  return (...args) => {
+    const chain = fns.reduce(async (acc, fn) => {
+      return fn(await acc) as Promise<Await<TFns[number]>>;
+    }, Promise.resolve(args[0]));
+
+    return chain;
+  };
 };
