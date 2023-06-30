5f5ccc754be7d5e9e413477ba0ce18632d854b93
Anand Chowdhary <github@anandchowdhary.com>
2023-06-30 09:52:37 +0200

    chore: Copy express.ts to koa.ts

---
+++ src/koa.ts
@@ -0,0 +1,69 @@
+import { type Request, type Response } from "express";
+import {
+  InngestCommHandler,
+  type ServeHandler,
+} from "./components/InngestCommHandler";
+import { headerKeys, queryKeys } from "./helpers/consts";
+import { type SupportedFrameworkName } from "./types";
+
+export const name: SupportedFrameworkName = "express";
+
+/**
+ * Serve and register any declared functions with Inngest, making them available
+ * to be triggered by events.
+ *
+ * @public
+ */
+export const serve: ServeHandler = (nameOrInngest, fns, opts) => {
+  const handler = new InngestCommHandler(
+    name,
+    nameOrInngest,
+    fns,
+    opts,
+    (req: Request, _res: Response) => {
+      const hostname = req.get("host") || req.headers["host"];
+      const protocol = hostname?.includes("://") ? "" : `${req.protocol}://`;
+      const url = new URL(req.originalUrl, `${protocol}${hostname || ""}`);
+
+      return {
+        url,
+        run: () => {
+          if (req.method === "POST") {
+            return {
+              fnId: req.query[queryKeys.FnId] as string,
+              stepId: req.query[queryKeys.StepId] as string,
+              data: req.body as Record<string, unknown>,
+              signature: req.headers[headerKeys.Signature] as string,
+            };
+          }
+        },
+        register: () => {
+          if (req.method === "PUT") {
+            return {
+              deployId: req.query[queryKeys.DeployId]?.toString(),
+            };
+          }
+        },
+        view: () => {
+          if (req.method === "GET") {
+            return {
+              isIntrospection: Object.hasOwnProperty.call(
+                req.query,
+                queryKeys.Introspect
+              ),
+            };
+          }
+        },
+      };
+    },
+    (actionRes, _req, res) => {
+      for (const [name, value] of Object.entries(actionRes.headers)) {
+        res.setHeader(name, value);
+      }
+
+      return res.status(actionRes.status).send(actionRes.body);
+    }
+  );
+
+  return handler.createHandler();
+};
