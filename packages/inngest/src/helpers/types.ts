b62c88ba6045ebaae02ce176a2ab9de4e9b63c78
Jack Williams <jack@inngest.com>
2023-08-15 14:54:16 +0100

    Support Vercel's Express in `"inngest/express"` serve handler (#290)
    ## Summary
    <!-- Succinctly describe your change, providing context, what you've
    changed, and why. -->
    
    Adds support for Vercel's Node Express server, which has differing
    reported types from the `express` package.
    
    ## Checklist
    <!-- Tick these items off as you progress. -->
    <!-- If an item isn't applicable, ideally please strikeout the item by
    wrapping it in "~~"" and suffix it with "N/A My reason for skipping
    this." -->
    <!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->
    
    - [ ] ~~Added a [docs PR](https://github.com/inngest/website) that
    references this PR~~ N/A Bug fix
    - [x] Added unit/integration tests
    - [x] Added changesets if applicable
    
    ## Related
    <!-- A space for any related links, issues, or PRs. -->
    <!-- Linear issues are autolinked. -->
    <!-- e.g. - INN-123 -->
    <!-- GitHub issues/PRs can be linked using shorthand. -->
    <!-- e.g. "- inngest/inngest#123" -->
    <!-- Feel free to remove this section if there are no applicable related
    links.-->
    - Supersedes #288
    - Closes inngest/inngest#553

--- packages/inngest/src/helpers/types.ts
+++ packages/inngest/src/helpers/types.ts
@@ -186,3 +186,9 @@ export type ObjectAssign<TArr, TAcc = {}> = TArr extends [
 export type ExclusiveKeys<T, Keys1 extends keyof T, Keys2 extends keyof T> =
   | (Omit<T, Keys1> & { [K in Keys1]?: never })
   | (Omit<T, Keys2> & { [K in Keys2]?: never });
+
+/**
+ * A type that represents either `A` or `B`. Shared properties retain their
+ * types and unique properties are marked as optional.
+ */
+export type Either<A, B> = Partial<A> & Partial<B> & (A | B);
