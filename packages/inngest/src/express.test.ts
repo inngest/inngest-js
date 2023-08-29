a1b570500525900d87db58260ca221a3e8b06e01
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-08-29 14:46:18 +0000

    Remove unneeded `InngestCommHandler` test in `"inngest/express"` handler

--- packages/inngest/src/express.test.ts
+++ packages/inngest/src/express.test.ts
@@ -1,7 +1,6 @@
-import { InngestCommHandler } from "@local/components/InngestCommHandler";
 import * as ExpressHandler from "@local/express";
 import { type VercelRequest, type VercelRequestQuery } from "@vercel/node";
-import { createClient, testFramework } from "./test/helpers";
+import { testFramework } from "./test/helpers";
 
 testFramework("Express", ExpressHandler);
 
@@ -18,33 +17,4 @@ testFramework("Express (Vercel)", ExpressHandler, {
 
     return [req, res];
   },
-});
-
-describe("InngestCommHandler", () => {
-  describe("registerBody", () => {
-    it("Includes correct base URL for functions", () => {
-      const client = createClient({ id: "test" });
-
-      const fn = client.createFunction(
-        { id: "Test Express Function" },
-        { event: "test/event.name" },
-        () => undefined
-      );
-      const ch = new InngestCommHandler(
-        "test-framework",
-        client,
-        [fn],
-        {},
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
-        () => undefined as unknown as any,
-        () => undefined
-      );
-
-      const url = new URL("http://localhost:8000/api/inngest");
-
-      const body = ch["registerBody"](url);
-      expect(body.appName).toBe("test");
-      expect(body.url).toBe("http://localhost:8000/api/inngest");
-    });
-  });
 });
