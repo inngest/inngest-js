1576888519439ca50c1dddb1a9ffc5bd9c9afeec
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-08-30 17:51:09 +0000

    Add `rethrowError()` helper for prefixing errors with Inngest context

--- packages/inngest/src/helpers/errors.ts
+++ packages/inngest/src/helpers/errors.ts
@@ -360,3 +360,29 @@ export class OutgoingResultError extends Error {
     this.result = result;
   }
 }
+
+/**
+ * Create a function that will rethrow an error with a prefix added to the
+ * message.
+ *
+ * Useful for adding context to errors that are rethrown.
+ *
+ * @example
+ * ```ts
+ * await doSomeAction().catch(rethrowError("Failed to do some action"));
+ * ```
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const rethrowError = (prefix: string): ((err: any) => never) => {
+  return (err) => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-template-expressions
+      err.message &&= `${prefix}; ${err.message}`;
+    } catch (noopErr) {
+      // no-op
+    } finally {
+      // eslint-disable-next-line no-unsafe-finally
+      throw err;
+    }
+  };
+};
