289dd4e48450d47e1b6ff1f7098767f75c7bc549
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-10-09 11:26:37 +0000

    Fix expected returns for v0.18.0 of the dev server

--- packages/inngest/src/test/functions/parallel-reduce/index.test.ts
+++ packages/inngest/src/test/functions/parallel-reduce/index.test.ts
@@ -43,7 +43,7 @@ describe("run", () => {
       runHasTimeline(runId, {
         __typename: "StepEvent",
         stepType: "COMPLETED",
-        output: JSON.stringify({ body: "150", status: 200 }),
+        output: JSON.stringify("150"),
       })
     ).resolves.toBeDefined();
   }, 60000);
