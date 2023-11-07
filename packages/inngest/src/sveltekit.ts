7e219cc647d13595fbe393ebde7af29918bffebe
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-11-07 12:35:21 +0000

    Modernize SvelteKit handler

--- packages/inngest/src/sveltekit.ts
+++ packages/inngest/src/sveltekit.ts
@@ -1,13 +1,12 @@
 import { type RequestEvent } from "@sveltejs/kit";
 import {
   InngestCommHandler,
-  type ServeHandler,
+  type ServeHandlerOptions,
 } from "./components/InngestCommHandler";
-import { headerKeys, queryKeys } from "./helpers/consts";
 import { processEnv } from "./helpers/env";
 import { type SupportedFrameworkName } from "./types";
 
-export const name: SupportedFrameworkName = "sveltekit";
+export const frameworkName: SupportedFrameworkName = "sveltekit";
 
 /**
  * Using SvelteKit, serve and register any declared functions with Inngest,
@@ -15,66 +14,43 @@ export const name: SupportedFrameworkName = "sveltekit";
  *
  * @example
  * ```ts
- * export const { GET, POST, PUT } = serve(inngest, [fn1, fn2]);
+ * export const { GET, POST, PUT } = serve(...);
  * ```
  *
  * @public
  */
-export const serve: ServeHandler = (nameOrInngest, fns, opts) => {
-  const handler = new InngestCommHandler(
-    name,
-    nameOrInngest,
-    fns,
-    opts,
-    (method: string, event: RequestEvent) => {
-      const protocol =
-        processEnv("NODE_ENV") === "development" ? "http" : "https";
-      const url = new URL(
-        event.request.url,
-        `${protocol}://${
-          event.request.headers.get("host") || opts?.serveHost || ""
-        }`
-      );
-
+export const serve = (options: ServeHandlerOptions) => {
+  const handler = new InngestCommHandler({
+    frameworkName,
+    ...options,
+    handler: (method: string, event: RequestEvent) => {
       return {
-        url,
-        run: async () => {
-          if (method === "POST") {
-            return {
-              fnId: url.searchParams.get(queryKeys.FnId) as string,
-              stepId: url.searchParams.get(queryKeys.StepId) as string,
-              data: (await event.request.json()) as Record<string, unknown>,
-              signature:
-                event.request.headers.get(headerKeys.Signature) || undefined,
-            };
-          }
+        method: () => method,
+        body: () => event.request.json(),
+        headers: (key) => event.request.headers.get(key),
+        url: () => {
+          const protocol =
+            processEnv("NODE_ENV") === "development" ? "http" : "https";
+
+          return new URL(
+            event.request.url,
+            `${protocol}://${
+              event.request.headers.get("host") || options.serveHost || ""
+            }`
+          );
         },
-        register: () => {
-          if (method === "PUT") {
-            return {
-              deployId: url.searchParams.get(queryKeys.DeployId),
-            };
-          }
-        },
-        view: () => {
-          if (method === "GET") {
-            return {
-              isIntrospection: url.searchParams.has(queryKeys.Introspect),
-            };
-          }
+        transformResponse: ({ body, headers, status }) => {
+          return new Response(body, { status, headers });
         },
       };
     },
-    ({ body, headers, status }): Response => {
-      return new Response(body, { status, headers });
-    }
-  );
-
-  const fn = handler.createHandler();
-
-  return Object.defineProperties(fn.bind(null), {
-    GET: { value: fn.bind(null, "GET") },
-    POST: { value: fn.bind(null, "POST") },
-    PUT: { value: fn.bind(null, "PUT") },
   });
+
+  const baseFn = handler.createHandler();
+
+  return {
+    GET: baseFn.bind(null, "GET"),
+    POST: baseFn.bind(null, "POST"),
+    PUT: baseFn.bind(null, "PUT"),
+  };
 };
