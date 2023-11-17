98f556075a4ab8d8833e95714938665d102233ef
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-11-17 13:41:52 +0000

    Remove `deterministic-sleep-until-dates` - too inaccurate

--- packages/eslint-plugin/src/configs/recommended.ts
+++ packages/eslint-plugin/src/configs/recommended.ts
@@ -1,7 +1,6 @@
 export const recommended = {
   rules: {
     "@inngest/await-inngest-send": "warn",
-    "@inngest/deterministic-sleep-until-dates": "warn",
     "@inngest/no-nested-steps": "error",
     "@inngest/no-variable-mutation-in-step": "error",
   },
--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -1,12 +1,10 @@
 import { TSESLint } from "@typescript-eslint/utils";
 import { awaitInngestSend } from "./await-inngest-send";
-import { deterministicSleepUntilDates } from "./deterministic-sleep-until-dates";
 import { noNestedSteps } from "./no-nested-steps";
 import { noVariableMutationInStep } from "./no-variable-mutation-in-step";
 
 export const rules = {
   "await-inngest-send": awaitInngestSend,
-  "deterministic-sleep-until-dates": deterministicSleepUntilDates,
   "no-nested-steps": noNestedSteps,
   "no-variable-mutation-in-step": noVariableMutationInStep,
 } satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>;
