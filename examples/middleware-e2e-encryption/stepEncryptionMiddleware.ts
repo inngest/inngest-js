dcfee74050bda7f9f7e7c97fad577cedf40f55f0
Jack Williams <jack@inngest.com>
2023-11-17 16:21:55 +0000

    Add `middleware-e2e-encryption` middleware example (#402)
    ## Summary
    <!-- Succinctly describe your change, providing context, what you've
    changed, and why. -->
    
    Adds an example middleware,
    [`middleware-e2e-encryption`](https://github.com/inngest/inngest-js/tree/middleware-encryption-example/examples/middleware-e2e-encryption).
    
    ## Checklist
    <!-- Tick these items off as you progress. -->
    <!-- If an item isn't applicable, ideally please strikeout the item by
    wrapping it in "~~"" and suffix it with "N/A My reason for skipping
    this." -->
    <!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->
    
    - [ ] ~~Added a [docs PR](https://github.com/inngest/website) that
    references this PR~~ N/A
    - [ ] ~~Added unit/integration tests~~ N/A
    - [x] Added changesets if applicable

---
+++ examples/middleware-e2e-encryption/stepEncryptionMiddleware.ts
@@ -0,0 +1,74 @@
+import { InngestMiddleware } from "inngest";
+
+const encryptionMarker = "__ENCRYPTED__";
+type EncryptedValue = { [encryptionMarker]: true; data: string };
+
+export const encryptionMiddleware = (
+  key: string = process.env.INNGEST_ENCRYPTION_KEY as string
+) => {
+  if (!key) {
+    throw new Error("Missing INNGEST_ENCRYPTION_KEY environment variable");
+  }
+
+  // Some internal functions that we'll use to encrypt and decrypt values.
+  // In practice, you'll want to use the `key` passed in to handle encryption
+  // properly.
+  const isEncryptedValue = (value: unknown): value is EncryptedValue => {
+    return (
+      typeof value === "object" &&
+      value !== null &&
+      encryptionMarker in value &&
+      value[encryptionMarker] === true &&
+      "data" in value &&
+      typeof value["data"] === "string"
+    );
+  };
+
+  const encrypt = (value: unknown): EncryptedValue => {
+    return {
+      [encryptionMarker]: true,
+      data: JSON.stringify(value).split("").reverse().join(""),
+    };
+  };
+
+  const decrypt = <T>(value: T): T => {
+    if (isEncryptedValue(value)) {
+      return JSON.parse(value.data.split("").reverse().join("")) as T;
+    }
+
+    return value;
+  };
+
+  return new InngestMiddleware({
+    name: "Step Encryption Middleware",
+    init: () => ({
+      onSendEvent: () => ({
+        transformInput: ({ payloads }) => ({
+          payloads: payloads.map((payload) => ({
+            ...payload,
+            data: payload.data && encrypt(payload.data),
+          })),
+        }),
+      }),
+      onFunctionRun: () => ({
+        transformInput: ({ ctx, steps }) => ({
+          steps: steps.map((step) => ({
+            ...step,
+            data: step.data && decrypt(step.data),
+          })),
+        }),
+        transformOutput: (ctx) => {
+          if (!ctx.step) {
+            return;
+          }
+
+          return {
+            result: {
+              data: ctx.result.data && encrypt(ctx.result.data),
+            },
+          };
+        },
+      }),
+    }),
+  });
+};
