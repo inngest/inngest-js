327a4a0616b20958c045c840c08da9b18d8842b6
Jack Williams <jack@inngest.com>
2023-12-11 17:40:35 +0000

    Add `@inngest/middleware-encryption` package (#417)
    ## Summary
    <!-- Succinctly describe your change, providing context, what you've
    changed, and why. -->
    
    Add `@inngest/middleware-encryption` package to allow easy E2E
    encryption.
    
    See the
    [README.md](https://github.com/inngest/inngest-js/blob/middleware-encryption/packages/middleware-encryption/README.md).
    
    The package is already released (first release must always be done by
    hand); this PR merging makes no immediate changes, but ideally we use it
    to review `README.md`, license, and structure.
    
    ## Checklist
    <!-- Tick these items off as you progress. -->
    <!-- If an item isn't applicable, ideally please strikeout the item by
    wrapping it in "~~"" and suffix it with "N/A My reason for skipping
    this." -->
    <!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->
    
    - [x] Added a [docs PR](https://github.com/inngest/website) that
    references this PR
    - [ ] ~~Added unit/integration tests~~ N/A Testing story needed for this
    - [x] Added changesets if applicable
    
    ## Related
    
    - Docs at inngest/website#591

---
+++ packages/middleware-encryption/src/middleware.ts
@@ -0,0 +1,269 @@
+import AES from "crypto-js/aes";
+import CryptoJSUtf8 from "crypto-js/enc-utf8";
+import { InngestMiddleware, type MiddlewareRegisterReturn } from "inngest";
+
+/**
+ * A marker used to identify encrypted values without having to guess.
+ */
+const ENCRYPTION_MARKER = "__ENCRYPTED__";
+export const DEFAULT_ENCRYPTION_FIELD = "encrypted";
+
+export type EventEncryptionFieldInput =
+  | string
+  | string[]
+  | ((field: string) => boolean)
+  | false;
+
+/**
+ * Options used to configure the encryption middleware.
+ */
+export interface EncryptionMiddlewareOptions {
+  /**
+   * The key or keys used to encrypt and decrypt data. If multiple keys are
+   * provided, the first key will be used to encrypt data and all keys will
+   * be tried when decrypting data.
+   */
+  key?: string | string[];
+
+  /**
+   * The encryption service used to encrypt and decrypt data. If not provided,
+   * a default encryption service will be used.
+   */
+  encryptionService?: EncryptionService;
+
+  /**
+   * The top-level fields of the event that will be encrypted. Can be a single
+   * field name, an array of field names, a function that returns `true` if
+   * a field should be encrypted, or `false` to disable all event encryption.
+   *
+   * By default, the top-level field named `"encrypted"` will be encrypted (exported as `DEFAULT_ENCRYPTION_FIELD`).
+   */
+  eventEncryptionField?: EventEncryptionFieldInput;
+}
+
+/**
+ * Encrypts and decrypts data sent to and from Inngest.
+ */
+export const encryptionMiddleware = (
+  /**
+   * Options used to configure the encryption middleware. If a custom
+   * `encryptionService` is not provided, the `key` option is required.
+   */
+  opts: EncryptionMiddlewareOptions
+) => {
+  const service =
+    opts.encryptionService || new DefaultEncryptionService(opts.key);
+  const shouldEncryptEvents = Boolean(
+    opts.eventEncryptionField ?? DEFAULT_ENCRYPTION_FIELD
+  );
+
+  const encryptValue = (value: unknown): EncryptedValue => {
+    return {
+      [ENCRYPTION_MARKER]: true,
+      data: service.encrypt(value),
+    };
+  };
+
+  const decryptValue = (value: unknown): unknown => {
+    if (isEncryptedValue(value)) {
+      return service.decrypt(value.data);
+    }
+
+    return value;
+  };
+
+  const fieldShouldBeEncrypted = (field: string): boolean => {
+    if (typeof opts.eventEncryptionField === "undefined") {
+      return field === DEFAULT_ENCRYPTION_FIELD;
+    }
+
+    if (typeof opts.eventEncryptionField === "function") {
+      return opts.eventEncryptionField(field);
+    }
+
+    if (Array.isArray(opts.eventEncryptionField)) {
+      return opts.eventEncryptionField.includes(field);
+    }
+
+    return opts.eventEncryptionField === field;
+  };
+
+  const encryptEventData = (data: Record<string, unknown>): unknown => {
+    const encryptedData = Object.keys(data).reduce((acc, key) => {
+      if (fieldShouldBeEncrypted(key)) {
+        return { ...acc, [key]: encryptValue(data[key]) };
+      }
+
+      return { ...acc, [key]: data[key] };
+    }, {});
+
+    return encryptedData;
+  };
+
+  const decryptEventData = (data: Record<string, unknown>): unknown => {
+    const decryptedData = Object.keys(data).reduce((acc, key) => {
+      if (isEncryptedValue(data[key])) {
+        return { ...acc, [key]: decryptValue(data[key]) };
+      }
+
+      return { ...acc, [key]: data[key] };
+    }, {});
+
+    return decryptedData;
+  };
+
+  return new InngestMiddleware({
+    name: "@inngest/middleware-encryption",
+    init: () => {
+      const registration: MiddlewareRegisterReturn = {
+        onFunctionRun: () => {
+          return {
+            transformInput: ({ ctx, steps }) => {
+              const inputTransformer: InputTransformer = {
+                steps: steps.map((step) => ({
+                  ...step,
+                  data: step.data && decryptValue(step.data),
+                })),
+              };
+
+              if (shouldEncryptEvents) {
+                inputTransformer.ctx = {
+                  event: ctx.event && {
+                    ...ctx.event,
+                    data: ctx.event.data && decryptEventData(ctx.event.data),
+                  },
+                  events:
+                    ctx.events &&
+                    ctx.events?.map((event) => ({
+                      ...event,
+                      data: event.data && decryptEventData(event.data),
+                    })),
+                } as {};
+              }
+
+              return inputTransformer;
+            },
+            transformOutput: (ctx) => {
+              if (!ctx.step) {
+                return;
+              }
+
+              return {
+                result: {
+                  data: ctx.result.data && encryptValue(ctx.result.data),
+                },
+              };
+            },
+          };
+        },
+      };
+
+      if (shouldEncryptEvents) {
+        registration.onSendEvent = () => {
+          return {
+            transformInput: ({ payloads }) => {
+              return {
+                payloads: payloads.map((payload) => ({
+                  ...payload,
+                  data: payload.data && encryptEventData(payload.data),
+                })),
+              };
+            },
+          };
+        };
+      }
+
+      return registration;
+    },
+  });
+};
+
+export interface EncryptedValue {
+  [ENCRYPTION_MARKER]: true;
+  data: string;
+}
+
+type InputTransformer = NonNullable<
+  Awaited<
+    ReturnType<
+      NonNullable<
+        Awaited<
+          ReturnType<NonNullable<MiddlewareRegisterReturn["onFunctionRun"]>>
+        >["transformInput"]
+      >
+    >
+  >
+>;
+
+const isEncryptedValue = (value: unknown): value is EncryptedValue => {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    ENCRYPTION_MARKER in value &&
+    value[ENCRYPTION_MARKER] === true &&
+    "data" in value &&
+    typeof value["data"] === "string"
+  );
+};
+
+/**
+ * A service that encrypts and decrypts data. You can implement this abstract
+ * class to provide your own encryption service, or use the default encryption
+ * service provided by this package.
+ */
+export abstract class EncryptionService {
+  public abstract encrypt(value: unknown): string;
+  public abstract decrypt(value: string): unknown;
+}
+
+/**
+ * The default encryption service used by the encryption middleware.
+ *
+ * This service uses AES encryption to encrypt and decrypt data. It supports
+ * multiple keys, so that you can rotate keys without breaking existing
+ * encrypted data.
+ */
+export class DefaultEncryptionService extends EncryptionService {
+  private readonly keys: [string, ...string[]];
+
+  constructor(key: string | string[] | undefined) {
+    super();
+
+    if (!key) {
+      throw new Error("Missing encryption key(s) in encryption middleware");
+    }
+
+    const keys = (Array.isArray(key) ? key : [key])
+      .map((s) => s.trim())
+      .filter(Boolean);
+
+    if (!keys.length) {
+      throw new Error("Missing encryption key(s) in encryption middleware");
+    }
+
+    this.keys = keys as [string, ...string[]];
+  }
+
+  encrypt(value: unknown): string {
+    return AES.encrypt(JSON.stringify(value), this.keys[0]).toString();
+  }
+
+  decrypt(value: string): unknown {
+    let err: unknown;
+
+    for (const key of this.keys) {
+      try {
+        const decrypted = AES.decrypt(value, key).toString(CryptoJSUtf8);
+        return JSON.parse(decrypted);
+      } catch (decryptionError) {
+        err = decryptionError;
+        continue;
+      }
+    }
+
+    throw (
+      err ||
+      new Error("Unable to decrypt value; no keys were able to decrypt it")
+    );
+  }
+}
