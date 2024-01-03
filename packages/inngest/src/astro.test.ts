a41af596f8f1486935fa6d7ad1ef67bf6e5da727
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-01-03 09:24:51 +0000

    Add unit tests for `"inngest/astro"`

---
+++ packages/inngest/src/astro.test.ts
@@ -0,0 +1,27 @@
+import * as EdgeHandler from "@local/edge";
+import { testFramework } from "./test/helpers";
+
+testFramework("Edge", EdgeHandler, {
+  transformReq: (req) => {
+    const headers = new Headers();
+    Object.entries(req.headers).forEach(([k, v]) => {
+      headers.set(k, v as string);
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    (req as any).headers = headers;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    (req as any).json = () => Promise.resolve(req.body);
+    return [req];
+  },
+  transformRes: async (_args, ret: Response) => {
+    const headers: Record<string, string> = {};
+    ret.headers.forEach((v, k) => {
+      headers[k] = v;
+    });
+    return {
+      status: ret.status,
+      body: await ret.text(),
+      headers,
+    };
+  },
+});
