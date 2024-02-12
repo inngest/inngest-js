inngest/inngest-js

commit 49a58d1d5fe407202774dfbbd29bbb40f29504ed

Add internal events to default `EventSchemas` (#426)
## Summary
<!-- Succinctly describe your change, providing context, what you've
changed, and why. -->

Adds internal `inngest/function.failed` and `inngest/function.completed`
events to the list of default events when using `new EventSchemas()`.

## Checklist
<!-- Tick these items off as you progress. -->
<!-- If an item isn't applicable, ideally please strikeout the item by
wrapping it in "~~"" and suffix it with "N/A My reason for skipping
this." -->
<!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->

- [ ] ~~Added a [docs PR](https://github.com/inngest/website) that
references this PR~~ N/A Bug fix - already stated that we do this
- [x] Added unit/integration tests
- [x] Added changesets if applicable

packages/inngest/src/helpers/types.ts
@@ -27,9 +27,16 @@ export type PartialK<T, K extends PropertyKey = PropertyKey> = Partial<
 export type SendEventPayload<Events extends Record<string, EventPayload>> =
   SingleOrArray<
     {
-      [K in keyof Events]: PartialK<Omit<Events[K], "v">, "ts">;
-    }[keyof Events]
+      [K in keyof WithoutInternal<Events>]: PartialK<
+        Omit<WithoutInternal<Events>[K], "v">,
+        "ts"
+      >;
+    }[keyof WithoutInternal<Events>]
   >;
+
+export type WithoutInternal<T extends Record<string, EventPayload>> = {
+  [K in keyof T as K extends `inngest/${string}` ? never : K]: T[K];
+};
 
 /**
  * A list of simple, JSON-compatible, primitive types that contain no other

packages/inngest/src/types.ts
@@ -99,6 +99,22 @@ export type FailureEventArgs<P extends EventPayload = EventPayload> = {
   error: Error;
 };
 
+export type FinishedEventPayload = {
+  name: `${internalEvents.FunctionFinished}`;
+  data: {
+    function_id: string;
+    run_id: string;
+    correlation_id?: string;
+  } & (
+    | {
+        error: z.output<typeof failureEventErrorSchema>;
+      }
+    | {
+        result: unknown;
+      }
+  );
+};
+
 /**
  * Unique codes for the different types of operation that can be sent to Inngest
  * from SDK step functions.

packages/inngest/src/components/EventSchemas.test.ts
@@ -12,12 +12,18 @@ type Schemas<T extends EventSchemas<any>> = GetEvents<
 >;
 
 describe("EventSchemas", () => {
-  test("creates generic types by default", () => {
+  test("adds internal types by default", () => {
     const schemas = new EventSchemas();
 
-    assertType<IsEqual<Schemas<typeof schemas>, Record<string, EventPayload>>>(
-      true
-    );
+    type Expected =
+      | `${internalEvents.FunctionFailed}`
+      | `${internalEvents.FunctionFinished}`;
+
+    type Actual = Schemas<typeof schemas>[keyof Schemas<
+      typeof schemas
+    >]["name"];
+
+    assertType<IsEqual<Expected, Actual>>(true);
   });
 
   describe("fromRecord", () => {

packages/inngest/src/components/Inngest.test.ts
@@ -7,11 +7,7 @@ import {
   type GetStepTools,
 } from "@local";
 import { type createStepTools } from "@local/components/InngestStepTools";
-import {
-  envKeys,
-  headerKeys,
-  type internalEvents,
-} from "@local/helpers/consts";
+import { envKeys, headerKeys, internalEvents } from "@local/helpers/consts";
 import { type IsAny } from "@local/helpers/types";
 import { type Logger } from "@local/middleware/logger";
 import { type SendEventResponse } from "@local/types";
@@ -519,6 +515,12 @@ describe("send", () => {
             { name: "bar", data: { bar: "" } },
           ]);
       });
+
+      test("disallows sending an internal event", () => {
+        const _fn = () =>
+          // @ts-expect-error Internal event
+          inngest.send({ name: internalEvents.FunctionFinished });
+      });
     });
   });
 });
@@ -769,7 +771,12 @@ describe("helper types", () => {
     type T0 = GetFunctionInput<typeof inngest>;
 
     test("returns event typing", () => {
-      type Expected = `${internalEvents.FunctionInvoked}` | "foo" | "bar";
+      type Expected =
+        | `${internalEvents.FunctionFailed}`
+        | `${internalEvents.FunctionFinished}`
+        | `${internalEvents.FunctionInvoked}`
+        | "foo"
+        | "bar";
       type Actual = T0["event"]["name"];
       assertType<IsEqual<Expected, Actual>>(true);
     });

packages/inngest/src/components/EventSchemas.ts
@@ -1,7 +1,12 @@
 import { type Simplify } from "type-fest";
+import { type internalEvents } from "../helpers/consts";
 import { type IsEmptyObject, type IsStringLiteral } from "../helpers/types";
 import type * as z from "../helpers/validators/zod";
-import { type EventPayload } from "../types";
+import {
+  type EventPayload,
+  type FailureEventPayload,
+  type FinishedEventPayload,
+} from "../types";
 
 /**
  * Declares the shape of an event schema we expect from the user. This may be
@@ -210,7 +215,12 @@ export type Combine<
  *
  * @public
  */
-export class EventSchemas<S extends Record<string, EventPayload>> {
+export class EventSchemas<
+  S extends Record<string, EventPayload> = {
+    [internalEvents.FunctionFailed]: FailureEventPayload;
+    [internalEvents.FunctionFinished]: FinishedEventPayload;
+  },
+> {
   /**
    * Use generated Inngest types to type events.
    */

packages/inngest/src/helpers/consts.ts
@@ -142,6 +142,7 @@ export enum internalEvents {
    */
   FunctionFailed = "inngest/function.failed",
   FunctionInvoked = "inngest/function.invoked",
+  FunctionFinished = "inngest/function.finished",
 }
 
 export const logPrefix = chalk.magenta.bold("[Inngest]");

commit e1940e434192d33b9444106353775063d258a660

Fix `GetEvents<>` helper returning internal events by default (#480)
## Summary
<!-- Succinctly describe your change, providing context, what you've
changed, and why. -->

As seen in #452, the `GetEvents<>` type helper returns user-defined
_and_ internal functions such as `inngest/function.finished`. This was
an unintentional change to the helper when adding internal events to the
typing in #426.

The most common use for this helper is to create shims over
`inngest.send()`. The inclusion of the internal events in this helper
type means that the type cannot be passed to `inngest.send()` without
wrapping it with `WithoutInternal<>`.

In this PR, we change `GetEvents<>` to now only return the user's events
by default. If a user wishes to access internal events too, they can
pass an extra generic parameter to do so:

```ts
type OnlyUserEvents = GetEvents<typeof inngest>; // compatible with `inngest.send()`
type AllEvents = GetEvents<typeof inngest, true>; // not compatible with `inngest.send()`
```

## Checklist
<!-- Tick these items off as you progress. -->
<!-- If an item isn't applicable, ideally please strikeout the item by
wrapping it in "~~"" and suffix it with "N/A My reason for skipping
this." -->
<!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->

- [x] Added a [docs PR](https://github.com/inngest/website) that
references this PR
- [x] Added unit/integration tests
- [x] Added changesets if applicable

## Related
<!-- A space for any related links, issues, or PRs. -->
<!-- Linear issues are autolinked. -->
<!-- e.g. - INN-123 -->
<!-- GitHub issues/PRs can be linked using shorthand. -->
<!-- e.g. "- inngest/inngest#123" -->
<!-- Feel free to remove this section if there are no applicable related
links.-->
- Caused by #426
- Fixes #452 
- Added docs in inngest/website#671

packages/inngest/src/components/Inngest.ts
@@ -18,7 +18,11 @@ import {
 } from "../helpers/env";
 import { fixEventKeyMissingSteps, prettyError } from "../helpers/errors";
 import { stringify } from "../helpers/strings";
-import { type ExclusiveKeys, type SendEventPayload } from "../helpers/types";
+import {
+  type ExclusiveKeys,
+  type SendEventPayload,
+  type WithoutInternal,
+} from "../helpers/types";
 import { DefaultLogger, ProxyLogger, type Logger } from "../middleware/logger";
 import {
   sendEventResponseSchema,
@@ -736,12 +740,15 @@ export type GetStepTools<
 export type GetFunctionInput<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   TInngest extends Inngest<any>,
-  TTrigger extends keyof GetEvents<TInngest> &
-    string = keyof GetEvents<TInngest> & string,
+  TTrigger extends keyof GetEvents<TInngest, true> & string = keyof GetEvents<
+    TInngest,
+    true
+  > &
+    string,
 > = Parameters<
   Handler<
     ClientOptionsFromInngest<TInngest>,
-    GetEvents<TInngest>,
+    GetEvents<TInngest, true>,
     TTrigger,
     ExtendWithMiddleware<
       [
@@ -810,24 +817,36 @@ export type GetFunctionOutputFromReferenceInngestFunction<
   : unknown;
 
 /**
- * A helper type to extract the inferred event schemas from a given Inngest
- * instance.
+ * When passed an Inngest client, will return all event types for that client.
  *
- * It's recommended to use this type instead of directly passing
- * schemas around, as it will ensure that extra properties such as `ts` and
- * `user` are always added.
+ * It's recommended to use this instead of directly reusing your event types, as
+ * Inngest will add extra properties and internal events such as `ts` and
+ * `inngest/function.finished`.
  *
  * @example
  * ```ts
+ * import { EventSchemas, Inngest, type GetEvents } from "inngest";
+ *
+ * export const inngest = new Inngest({
+ *   id: "example-app",
+ *   schemas: new EventSchemas().fromRecord<{
+ *     "app/user.created": { data: { userId: string } };
+ *   }>(),
+ * });
+ *
  * type Events = GetEvents<typeof inngest>;
+ * type AppUserCreated = Events["app/user.created"];
+ *
  * ```
  *
  * @public
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type GetEvents<TInngest extends Inngest<any>> = EventsFromOpts<
-  ClientOptionsFromInngest<TInngest>
->;
+export type GetEvents<
+  TInngest extends Inngest.Any,
+  TWithInternal extends boolean = false,
+> = TWithInternal extends true
+  ? EventsFromOpts<ClientOptionsFromInngest<TInngest>>
+  : WithoutInternal<EventsFromOpts<ClientOptionsFromInngest<TInngest>>>;
 
 /**
  * A helper type to extract the inferred options from a given Inngest instance.

packages/inngest/src/helpers/types.ts
@@ -34,6 +34,9 @@ export type SendEventPayload<Events extends Record<string, EventPayload>> =
     }[keyof WithoutInternal<Events>]
   >;
 
+/**
+ * @public
+ */
 export type WithoutInternal<T extends Record<string, EventPayload>> = {
   [K in keyof T as K extends `inngest/${string}` ? never : K]: T[K];
 };

packages/inngest/src/index.ts
@@ -36,6 +36,7 @@ export type {
   StrictUnion,
   StrictUnionHelper,
   UnionKeys,
+  WithoutInternal,
 } from "./helpers/types";
 export { ProxyLogger } from "./middleware/logger";
 export type { LogArg } from "./middleware/logger";

packages/inngest/src/types.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 import { type EventSchemas } from "./components/EventSchemas";
 import {
   type EventsFromOpts,
-  type Inngest,
   type builtInMiddleware,
 } from "./components/Inngest";
 import { type InngestFunction } from "./components/InngestFunction";
@@ -22,35 +21,6 @@ import {
   type StrictUnion,
 } from "./helpers/types";
 import { type Logger } from "./middleware/logger";
-
-/**
- * When passed an Inngest client, will return all event types for that client.
- *
- * It's recommended to use this instead of directly reusing your event types, as
- * Inngest will add extra properties and internal events such as `ts` and
- * `inngest/function.failed`.
- *
- * @example
- * ```ts
- * import { EventSchemas, Inngest, type GetEvents } from "inngest";
- *
- * export const inngest = new Inngest({
- *   id: "example-app",
- *   schemas: new EventSchemas().fromRecord<{
- *     "app/user.created": { data: { userId: string } };
- *   }>(),
- * });
- *
- * type Events = GetEvents<typeof inngest>;
- * type AppUserCreated = Events["app/user.created"];
- *
- * ```
- *
- * @public
- */
-export type GetEvents<T extends Inngest.Any> = T extends Inngest<infer U>
-  ? EventsFromOpts<U>
-  : never;
 
 export const failureEventErrorSchema = z
   .object({

packages/inngest/src/components/EventSchemas.test.ts
@@ -8,7 +8,8 @@ import { z } from "zod";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Schemas<T extends EventSchemas<any>> = GetEvents<
-  Inngest<{ id: "test"; schemas: T }>
+  Inngest<{ id: "test"; schemas: T }>,
+  true
 >;
 
 describe("EventSchemas", () => {

packages/inngest/src/components/Inngest.test.ts
@@ -3,6 +3,7 @@ import {
   Inngest,
   InngestMiddleware,
   type EventPayload,
+  type GetEvents,
   type GetFunctionInput,
   type GetStepTools,
 } from "@local";
@@ -784,6 +785,17 @@ describe("helper types", () => {
     T,
     K extends string | number | symbol,
   > = T extends Record<K, infer U> ? U : never;
+
+  describe("type GetEvents", () => {
+    test("can use GetEvents to send an event", () => {
+      type T0 = GetEvents<typeof inngest>;
+      type T1 = T0[keyof T0];
+
+      const _myEventSendingFn = (events: T1[]) => {
+        void inngest.send(events);
+      };
+    });
+  });
 
   describe("type GetFunctionInput", () => {
     type T0 = GetFunctionInput<typeof inngest>;