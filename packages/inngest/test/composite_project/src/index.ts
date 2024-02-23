6782fa264f12748e734637a5faa0249007e1d655
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-02-23 20:13:46 +0000

    Use many pieces of the `inngest` package in the composite test

--- packages/inngest/test/composite_project/src/index.ts
+++ packages/inngest/test/composite_project/src/index.ts
@@ -1,9 +1,13 @@
-import { Inngest, InngestMiddleware } from "inngest";
+import * as Inngest from "inngest";
 
-export const inngest = new Inngest({
+export const inngest = new Inngest.Inngest({
   id: "me",
+  schemas: new Inngest.EventSchemas().fromRecord<{
+    foo: { data: { foo: string } };
+    bar: { data: { bar: string } };
+  }>(),
   middleware: [
-    new InngestMiddleware({
+    new Inngest.InngestMiddleware({
       name: "foo",
       init() {
         return {
@@ -13,12 +17,34 @@ export const inngest = new Inngest({
             return {
               transformInput(ctx) {
                 console.log(ctx);
-
-                return {
-                  ctx: {
-                    oof: "doof",
-                  },
-                };
+              },
+              afterExecution() {
+                console.log("afterExecution");
+              },
+              afterMemoization() {
+                console.log("afterMemoization");
+              },
+              beforeExecution() {
+                console.log("beforeExecution");
+              },
+              beforeMemoization() {
+                console.log("beforeMemoization");
+              },
+              beforeResponse() {
+                console.log("beforeResponse");
+              },
+              transformOutput(ctx) {
+                console.log(ctx);
+              },
+            };
+          },
+          onSendEvent() {
+            return {
+              transformInput(ctx) {
+                console.log(ctx);
+              },
+              transformOutput(ctx) {
+                console.log(ctx);
               },
             };
           },
@@ -26,4 +52,11 @@ export const inngest = new Inngest({
       },
     }),
   ],
+});
+
+void inngest.send({ name: "foo", data: { foo: "bar" } });
+
+inngest.createFunction({ id: "my-fn" }, { event: "foo" }, async (ctx) => {
+  console.log(ctx);
+  return { foo: "bar" };
 });
