76187c1f8afc41f2e49edd94b893600f30d7beb0
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-03-07 01:22:19 +0000

    Add tests for #98

--- packages/inngest/src/helpers/jsonify.test.ts
+++ packages/inngest/src/helpers/jsonify.test.ts
@@ -8,21 +8,21 @@ describe("Jsonify", () => {
     test("allows `any`", () => {
       type Actual = Jsonify<any>;
       assertType<IsAny<Actual>>(true);
-  });
+    });
 
-  test("allows `unknown`", () => {
-    type Actual = Jsonify<unknown>;
-    assertType<IsUnknown<Actual>>(true);
-  });
+    test("allows `unknown`", () => {
+      type Actual = Jsonify<unknown>;
+      assertType<IsUnknown<Actual>>(true);
+    });
 
-  test("allows number literals", () => {
-    type Actual = Jsonify<1>;
-    type Expected = 1;
-    assertType<IsEqual<Actual, Expected>>(true);
-  });
+    test("allows number literals", () => {
+      type Actual = Jsonify<1>;
+      type Expected = 1;
+      assertType<IsEqual<Actual, Expected>>(true);
+    });
 
-  test("allows string literals", () => {
-    type Actual = Jsonify<"foo">;
+    test("allows string literals", () => {
+      type Actual = Jsonify<"foo">;
       type Expected = "foo";
       assertType<IsEqual<Actual, Expected>>(true);
     });
@@ -67,6 +67,39 @@ describe("Jsonify", () => {
       type Expected = { foo: string | null };
       assertType<IsAny<Actual["foo"]>>(false);
       assertType<IsEqual<Actual, Expected>>(true);
+    });
+  });
+
+  describe("#98", () => {
+    test("allows mapped types with overrides when unnested", () => {
+      interface Foo {
+        [x: string]: any;
+        foo: boolean;
+      }
+
+      type Actual = Jsonify<Foo>;
+      type Expected = {
+        [x: string]: any;
+        foo: boolean;
+      };
+
+      assertType<IsEqual<Actual, Expected>>(true);
+      assertType<IsEqual<Actual["foo"], boolean>>(true);
+      assertType<IsAny<Actual["bar"]>>(true);
+    });
+
+    test("allows mapped types with overrides when nested", () => {
+      interface Foo {
+        [x: string]: any;
+        foo: boolean;
+      }
+
+      type Actual = Jsonify<{ foo: Foo }>;
+      type Expected = { foo: { [x: string]: any; foo: boolean } };
+
+      assertType<IsEqual<Actual, Expected>>(true);
+      assertType<IsEqual<Actual["foo"]["foo"], boolean>>(true);
+      assertType<IsAny<Actual["foo"]["bar"]>>(true);
     });
   });
 });
