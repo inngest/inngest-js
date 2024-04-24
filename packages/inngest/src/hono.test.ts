bd4fd65b9653435eb734ed4f08ee5e34031baa0f
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-04-24 16:32:09 +0000

    Fix `inngest/hono` unit tests

--- packages/inngest/src/hono.test.ts
+++ packages/inngest/src/hono.test.ts
@@ -1,18 +1,34 @@
 import * as HonoHandler from "@local/hono";
+import { Response } from "cross-fetch";
 import { testFramework } from "./test/helpers";
 
 testFramework("Hono", HonoHandler, {
   transformReq: (req) => {
     const c = {
       req: {
-        url: req.url,
+        // in practice, this is an absolute URL
+        url: new URL(`https://${req.headers["host"]}${req.url}`).href,
         query: (key: string) =>
           new URLSearchParams(req.url.split("?")[1] || "").get(key),
         header: (key: string) => req.headers[key] as string,
         method: req.method,
-        json: Promise.resolve(req.body),
+        json: () => Promise.resolve(req.body),
       },
+      body: (data: BodyInit, init: ResponseInit) => new Response(data, init),
     };
     return [c];
+  },
+  transformRes: async (_args, ret: Response) => {
+    const headers: Record<string, string> = {};
+
+    ret.headers.forEach((v, k) => {
+      headers[k] = v;
+    });
+
+    return {
+      status: ret.status,
+      body: await ret.text(),
+      headers,
+    };
   },
 });
