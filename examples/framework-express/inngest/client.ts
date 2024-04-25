11c1c422f1ae2feedf10f7710fdabb02df9537df
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-04-25 22:06:51 +0000

    Rename app in `examples/framework-express`

--- examples/framework-express/inngest/client.ts
+++ examples/framework-express/inngest/client.ts
@@ -1,4 +1,4 @@
 import { Inngest } from "inngest";
 import { schemas } from "./types";
 
-export const inngest = new Inngest({ id: "my-fastify-app", schemas });
+export const inngest = new Inngest({ id: "my-express-app", schemas });
