02be8f4cacfb98e0f6fcd116d05f6f57459ec055
Jack Williams <jack@inngest.com>
2024-05-15 14:56:24 +0100

    Add scrappy benchmarking (#559)
    ## Summary
    <!-- Succinctly describe your change, providing context, what you've
    changed, and why. -->
    
    Adds a rough basis for benchmarking pieces of the SDK.
    
    ## Checklist
    <!-- Tick these items off as you progress. -->
    <!-- If an item isn't applicable, ideally please strikeout the item by
    wrapping it in "~~"" and suffix it with "N/A My reason for skipping
    this." -->
    <!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->
    
    - [ ] ~Added a [docs PR](https://github.com/inngest/website) that
    references this PR~ N/A
    - [ ] ~Added unit/integration tests~ N/A
    - [x] Added changesets if applicable

---
+++ packages/inngest/test/benchmark/util.ts
@@ -0,0 +1,61 @@
+import callsites from "callsites";
+import { bench } from "mitata";
+import path from "node:path";
+
+interface BenchmarkRef {
+  name: string;
+  fn: () => Promise<any>;
+}
+
+class BenchmarkRefs {
+  constructor(private refs: BenchmarkRef[]) {}
+
+  public register() {
+    for (const ref of this.refs) {
+      bench(ref.name, ref.fn);
+    }
+  }
+}
+
+export const loadBenchmarks = async (
+  benchmarkNames: string[]
+): Promise<BenchmarkRefs> => {
+  const caller = getCallerDir();
+  const rawBenchmarkPromises: Promise<BenchmarkRef | BenchmarkRef[]>[] = [];
+
+  for (const benchmark of benchmarkNames) {
+    rawBenchmarkPromises.push(
+      (async () => {
+        const p = path.resolve(caller, `./${benchmark}`);
+        const fn = (await import(p)).default;
+
+        if (typeof fn === "function") {
+          return { name: benchmark, fn };
+        }
+
+        // Blindly assume it's an obj
+        const benchmarkObj = fn as Record<string, () => Promise<any>>;
+
+        return Object.entries(benchmarkObj).map<BenchmarkRef>(
+          ([key, value]) => {
+            return { name: `${benchmark}/${key}`, fn: value };
+          }
+        );
+      })()
+    );
+  }
+
+  const benchmarks = (await Promise.all(rawBenchmarkPromises)).flat();
+
+  return new BenchmarkRefs(benchmarks);
+};
+
+const getCallerDir = () => {
+  const stack = callsites();
+  let targetStack = stack[2];
+  if ("Bun" in globalThis) {
+    targetStack = stack[3];
+  }
+
+  return targetStack ? path.dirname(targetStack.getFileName()) : undefined;
+};
