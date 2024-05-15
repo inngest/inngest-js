6fe3c7d480b3da904dbd37997ed3ac614be615c7
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-05-15 16:51:48 +0000

    Rewrite `"inngest/hono"` `url` adapter to handle many URL variants

--- packages/inngest/src/hono.ts
+++ packages/inngest/src/hono.ts
@@ -36,7 +36,47 @@ export const serve = (options: ServeHandlerOptions) => {
         transformResponse: ({ headers, status, body }) => {
           return c.body(body, { headers, status });
         },
-        url: () => new URL(c.req.url, c.req.header("host")),
+        url: () => {
+          try {
+            // If this is an absolute URL, use it right now.
+            return new URL(c.req.url);
+          } catch {
+            // no-op
+          }
+
+          // We now know that `c.req.url` is a relative URL, so let's try
+          // to build a base URL to pair it with.
+          const host = options.serveHost || c.req.header("host");
+          if (!host) {
+            throw new Error(
+              "No host header found in request and no `serveHost` given either."
+            );
+          }
+
+          let baseUrl = host;
+          // Only set the scheme if we don't already have one, as a user may
+          // have specified the protocol in `serveHost` as a way to force it
+          // in their environment, e.g. for testing.
+          if (!baseUrl.includes("://")) {
+            let scheme: "http" | "https" = "https";
+            try {
+              // If we're in dev, assume `http` instead. Not that we directly
+              // access the environment instead of using any helpers here to
+              // ensure compatibility with tools with Webpack which will replace
+              // this with a literal.
+              // eslint-disable-next-line @inngest/internal/process-warn
+              if (process.env.NODE_ENV !== "production") {
+                scheme = "http";
+              }
+            } catch (err) {
+              // no-op
+            }
+
+            baseUrl = `${scheme}://${baseUrl}`;
+          }
+
+          return new URL(c.req.url, baseUrl);
+        },
         queryString: (key) => c.req.query(key),
         headers: (key) => c.req.header(key),
         method: () => c.req.method,
