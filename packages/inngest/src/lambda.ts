inngest/inngest-js

commit c51c3df373c2f6fbc3cf0276807bc7bb83db8f3f

Document all entrypoints with `@module` (#587)
## Summary
<!-- Succinctly describe your change, providing context, what you've
changed, and why. -->

Small tweaks to documented entrypoints in `inngest`.

## Checklist
<!-- Tick these items off as you progress. -->
<!-- If an item isn't applicable, ideally please strikeout the item by
wrapping it in "~~"" and suffix it with "N/A My reason for skipping
this." -->
<!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->

- [ ] ~Added a [docs PR](https://github.com/inngest/website) that
references this PR~ N/A
- [ ] ~Added unit/integration tests~ N/A
- [x] Added changesets if applicable

packages/inngest/src/lambda.ts
@@ -1,5 +1,32 @@
+/**
+ * An adapter for AWS Lambda to serve and register any declared functions with
+ * Inngest, making them available to be triggered by events.
+ *
+ * @example
+ *
+ * ```ts
+ * import { Inngest } from "inngest";
+ * import { serve } from "inngest/lambda";
+ *
+ * const inngest = new Inngest({ id: "my-lambda-app" });
+ *
+ * const fn = inngest.createFunction(
+ *   { id: "hello-world" },
+ *   { event: "test/hello.world" },
+ *   async ({ event }) => {
+ *    return "Hello World";
+ *  }
+ * );
+ *
+ * export const handler = serve({ client: inngest, functions: [fn] });
+ * ```
+ *
+ * @module
+ */
+
 import {
   type APIGatewayEvent,
+  type APIGatewayProxyEvent,
   type APIGatewayProxyEventV2,
   type APIGatewayProxyResult,
   type Context,
@@ -11,6 +38,10 @@ import {
 import { type Either } from "./helpers/types";
 import { type SupportedFrameworkName } from "./types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "aws-lambda";
 
 /**
@@ -38,12 +69,18 @@ export const frameworkName: SupportedFrameworkName = "aws-lambda";
  *
  * @public
  */
-export const serve = (options: ServeHandlerOptions) => {
+// Has explicit return type to avoid JSR-defined "slow types"
+export const serve = (
+  options: ServeHandlerOptions
+): ((
+  event: Either<APIGatewayProxyEvent, APIGatewayProxyEventV2>,
+  _context: Context
+) => Promise<APIGatewayProxyResult>) => {
   const handler = new InngestCommHandler({
     frameworkName,
     ...options,
     handler: (
-      event: Either<APIGatewayEvent, APIGatewayProxyEventV2>,
+      event: Either<APIGatewayProxyEvent, APIGatewayProxyEventV2>,
       _context: Context
     ) => {
       /**

packages/inngest/src/astro.ts
@@ -1,14 +1,33 @@
+/**
+ * An adapter for Astro to serve and register any declared functions with
+ * Inngest, making them available to be triggered by events.
+ *
+ * @example
+ * ```ts
+ * export const { GET, POST, PUT } = serve({
+ *   client: inngest,
+ *   functions: [fn1, fn2],
+ * });
+ * ```
+ *
+ * @module
+ */
+
 import {
   InngestCommHandler,
   type ServeHandlerOptions,
 } from "./components/InngestCommHandler";
 import { type SupportedFrameworkName } from "./types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "astro";
 
 /**
- * In Astro, serve and register any declared functions with Inngest, making
- * them available to be triggered by events.
+ * In Astro, serve and register any declared functions with Inngest, making them
+ * available to be triggered by events.
  *
  * @example
  * ```ts
@@ -20,7 +39,14 @@ export const frameworkName: SupportedFrameworkName = "astro";
  *
  * @public
  */
-export const serve = (options: ServeHandlerOptions) => {
+// Has explicit return type to avoid JSR-defined "slow types"
+export const serve = (
+  options: ServeHandlerOptions
+): ((ctx: { request: Request }) => Promise<Response>) & {
+  GET: (ctx: { request: Request }) => Promise<Response>;
+  POST: (ctx: { request: Request }) => Promise<Response>;
+  PUT: (ctx: { request: Request }) => Promise<Response>;
+} => {
   const commHandler = new InngestCommHandler({
     frameworkName,
     fetch: fetch.bind(globalThis),

packages/inngest/src/bun.ts
@@ -1,3 +1,29 @@
+/**
+ * An adapter for Bun to serve and register any declared functions with Inngest,
+ * making them available to be triggered by events.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/bun";
+ * import { functions, inngest } from "./inngest";
+ *
+ * Bun.serve({
+ *   port: 3000,
+ *   fetch(request: Request) {
+ *     const url = new URL(request.url);
+ *
+ *     if (url.pathname === "/api/inngest") {
+ *       return serve({ client: inngest, functions })(request);
+ *     }
+ *
+ *     return new Response("Not found", { status: 404 });
+ *   },
+ * });
+ * ```
+ *
+ * @module
+ */
+
 import {
   type InternalServeHandlerOptions,
   type ServeHandlerOptions,
@@ -5,15 +31,41 @@ import {
 import { serve as serveEdge } from "./edge";
 import { type SupportedFrameworkName } from "./types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "bun";
 
 /**
  * Using `Bun.serve()`, serve and register any declared functions with Inngest,
  * making them available to be triggered by events.
  *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/bun";
+ * import { functions, inngest } from "./inngest";
+ *
+ * Bun.serve({
+ *   port: 3000,
+ *   fetch(request: Request) {
+ *     const url = new URL(request.url);
+ *
+ *     if (url.pathname === "/api/inngest") {
+ *       return serve({ client: inngest, functions })(request);
+ *     }
+ *
+ *     return new Response("Not found", { status: 404 });
+ *   },
+ * });
+ * ```
+ *
  * @public
  */
-export const serve = (options: ServeHandlerOptions) => {
+// Has explicit return type to avoid JSR-defined "slow types"
+export const serve = (
+  options: ServeHandlerOptions
+): ((req: Request) => Promise<Response>) => {
   const optsOverrides: InternalServeHandlerOptions = {
     ...options,
     frameworkName,

packages/inngest/src/cloudflare.ts
@@ -1,18 +1,60 @@
+/**
+ * An adapter for Cloudflare Workers (and Workers on Pages) to serve and
+ * register any declared functions with Inngest, making them available to be
+ * triggered by events.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/cloudflare";
+ * import { inngest } from "../../inngest/client";
+ * import fnA from "../../inngest/fnA"; // Your own function
+ *
+ * export const onRequest = serve({
+ *   client: inngest,
+ *   functions: [fnA],
+ * });
+ * ```
+ *
+ * @module
+ */
+
 import {
   InngestCommHandler,
   type ServeHandlerOptions,
 } from "./components/InngestCommHandler";
 import { type SupportedFrameworkName } from "./types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "cloudflare-pages";
 
 /**
  * In Cloudflare, serve and register any declared functions with Inngest, making
  * them available to be triggered by events.
  *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/cloudflare";
+ * import { inngest } from "../../inngest/client";
+ * import fnA from "../../inngest/fnA"; // Your own function
+ *
+ * export const onRequest = serve({
+ *   client: inngest,
+ *   functions: [fnA],
+ * });
+ * ```
+ *
  * @public
  */
-export const serve = (options: ServeHandlerOptions) => {
+// Has explicit return type to avoid JSR-defined "slow types"
+export const serve = (
+  options: ServeHandlerOptions
+): ((ctx: {
+  request: Request;
+  env: Record<string, string | undefined>;
+}) => Promise<Response>) => {
   const handler = new InngestCommHandler({
     frameworkName,
 

packages/inngest/src/deno/fresh.ts
@@ -1,18 +1,56 @@
+/**
+ * An adapter for Deno's Fresh to serve and register any declared functions with
+ * Inngest, making them available to be triggered by events.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "https://esm.sh/inngest/deno/fresh";
+ * import { inngest } from "./src/inngest/client.ts";
+ * import fnA from "./src/inngest/fnA"; // Your own function
+ *
+ * export const handler = serve({
+ *   client: inngest,
+ *   functions: [fnA],
+ * });
+ * ```
+ *
+ * @module
+ */
+
 import {
   InngestCommHandler,
   type ServeHandlerOptions,
 } from "../components/InngestCommHandler";
 import { type SupportedFrameworkName } from "../types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "deno/fresh";
 
 /**
  * With Deno's Fresh framework, serve and register any declared functions with
  * Inngest, making them available to be triggered by events.
  *
+ * @example
+ * ```ts
+ * import { serve } from "https://esm.sh/inngest/deno/fresh";
+ * import { inngest } from "./src/inngest/client.ts";
+ * import fnA from "./src/inngest/fnA"; // Your own function
+ *
+ * export const handler = serve({
+ *   client: inngest,
+ *   functions: [fnA],
+ * });
+ * ```
+ *
  * @public
  */
-export const serve = (options: ServeHandlerOptions) => {
+// Has explicit return type to avoid JSR-defined "slow types"
+export const serve = (
+  options: ServeHandlerOptions
+): ((req: Request) => Promise<Response>) => {
   const handler = new InngestCommHandler({
     frameworkName,
     ...options,

packages/inngest/src/digitalocean.ts
@@ -1,5 +1,33 @@
+/**
+ * An adapter for DigitalOcean Functions to serve and register any declared
+ * functions with Inngest, making them available to be triggered by events.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/digitalocean";
+ * import { inngest } from "./src/inngest/client";
+ * import fnA from "./src/inngest/fnA"; // Your own function
+ *
+ * const main = serve({
+ *   client: inngest,
+ *   functions: [fnA],
+ *   // Your digitalocean hostname.  This is required otherwise your functions won't work.
+ *   serveHost: "https://faas-sfo3-your-url.doserverless.co",
+ *   // And your DO path, also required.
+ *   servePath: "/api/v1/web/fn-your-uuid/inngest",
+ * });
+ *
+ * // IMPORTANT: Makes the function available as a module in the project.
+ * // This is required for any functions that require external dependencies.
+ * module.exports.main = main;
+ * ```
+ *
+ * @module
+ */
+
 import {
   InngestCommHandler,
+  type ActionResponse,
   type ServeHandlerOptions,
 } from "./components/InngestCommHandler";
 import { type SupportedFrameworkName } from "./types";
@@ -18,12 +46,43 @@ type Main =
     }
   | undefined;
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "digitalocean";
 
+/**
+ * In DigitalOcean Functions, serve and register any declared functions with
+ * Inngest, making them available to be triggered by events.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/digitalocean";
+ * import { inngest } from "./src/inngest/client";
+ * import fnA from "./src/inngest/fnA"; // Your own function
+ *
+ * const main = serve({
+ *   client: inngest,
+ *   functions: [fnA],
+ *   // Your digitalocean hostname.  This is required otherwise your functions won't work.
+ *   serveHost: "https://faas-sfo3-your-url.doserverless.co",
+ *   // And your DO path, also required.
+ *   servePath: "/api/v1/web/fn-your-uuid/inngest",
+ * });
+ *
+ * // IMPORTANT: Makes the function available as a module in the project.
+ * // This is required for any functions that require external dependencies.
+ * module.exports.main = main;
+ * ```
+ *
+ * @public
+ */
+// Has explicit return type to avoid JSR-defined "slow types"
 export const serve = (
   options: ServeHandlerOptions &
     Required<Pick<NonNullable<ServeHandlerOptions>, "serveHost">>
-) => {
+): ((main?: Main) => Promise<ActionResponse<string>>) => {
   const handler = new InngestCommHandler({
     frameworkName,
     ...options,

packages/inngest/src/edge.ts
@@ -1,9 +1,31 @@
+/**
+ * An adapter for any request that handles standard Web APIs such as `fetch`,
+ * `Request,` and `Response` to serve and register any declared functions with
+ * Inngest, making them available to be triggered by events.
+ *
+ * This is reused by many other adapters, but can be used directly.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/edge";
+ * import functions from "~/inngest";
+ *
+ * export const handler = serve({ id: "my-edge-app", functions });
+ * ```
+ *
+ * @module
+ */
+
 import {
   InngestCommHandler,
   type ServeHandlerOptions,
 } from "./components/InngestCommHandler";
 import { type SupportedFrameworkName } from "./types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "edge";
 
 /**
@@ -21,8 +43,13 @@ export const frameworkName: SupportedFrameworkName = "edge";
  *
  * export const handler = serve({ id: "my-edge-app", functions });
  * ```
+ *
+ * @public
  */
-export const serve = (options: ServeHandlerOptions) => {
+// Has explicit return type to avoid JSR-defined "slow types"
+export const serve = (
+  options: ServeHandlerOptions
+): ((req: Request) => Promise<Response>) => {
   const handler = new InngestCommHandler({
     frameworkName,
     fetch: fetch.bind(globalThis),

packages/inngest/src/express.ts
@@ -1,3 +1,25 @@
+/**
+ * An adapter for Express to serve and register any declared functions with
+ * Inngest, making them available to be triggered by events.
+ *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/express";
+ * import { inngest } from "./src/inngest/client";
+ * import fnA from "./src/inngest/fnA"; // Your own function
+ *
+ * // Important:  ensure you add JSON middleware to process incoming JSON POST payloads.
+ * app.use(express.json());
+ * app.use(
+ *   // Expose the middleware on our recommended path at `/api/inngest`.
+ *   "/api/inngest",
+ *   serve({ client: inngest, functions: [fnA] })
+ * );
+ * ```
+ *
+ * @module
+ */
+
 import { type VercelRequest, type VercelResponse } from "@vercel/node";
 import { type Request, type Response } from "express";
 import {
@@ -7,6 +29,10 @@ import {
 import { type Either } from "./helpers/types";
 import { type SupportedFrameworkName } from "./types";
 
+/**
+ * The name of the framework, used to identify the framework in Inngest
+ * dashboards and during testing.
+ */
 export const frameworkName: SupportedFrameworkName = "express";
 
 /**
@@ -17,8 +43,24 @@ export const frameworkName: SupportedFrameworkName = "express";
  * between the `express` and `vercel` packages. This may change in the future to
  * appropriately infer.
  *
+ * @example
+ * ```ts
+ * import { serve } from "inngest/express";
+ * import { inngest } from "./src/inngest/client";
+ * import fnA from "./src/inngest/fnA"; // Your own function
+ *
+ * // Important:  ensure you add JSON middleware to process incoming JSON POST payloads.
+ * app.use(express.json());
+ * app.use(
+ *   // Expose the middleware on our recommended path at `/api/inngest`.
+ *   "/api/inngest",
+ *   serve({ client: inngest, functions: [fnA] })
+ * );
+ * ```
+ *
  * @public
  */
+// Has explicit return type to avoid JSR-defined "slow types"
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const serve = (options: ServeHandlerOptions): any => {
   const handler = new InngestCommHandler({

commit 41b02b228c51ce70c2be3a67f058483b3519abf0

Fix typing for `"inngest/lambda"` handler being incorrect Proxy type (#594)
## Summary
<!-- Succinctly describe your change, providing context, what you've
changed, and why. -->

Fix incorrect type being used for `"inngest/lambda"` handler following
#587.

## Checklist
<!-- Tick these items off as you progress. -->
<!-- If an item isn't applicable, ideally please strikeout the item by
wrapping it in "~~"" and suffix it with "N/A My reason for skipping
this." -->
<!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->

- [ ] ~Added a [docs PR](https://github.com/inngest/website) that
references this PR~ N/A
- [ ] ~Added unit/integration tests~ N/A
- [x] Added changesets if applicable

## Related
<!-- A space for any related links, issues, or PRs. -->
<!-- Linear issues are autolinked. -->
<!-- e.g. - INN-123 -->
<!-- GitHub issues/PRs can be linked using shorthand. -->
<!-- e.g. "- inngest/inngest#123" -->
<!-- Feel free to remove this section if there are no applicable related
links.-->
- Caused by #587

packages/inngest/src/lambda.ts
@@ -26,7 +26,6 @@
 
 import {
   type APIGatewayEvent,
-  type APIGatewayProxyEvent,
   type APIGatewayProxyEventV2,
   type APIGatewayProxyResult,
   type Context,
@@ -73,14 +72,14 @@ export const frameworkName: SupportedFrameworkName = "aws-lambda";
 export const serve = (
   options: ServeHandlerOptions
 ): ((
-  event: Either<APIGatewayProxyEvent, APIGatewayProxyEventV2>,
+  event: Either<APIGatewayEvent, APIGatewayProxyEventV2>,
   _context: Context
 ) => Promise<APIGatewayProxyResult>) => {
   const handler = new InngestCommHandler({
     frameworkName,
     ...options,
     handler: (
-      event: Either<APIGatewayProxyEvent, APIGatewayProxyEventV2>,
+      event: Either<APIGatewayEvent, APIGatewayProxyEventV2>,
       _context: Context
     ) => {
       /**