2019fe218bba7e82c85622d9b90b7eebaa305488
Aaron Harper <aaron@inngest.com>
2024-08-21 12:21:09 -0400

    Fix empty response body when streaming (#682)
    ## Summary
    Fix a bug where streaming would send back an empty body. This happened
    because we stringified an unawaited `Promise` within `createStream`
    
    ## Checklist
    <!-- Tick these items off as you progress. -->
    <!-- If an item isn't applicable, ideally please strikeout the item by
    wrapping it in "~~"" and suffix it with "N/A My reason for skipping
    this." -->
    <!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->
    
    - [ ] ~Added a [docs PR](https://github.com/inngest/website) that
    references this PR~ N/A Bug fix
    - [x] Added unit/integration tests
    - [x] Added changesets if applicable
    
    ---------
    
    Co-authored-by: Jack Williams <1736957+jpwilliams@users.noreply.github.com>

--- examples/framework-nextjs-app-router/src/app/api/inngest/route.ts
+++ examples/framework-nextjs-app-router/src/app/api/inngest/route.ts
@@ -1,6 +1,14 @@
 import { functions, inngest } from "@/inngest";
 import { serve } from "inngest/next";
 
+/**
+ * Try to automatically choose the edge runtime if `INNGEST_STREAMING` is set.
+ *
+ * See https://innge.st/streaming.
+ */
+export const runtime =
+  process.env.INNGEST_STREAMING?.toLowerCase() === "force" ? "edge" : "nodejs";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions,
