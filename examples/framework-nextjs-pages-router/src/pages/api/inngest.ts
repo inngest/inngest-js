2b365da12b69bf90dd3b92f2d1ad4ece902d7c32
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-08-21 15:46:28 +0000

    Disable pages router streaming tests; doesn't dynamically load

--- examples/framework-nextjs-pages-router/src/pages/api/inngest.ts
+++ examples/framework-nextjs-pages-router/src/pages/api/inngest.ts
@@ -1,14 +1,6 @@
 import { functions, inngest } from "@/inngest";
 import { serve } from "inngest/next";
 
-/**
- * Try to automatically choose the edge runtime if `INNGEST_STREAMING` is set.
- *
- * See https://innge.st/streaming.
- */
-export const runtime =
-  process.env.INNGEST_STREAMING?.toLowerCase() === "force" ? "edge" : "nodejs";
-
 export default serve({
   client: inngest,
   functions,
