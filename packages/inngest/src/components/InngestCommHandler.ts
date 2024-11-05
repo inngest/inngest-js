aff2a3c96e1037184c5daa7aae2714b2ac5ab0c0
Jack Williams <jack@inngest.com>
2024-11-05 18:47:03 +0000

    If response signing fails when successfully validated req, throw (#737)
    ## Summary
    <!-- Succinctly describe your change, providing context, what you've
    changed, and why. -->
    
    If the request is signed, we expect that the response will also be
    signed. This PR ensures that if signing the response fails, we throw a
    `500 Internal Server Error` instead of silently failing.
    
    This is to address issues where trust probes are performed but the
    response is not signed.
    
    ## Checklist
    <!-- Tick these items off as you progress. -->
    <!-- If an item isn't applicable, ideally please strikeout the item by
    wrapping it in "~~"" and suffix it with "N/A My reason for skipping
    this." -->
    <!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->
    
    - [ ] ~Added a [docs PR](https://github.com/inngest/website) that
    references this PR~ N/A Internal
    - [x] Added unit/integration tests
    - [x] Added changesets if applicable

--- packages/inngest/src/components/InngestCommHandler.ts
+++ packages/inngest/src/components/InngestCommHandler.ts
@@ -837,35 +837,46 @@ export class InngestCommHandler<
       const prepareActionRes = async (
         res: ActionResponse
       ): Promise<ActionResponse> => {
-        const signature = await signatureValidation
-          .then((result) => {
+        const headers: Record<string, string> = {
+          ...getInngestHeaders(),
+          ...(await headersToForwardP),
+          ...res.headers,
+          ...(res.version === null
+            ? {}
+            : {
+                [headerKeys.RequestVersion]: (
+                  res.version ?? PREFERRED_EXECUTION_VERSION
+                ).toString(),
+              }),
+        };
+
+        let signature: string | undefined;
+
+        try {
+          signature = await signatureValidation.then((result) => {
             if (!result.success || !result.keyUsed) {
               return undefined;
             }
 
             return this.getResponseSignature(result.keyUsed, res.body);
-          })
-          .catch((_err) => {
-            // no-op; only sign responses if we have successfully validated the
-            // request
-            return undefined;
           });
+        } catch (err) {
+          // If we fail to sign, retun a 500 with the error.
+          return {
+            ...res,
+            headers,
+            body: stringify(serializeError(err)),
+            status: 500,
+          };
+        }
+
+        if (signature) {
+          headers[headerKeys.Signature] = signature;
+        }
 
         return {
           ...res,
-          headers: {
-            ...getInngestHeaders(),
-            ...(await headersToForwardP),
-            ...res.headers,
-            ...(res.version === null
-              ? {}
-              : {
-                  [headerKeys.RequestVersion]: (
-                    res.version ?? PREFERRED_EXECUTION_VERSION
-                  ).toString(),
-                }),
-            ...(signature ? { [headerKeys.Signature]: signature } : {}),
-          },
+          headers,
         };
       };
 
