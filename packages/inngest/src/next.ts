2ffb72d369ef62ed94086095d103307dc0093154
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-11-12 12:53:08 +0000

    Fix `next` JSR export
    We don't require this because we expect packages to also have ESM exports
    declared in their `package.json`, but `next` does not do this, so we must
    explicitly use the extension still.
    
    Annoyingly, a dry run publish with `jsr` doesn't reveal this.

--- packages/inngest/src/next.ts
+++ packages/inngest/src/next.ts
@@ -21,7 +21,7 @@
  */
 
 import { type NextApiRequest, type NextApiResponse } from "next";
-import { type NextRequest } from "next/server";
+import { type NextRequest } from "next/server.js";
 import {
   InngestCommHandler,
   type ServeHandlerOptions,
