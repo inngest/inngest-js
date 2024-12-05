af66ad5552dc93d41756ab3b913ceafb72739f77
Charly Poly <1252066+charlypoly@users.noreply.github.com>
2024-12-05 19:17:28 +0100

    feat(ai): support for OpenAI `o1` models (#768)
    Co-authored-by: Jack Williams <jack@inngest.com>

---
+++ .changeset/breezy-hotels-happen.md
@@ -0,0 +1,5 @@
+---
+"inngest": patch
+---
+
+Add `o1-preview` and `o1-mini` to possible OpenAI models
--- packages/inngest/src/components/ai/models/openai.ts
+++ packages/inngest/src/components/ai/models/openai.ts
@@ -45,6 +45,8 @@ export namespace OpenAi {
     | "chatgpt-4o-latest"
     | "gpt-4o-mini"
     | "gpt-4"
+    | "o1-preview"
+    | "o1-mini"
     | "gpt-3.5-turbo";
 
   /**
