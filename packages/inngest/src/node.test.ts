e0def78d2708252733adae182052d2be4a49859f
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-12-20 16:09:11 -0500

    Add vanilla Node.js handler

---
+++ packages/inngest/src/node.test.ts
@@ -0,0 +1,38 @@
+import http from "node:http";
+import { Socket } from "node:net";
+import { Buffer } from "node:buffer";
+import * as NodeHandler from "@local/node";
+import { testFramework } from "./test/helpers";
+
+testFramework("Node", NodeHandler, {
+  transformReq: (req, res) => {
+    const socket = new Socket();
+    const nodeReq = new http.IncomingMessage(socket);
+
+    // Set the method and URL
+    nodeReq.method = req.method;
+    nodeReq.url = req.url;
+
+    if (req.protocol === "https") {
+      nodeReq.headers["x-forwarded-proto"] = req.protocol;
+    }
+
+    // Set headers
+    for (const [key, value] of Object.entries(req.headers)) {
+      nodeReq.headers[key.toLowerCase()] = value;
+    }
+
+    // Mock the body data
+    const bodyData = Buffer.from(JSON.stringify(req.body));
+
+    nodeReq.on("end", () => {
+      console.log("END!");
+    });
+
+    // Override the read methods to return the body data
+    nodeReq.push(bodyData);
+    nodeReq.push(null); // Signals the end of the stream
+
+    return [nodeReq, res];
+  },
+});
