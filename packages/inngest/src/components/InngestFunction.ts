cdaf81817e6af27c884dc779c18a830cce9e333d
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-01-15 11:52:04 +0000

    Allow typings for `InngestFunction["client"]`

--- packages/inngest/src/components/InngestFunction.ts
+++ packages/inngest/src/components/InngestFunction.ts
@@ -53,7 +53,7 @@ export class InngestFunction<
   public readonly opts: TFnOpts;
   private readonly fn: THandler;
   private readonly onFailureFn?: TFailureHandler;
-  private readonly client: TClient;
+  protected readonly client: TClient;
   private readonly middleware: Promise<MiddlewareRegisterReturn[]>;
 
   /**
