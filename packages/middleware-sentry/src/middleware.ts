2a83b18261628e8a1ab3221ea84f9ff8b65c0b9a
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-01-16 21:19:14 +0000

    Add ESLint to `@inngest/middleware-sentry`

--- packages/middleware-sentry/src/middleware.ts
+++ packages/middleware-sentry/src/middleware.ts
@@ -65,7 +65,7 @@ export const sentryMiddleware = (
   /**
    * Options used to configure the Sentry middleware.
    */
-  opts?: SentryMiddlewareOptions
+  opts?: SentryMiddlewareOptions,
 ): SentryMiddleware => {
   const mw = new InngestMiddleware({
     name: "@inngest/middleware-sentry",
@@ -120,7 +120,7 @@ export const sentryMiddleware = (
                         },
                         (_memoSpan) => {
                           memoSpan = _memoSpan;
-                        }
+                        },
                       );
                     });
                   },
@@ -140,7 +140,7 @@ export const sentryMiddleware = (
                         },
                         (_execSpan) => {
                           execSpan = _execSpan;
-                        }
+                        },
                       );
                     });
                   },
@@ -184,7 +184,7 @@ export const sentryMiddleware = (
                     }
                   },
                 };
-              }
+              },
             );
           });
         },
