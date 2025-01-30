2253690561e557f8bcbc39cbf263cd1b6d779cee
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-01-30 17:12:48 -0500

    Add recommended env bindings middleware

--- examples/framework-hono/src/inngest/client.ts
+++ examples/framework-hono/src/inngest/client.ts
@@ -1,4 +1,9 @@
 import { Inngest } from "inngest";
+import { bindingsMiddleware } from "./middleware";
 import { schemas } from "./types";
 
-export const inngest = new Inngest({ id: "my-hono-app", schemas });
+export const inngest = new Inngest({
+  id: "my-hono-app",
+  schemas,
+  middleware: [bindingsMiddleware],
+});
