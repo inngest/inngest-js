ce828fd23d5461e1bfb47644aa6aac4c2fb1ca92
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-03-12 17:41:04 +0000

    Make space for options in `realtimeMiddleware()`

--- packages/realtime/src/middleware.ts
+++ packages/realtime/src/middleware.ts
@@ -3,63 +3,65 @@ import { type InngestApi } from "inngest/api/api";
 import { getAsyncCtx } from "inngest/experimental";
 import { type Realtime } from "./types";
 
-export const realtimeMiddleware = new InngestMiddleware({
-  name: "publish",
-  init({ client }) {
-    return {
-      onFunctionRun() {
-        return {
-          transformInput({ ctx: { step } }) {
-            const publish: Realtime.PublishFn = async (input) => {
-              const { topic, channel, data } = await input;
+export const realtimeMiddleware = () => {
+  return new InngestMiddleware({
+    name: "publish",
+    init({ client }) {
+      return {
+        onFunctionRun() {
+          return {
+            transformInput({ ctx: { step } }) {
+              const publish: Realtime.PublishFn = async (input) => {
+                const { topic, channel, data } = await input;
 
-              const store = await getAsyncCtx();
-              if (!store) {
-                throw new Error(
-                  "No ALS found, but is required for running `publish()`",
-                );
-              }
-
-              const subscription: InngestApi.Subscription = {
-                topics: [topic],
-                channel,
-              };
-
-              const action = async () => {
-                const result = await client["inngestApi"].publish(
-                  subscription,
-                  data,
-                );
-
-                if (!result.ok) {
+                const store = await getAsyncCtx();
+                if (!store) {
                   throw new Error(
-                    `Failed to publish event: ${result.error?.error}`,
+                    "No ALS found, but is required for running `publish()`",
                   );
                 }
+
+                const subscription: InngestApi.Subscription = {
+                  topics: [topic],
+                  channel,
+                };
+
+                const action = async () => {
+                  const result = await client["inngestApi"].publish(
+                    subscription,
+                    data,
+                  );
+
+                  if (!result.ok) {
+                    throw new Error(
+                      `Failed to publish event: ${result.error?.error}`,
+                    );
+                  }
+                };
+
+                return (
+                  store.executingStep
+                    ? action()
+                    : step.run(`publish:${subscription.channel}`, action)
+                ).then(() => {
+                  // Always return the data passed in to the `publish` call.
+
+                  return data;
+                });
               };
 
-              return (
-                store.executingStep
-                  ? action()
-                  : step.run(`publish:${subscription.channel}`, action)
-              ).then(() => {
-                // Always return the data passed in to the `publish` call.
-
-                return data;
-              });
-            };
-
-            return {
-              ctx: {
-                /**
-                 * TODO
-                 */
-                publish,
-              },
-            };
-          },
-        };
-      },
-    };
-  },
-});
+              return {
+                ctx: {
+                  /**
+                   * TODO
+                   */
+                  publish,
+                },
+              };
+            },
+          };
+        },
+      };
+    },
+  });
+};
