82cf822c2cf718f651ff1db503ebd825ca8223e7
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-03-14 19:44:49 +0000

    Example in `node`

--- examples/node/inngest/client.ts
+++ examples/node/inngest/client.ts
@@ -1,4 +1,9 @@
 import { Inngest } from "inngest";
+import { otlpMiddleware } from "inngest/experimental";
 import { schemas } from "./types";
 
-export const inngest = new Inngest({ id: "my-express-app", schemas });
+export const inngest = new Inngest({
+  id: "my-express-app",
+  schemas,
+  middleware: [otlpMiddleware()],
+});
--- examples/node/inngest/helloWorld.ts
+++ examples/node/inngest/helloWorld.ts
@@ -4,6 +4,8 @@ export default inngest.createFunction(
   { id: "hello-world" },
   { event: "demo/event.sent" },
   async ({ event, step }) => {
+    await fetch("http://localhost:3000/api/inngest");
+
     return {
       message: `Hello ${event.name}!`,
     };
