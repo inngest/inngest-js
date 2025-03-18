0c8544be255e10f384eef68e585db1633d4e40dd
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-03-18 13:36:55 +0000

    Solve span leaks and provide a method for pre-app tracing

--- packages/inngest/src/components/execution/otel.ts
+++ packages/inngest/src/components/execution/otel.ts
@@ -1,8 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import Debug from "debug";
+const baseDebug = Debug("inngest:otel");
+
+/**
+ * Attempt to import a variety of automatic instrumentations, swallowing errors.
+ *
+ * This allows us to automatically capture spans for a variety of common cases.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const importAttempts: (Instrumentation | Instrumentation[])[] = [
+  (() => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const {
+        getNodeAutoInstrumentations,
+      } = require("@opentelemetry/auto-ianstrumentations-node");
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+      return getNodeAutoInstrumentations();
+    } catch (err) {
+      baseDebug(
+        "failed to import @opentelemetry/auto-instrumentations-node",
+        err?.toString?.().split?.("\n")?.[0] ?? err
+      );
+    }
+  })(),
+].filter(Boolean);
+
 import { trace, type Span } from "@opentelemetry/api";
+import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import {
+  registerInstrumentations,
   type Instrumentation,
-  type InstrumentationConfig,
 } from "@opentelemetry/instrumentation";
 import {
   detectResourcesSync,
@@ -19,7 +49,6 @@ import {
   type ReadableSpan,
   type SpanProcessor,
 } from "@opentelemetry/sdk-trace-base";
-import Debug from "debug";
 import { envKeys } from "../../helpers/consts.js";
 import { processEnv } from "../../helpers/env.js";
 import { version } from "../../version.js";
@@ -27,14 +56,11 @@ import { type Inngest } from "../Inngest.js";
 import { InngestMiddleware } from "../InngestMiddleware.js";
 
 export type Behaviour = "createProvider" | "extendProvider" | "off" | "auto";
-export type Instrumentations = (
-  | Instrumentation<InstrumentationConfig>
-  | Instrumentation<InstrumentationConfig>[]
-)[];
+export type Instrumentations = (Instrumentation | Instrumentation[])[];
 
 let _resourceAttributes: IResource | undefined;
 
-const processorDebug = Debug("inngest:otel:InngestSpanProcessor");
+const processorDebug = baseDebug.extend("InngestSpanProcessor");
 
 /**
  * TODO
@@ -48,20 +74,40 @@ export const getClientProcessor = (client: Inngest.Any) => {
   return clientProcessorMap.get(client);
 };
 
+/**
+ * TODO
+ */
 export class InngestSpanProcessor implements SpanProcessor {
-  constructor(client: Inngest.Any) {
-    clientProcessorMap.set(client, this);
-  }
-
+  /**
+   * TODO
+   */
   #batcher: BatchSpanProcessor | undefined;
-  #resourceAttributes: IResource | undefined;
 
   /**
-   * A map of span IDs to the Inngest traceparent headers they belong to. This is
-   * used to track spans that we care about, so that we can export them to the
-   * OTel endpoint.
+   * A set of spans used to track spans that we care about, so that we can
+   * export them to the OTel endpoint.
+   *
+   * If a span falls out of reference, it will be removed from this set as we'll
+   * never get a chance to export it or remove it anyway.
    */
-  public allowed = new Map<string, string>();
+  #spansToExport = new WeakSet<Span>();
+
+  /**
+   * TODO
+   */
+  #traceParents = new Map<string, string>();
+
+  /**
+   * A registry used to clean up items from the `traceParents` map when spans
+   * fall out of reference. This is used to avoid memory leaks in the case where
+   * a span is not exported, remains unended, and is left in memory before being
+   * GC'd.
+   */
+  #spanCleanup = new FinalizationRegistry<string>((spanId) => {
+    if (spanId) {
+      this.#traceParents.delete(spanId);
+    }
+  });
 
   /**
    * TODO
@@ -77,11 +123,12 @@ export class InngestSpanProcessor implements SpanProcessor {
     );
 
     span.setAttributes(InngestSpanProcessor.resourceAttributes.attributes);
-    span.setAttribute("inngest.traceparent", traceparent);
-
-    this.allowed.set(span.spanContext().spanId, traceparent);
+    this.trackSpan(span, traceparent);
   }
 
+  /**
+   * TODO
+   */
   static get resourceAttributes(): IResource {
     if (!_resourceAttributes) {
       _resourceAttributes = detectResourcesSync({
@@ -134,6 +181,25 @@ export class InngestSpanProcessor implements SpanProcessor {
     return this.#batcher;
   }
 
+  private trackSpan(span: Span, traceparent: string): void {
+    const spanId = span.spanContext().spanId;
+
+    this.#spanCleanup.register(span, spanId, span);
+    this.#spansToExport.add(span);
+    this.#traceParents.set(spanId, traceparent);
+    span.setAttribute("inngest.traceparent", traceparent);
+  }
+
+  private cleanupSpan(span: Span): void {
+    const spanId = span.spanContext().spanId;
+
+    // This span is no longer in use, so we can remove it from the cleanup
+    // registry.
+    this.#spanCleanup.unregister(span);
+    this.#spansToExport.delete(span);
+    this.#traceParents.delete(spanId);
+  }
+
   onStart(span: Span): void {
     const debug = processorDebug.extend("onStart");
     const spanId = span.spanContext().spanId;
@@ -150,7 +216,7 @@ export class InngestSpanProcessor implements SpanProcessor {
       return;
     }
 
-    const traceparent = this.allowed.get(parentSpanId);
+    const traceparent = this.#traceParents.get(parentSpanId);
     if (traceparent) {
       // This span is a child of a span we care about, so add it to the list of
       // tracked spans so that we also capture its children
@@ -163,8 +229,7 @@ export class InngestSpanProcessor implements SpanProcessor {
         spanId
       );
 
-      this.allowed.set(spanId, traceparent);
-      span.setAttribute("inngest.traceparent", traceparent);
+      this.trackSpan(span, traceparent);
     }
   }
 
@@ -172,21 +237,16 @@ export class InngestSpanProcessor implements SpanProcessor {
     const debug = processorDebug.extend("onEnd");
     const spanId = span.spanContext().spanId;
 
-    if (this.allowed.has(spanId)) {
-      // This is a span that we care about, so make sure it gets exported by the
-      // batcher
-      debug("exporting span", spanId);
+    try {
+      if (this.#spansToExport.has(span as unknown as Span)) {
+        debug("exporting span", spanId);
+        return this.batcher.onEnd(span);
+      }
 
-      this.allowed.delete(spanId);
-
-      return this.batcher.onEnd(span);
+      debug("not exporting span", spanId, "as we don't care about it");
+    } finally {
+      this.cleanupSpan(span as unknown as Span);
     }
-
-    debug(
-      "not exporting span",
-      span.spanContext().spanId,
-      "as we don't care about it"
-    );
   }
 
   forceFlush(): Promise<void> {
@@ -210,75 +270,75 @@ export const otelMiddleware = ({
   behaviour?: Behaviour;
   instrumentations?: Instrumentations;
 } = {}) => {
-  const debug = Debug("inngest:otel:middleware");
+  const debug = baseDebug.extend("middleware");
   debug("behaviour:", behaviour);
+
+  let processor: InngestSpanProcessor | undefined;
+
+  switch (behaviour) {
+    case "auto": {
+      const extended = extendProvider(behaviour);
+      if (extended.success) {
+        debug("extended existing provider");
+        processor = extended.processor;
+        break;
+      }
+
+      const created = createProvider(behaviour, instrumentations);
+      if (created.success) {
+        debug("created new provider");
+        processor = created.processor;
+        break;
+      }
+
+      console.warn("no provider found to extend and unable to create one");
+
+      break;
+    }
+    case "createProvider": {
+      const created = createProvider(behaviour, instrumentations);
+      if (created.success) {
+        debug("created new provider");
+        processor = created.processor;
+        break;
+      }
+
+      console.warn("unable to create provider, OTel middleware will not work");
+
+      break;
+    }
+    case "extendProvider": {
+      const extended = extendProvider(behaviour);
+      if (extended.success) {
+        debug("extended existing provider");
+        processor = extended.processor;
+        break;
+      }
+
+      console.warn(
+        'unable to extend provider, OTel middleware will not work. Either allow the middleware to create a provider by setting `behaviour: "createProvider"` or `behaviour: "auto"`, or make sure that the provider is created and imported before the middleware is used.'
+      );
+
+      break;
+    }
+    case "off": {
+      break;
+    }
+    default: {
+      // unknown
+      console.warn(
+        `unknown behaviour ${JSON.stringify(behaviour)}, defaulting to "off"`
+      );
+    }
+  }
 
   return new InngestMiddleware({
     name: "Inngest: OTel",
-    async init({ client }) {
-      switch (behaviour) {
-        case "auto": {
-          const extended = extendProvider(client, behaviour);
-          if (extended) {
-            debug("extended existing provider");
-            break;
-          }
-
-          const created = await createProvider(
-            client,
-            behaviour,
-            instrumentations
-          );
-          if (created) {
-            debug("created new provider");
-            break;
-          }
-
-          console.warn("no provider found to extend and unable to create one");
-
-          break;
-        }
-        case "createProvider": {
-          const created = await createProvider(
-            client,
-            behaviour,
-            instrumentations
-          );
-          if (created) {
-            debug("created new provider");
-            break;
-          }
-
-          console.warn(
-            "unable to create provider, OTel middleware will not work"
-          );
-
-          break;
-        }
-        case "extendProvider": {
-          const extended = extendProvider(client, behaviour);
-          if (extended) {
-            debug("extended existing provider");
-            break;
-          }
-
-          console.warn(
-            'unable to extend provider, OTel middleware will not work. Either allow the middleware to create a provider by setting `behaviour: "createProvider"` or `behaviour: "auto"`, or make sure that the provider is created and imported before the middleware is used.'
-          );
-
-          break;
-        }
-        case "off": {
-          break;
-        }
-        default: {
-          // unknown
-          console.warn(
-            `unknown behaviour ${JSON.stringify(
-              behaviour
-            )}, defaulting to "off"`
-          );
-        }
+    init({ client }) {
+      // TODO Set client->processor weakmap here, from a processor that has been
+      // created either in create or extend provider.
+      if (processor) {
+        clientProcessorMap.set(client, processor);
       }
 
       return {
@@ -301,60 +361,40 @@ export const otelMiddleware = ({
   });
 };
 
-const createProvider = async (
-  app: Inngest.Any,
+const createProvider = (
   behaviour: Behaviour,
   instrumentations: Instrumentations | undefined = []
-): Promise<boolean> => {
+): { success: true; processor: InngestSpanProcessor } | { success: false } => {
   // TODO How do we tell if there's an existing provider?
-  const debug = Debug("inngest:otel:middleware:createProvider");
+  const processor = new InngestSpanProcessor();
 
   const p = new BasicTracerProvider({
-    spanProcessors: [new InngestSpanProcessor(app)],
+    spanProcessors: [processor],
   });
 
-  let contextManager;
+  const instrList: Instrumentations = [
+    ...instrumentations,
+    ...(importAttempts.filter(Boolean) as Instrumentations),
+  ];
 
-  try {
-    const { AsyncHooksContextManager } = await import(
-      "@opentelemetry/context-async-hooks"
-    );
-    // This is critical, otherwise we won't be able to track async spans
-    // correctly
-    contextManager = new AsyncHooksContextManager().enable();
-  } catch (_) {
-    // Not in Node, or package not installed — skip context manager
-    debug("error importing AsyncHooksContextManager, skipping");
-  }
-
-  try {
-    const { registerInstrumentations } = await import(
-      "@opentelemetry/instrumentation"
-    );
-    const { getNodeAutoInstrumentations } = await import(
-      "@opentelemetry/auto-instrumentations-node"
-    );
-
-    registerInstrumentations({
-      instrumentations: [getNodeAutoInstrumentations(), ...instrumentations],
-    });
-  } catch (_) {
-    // instrumentation is optional
-    debug("error importing automatic instrumentation, skipping");
-  }
+  registerInstrumentations({
+    instrumentations: instrList,
+  });
 
   p.register({
-    contextManager,
+    contextManager: new AsyncHooksContextManager().enable(),
   });
 
-  return true;
+  return { success: true, processor };
 };
 
 /**
  * Attempts to extend the existing OTel provider with our processor. Returns true
  * if the provider was extended, false if it was not.
  */
-const extendProvider = (app: Inngest.Any, behaviour: Behaviour): boolean => {
+const extendProvider = (
+  behaviour: Behaviour
+): { success: true; processor: InngestSpanProcessor } | { success: false } => {
   // Attempt to add our processor and export to the existing provider
   const existingProvider = trace.getTracerProvider();
   if (!existingProvider) {
@@ -364,7 +404,7 @@ const extendProvider = (app: Inngest.Any, behaviour: Behaviour): boolean => {
       );
     }
 
-    return false;
+    return { success: false };
   }
 
   if (
@@ -380,10 +420,11 @@ const extendProvider = (app: Inngest.Any, behaviour: Behaviour): boolean => {
       );
     }
 
-    return false;
+    return { success: false };
   }
 
-  existingProvider.addSpanProcessor(new InngestSpanProcessor(app));
+  const processor = new InngestSpanProcessor();
+  existingProvider.addSpanProcessor(processor);
 
-  return true;
+  return { success: true, processor };
 };
