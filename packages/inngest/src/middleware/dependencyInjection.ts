c5d5f52a8418570fa32f240756c7be4544dab5ac
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-03-25 12:29:10 +0000

    Fix inference

--- packages/inngest/src/middleware/dependencyInjection.ts
+++ packages/inngest/src/middleware/dependencyInjection.ts
@@ -4,12 +4,13 @@ import { InngestMiddleware } from "../components/InngestMiddleware.js";
  * Adds properties to the function input for every function created using this
  * app.
  */
-export const dependencyInjectionMiddleware = (
+// We can use `const` here yet due to TS constraints.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const dependencyInjectionMiddleware = <TCtx extends Record<string, any>>(
   /**
    * The context to inject into the function input.
    */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ctx: Record<string, any>
+  ctx: TCtx
 ) => {
   return new InngestMiddleware({
     name: "Inngest: Dependency Injection",
