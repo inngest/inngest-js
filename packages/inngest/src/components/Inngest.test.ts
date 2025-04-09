a80f2021d8c078c9b50932fa49ba559f856433f2
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-04-09 15:46:53 +0000

    Add test for idempotency seed

--- packages/inngest/src/components/Inngest.test.ts
+++ packages/inngest/src/components/Inngest.test.ts
@@ -474,6 +474,36 @@ describe("send", () => {
       );
     });
 
+    test("should use seed header for idempotency ID if none given", async () => {
+      const inngest = createClient({ id: "test" });
+      inngest.setEventKey(testEventKey);
+
+      const testEventWithoutId = {
+        name: "test.without.id",
+        data: {},
+      };
+
+      const mockedFetch = jest.mocked(global.fetch);
+
+      await expect(inngest.send(testEventWithoutId)).resolves.toMatchObject({
+        ids: Array(1).fill(expect.any(String)),
+      });
+
+      expect(mockedFetch).toHaveBeenCalledTimes(2); // 2nd for dev server check
+      expect(mockedFetch.mock.calls[1]).toHaveLength(2);
+
+      const reqHeaders = mockedFetch.mock.calls[1]?.[1]?.headers as Record<
+        string,
+        string
+      >;
+      expect(reqHeaders).toBeDefined();
+      expect(typeof reqHeaders).toBe("object");
+
+      expect(reqHeaders[headerKeys.EventIdSeed]).toBeDefined();
+      expect(typeof reqHeaders[headerKeys.EventIdSeed]).toBe("string");
+      expect(reqHeaders[headerKeys.EventIdSeed]).toBeTruthy();
+    });
+
     test("should allow middleware to mutate input", async () => {
       const inngest = createClient({
         id: "test",
