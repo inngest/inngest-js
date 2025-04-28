inngest/inngest-js

commit d18006dbffe7501b145f914992951439a9859261

Remove `inngest` from `@inngest/realtime` (#949)
## Summary
<!-- Succinctly describe your change, providing context, what you've
changed, and why. -->

Removes runtime `inngest` imports from `@inngest/realtime`, allowing us
to more safely import it in client-side code with more overzealous
bundlers like Next.js and Cloudflare Workers.

## Checklist
<!-- Tick these items off as you progress. -->
<!-- If an item isn't applicable, ideally please strikeout the item by
wrapping it in "~~"" and suffix it with "N/A My reason for skipping
this." -->
<!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->

- [x] Added a [docs PR](https://github.com/inngest/website) that
references this PR
- [x] Added unit/integration tests
- [x] Added changesets if applicable

## Related
<!-- A space for any related links, issues, or PRs. -->
<!-- Linear issues are autolinked. -->
<!-- e.g. - INN-123 -->
<!-- GitHub issues/PRs can be linked using shorthand. -->
<!-- e.g. "- inngest/inngest#123" -->
<!-- Feel free to remove this section if there are no applicable related
links.-->
- Closes #931 
- Fixes #939
- Documented in inngest/website#1101

packages/realtime/src/api.ts
@@ -1,0 +1,82 @@
+import { z } from "zod";
+import { fetchWithAuthFallback, getEnvVar, parseAsBoolean } from "./util";
+
+const tokenSchema = z.object({ jwt: z.string() });
+
+export const api = {
+  async getSubscriptionToken({
+    channel,
+    topics,
+    signingKey,
+    signingKeyFallback,
+    apiBaseUrl,
+  }: {
+    channel: string;
+    topics: string[];
+    signingKey: string;
+    signingKeyFallback: string | undefined;
+    apiBaseUrl: string | undefined;
+  }): Promise<string> {
+    let url: URL;
+    const path = "/v1/realtime/token";
+    const inputBaseUrl =
+      apiBaseUrl ||
+      getEnvVar("INNGEST_BASE_URL") ||
+      getEnvVar("INNGEST_API_BASE_URL");
+
+    const devEnvVar = getEnvVar("INNGEST_DEV");
+
+    if (inputBaseUrl) {
+      url = new URL(path, inputBaseUrl);
+    } else if (devEnvVar) {
+      try {
+        const devUrl = new URL(devEnvVar);
+        url = new URL(path, devUrl);
+      } catch {
+        if (parseAsBoolean(devEnvVar)) {
+          url = new URL(path, "http://localhost:8288/");
+        } else {
+          url = new URL(path, "https://api.inngest.com/");
+        }
+      }
+    } else {
+      url = new URL(
+        path,
+        getEnvVar("NODE_ENV") === "production"
+          ? "https://api.inngest.com/"
+          : "http://localhost:8288/",
+      );
+    }
+
+    const body = topics.map((topic) => ({
+      channel,
+      name: topic,
+      kind: "run",
+    }));
+
+    const res = await fetchWithAuthFallback({
+      authToken: signingKey,
+      authTokenFallback: signingKeyFallback,
+      fetch,
+      url,
+      options: {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(
+        `Failed to get subscription token: ${res.status} ${
+          res.statusText
+        } - ${await res.text()}`,
+      );
+    }
+
+    const data = await res.json();
+    return tokenSchema.parse(data).jwt;
+  },
+};

packages/realtime/src/subscribe/TokenSubscription.ts
@@ -1,17 +1,19 @@
 import debug from "debug";
-import { type Inngest } from "inngest";
-import { devServerAvailable, devServerUrl } from "inngest/helpers/devserver";
+import { api } from "../api";
 import { topic } from "../topic";
 import { Realtime } from "../types";
-import { createDeferredPromise } from "../util";
-import { getSubscriptionToken } from "./helpers";
+import {
+  createDeferredPromise,
+  getPublicEnvVar,
+  parseAsBoolean,
+} from "../util";
 import { StreamFanout } from "./StreamFanout";
 
 /**
  * TODO
  */
 export class TokenSubscription {
-  #app: Inngest.Any;
+  #apiBaseUrl?: string;
   #channelId: string;
   #debug = debug("inngest:realtime");
   #encoder = new TextEncoder();
@@ -19,6 +21,8 @@ export class TokenSubscription {
   #running = false;
   #topics: Map<string, Realtime.Topic.Definition>;
   #ws: WebSocket | null = null;
+  #signingKey: string | undefined;
+  #signingKeyFallback: string | undefined;
 
   /**
    * This is a map that tracks stream IDs to their corresponding streams and
@@ -33,14 +37,14 @@ export class TokenSubscription {
     /**
      * TODO
      */
-    app: Inngest.Like,
-
-    /**
-     * TODO
-     */
     public token: Realtime.Subscribe.Token,
+    apiBaseUrl: string | undefined,
+    signingKey: string | undefined,
+    signingKeyFallback: string | undefined,
   ) {
-    this.#app = app as Inngest.Any;
+    this.#apiBaseUrl = apiBaseUrl;
+    this.#signingKey = signingKey;
+    this.#signingKeyFallback = signingKeyFallback;
 
     if (typeof token.channel === "string") {
       this.#channelId = token.channel;
@@ -68,27 +72,28 @@ export class TokenSubscription {
   private async getWsUrl(token: string): Promise<URL> {
     let url: URL;
     const path = "/v1/realtime/connect";
+    const devEnvVar = getPublicEnvVar("INNGEST_DEV");
 
-    if (this.#app.apiBaseUrl) {
-      url = new URL(path, this.#app.apiBaseUrl);
-    } else {
-      url = new URL(path, "https://api.inngest.com/");
-
-      if (
-        this.#app["mode"].isDev &&
-        this.#app["mode"].isInferred &&
-        !this.#app.apiBaseUrl
-      ) {
-        const dsUrl = devServerUrl().toString();
-        const devAvailable = await devServerAvailable(
-          dsUrl,
-          this.#app["fetch"],
-        );
-
-        if (devAvailable) {
-          url = new URL(path, dsUrl);
+    if (this.#apiBaseUrl) {
+      url = new URL(path, this.#apiBaseUrl);
+    } else if (devEnvVar) {
+      try {
+        const devUrl = new URL(devEnvVar);
+        url = new URL(path, devUrl);
+      } catch {
+        if (parseAsBoolean(devEnvVar)) {
+          url = new URL(path, "http://localhost:8288/");
+        } else {
+          url = new URL(path, "https://api.inngest.com/");
         }
       }
+    } else {
+      url = new URL(
+        path,
+        getPublicEnvVar("NODE_ENV") === "production"
+          ? "https://api.inngest.com/"
+          : "http://localhost:8288/",
+      );
     }
 
     url.protocol = url.protocol === "http:" ? "ws:" : "wss:";
@@ -111,12 +116,31 @@ export class TokenSubscription {
       throw new Error("WebSockets not supported in current environment");
     }
 
-    const key =
-      this.token.key || (await getSubscriptionToken(this.#app, this.token)).key;
+    let key = this.token.key;
     if (!key) {
-      throw new Error(
-        "No subscription token key passed and failed to retrieve one automatically",
+      this.#debug(
+        "No subscription token key passed; attempting to retrieve one automatically...",
       );
+
+      if (!this.#signingKey) {
+        throw new Error(
+          "No subscription token key passed but have no signing key so cannot retrieve one",
+        );
+      }
+
+      key = (
+        await this.lazilyGetSubscriptionToken({
+          ...this.token,
+          signingKey: this.#signingKey,
+          signingKeyFallback: this.#signingKeyFallback,
+        })
+      ).key;
+
+      if (!key) {
+        throw new Error(
+          "No subscription token key passed and failed to retrieve one automatically",
+        );
+      }
     }
 
     const ret = createDeferredPromise<void>();
@@ -387,11 +411,74 @@ export class TokenSubscription {
   /**
    * TODO
    */
+  private async lazilyGetSubscriptionToken<
+    const InputChannel extends Realtime.Channel | string,
+    const InputTopics extends (keyof Realtime.Channel.InferTopics<
+      Realtime.Channel.AsChannel<InputChannel>
+    > &
+      string)[],
+    const TToken extends Realtime.Subscribe.Token<
+      Realtime.Channel.AsChannel<InputChannel>,
+      InputTopics
+    >,
+  >(
+    /**
+     * TODO
+     */
+    args: {
+      /**
+       * TODO
+       */
+      channel: Realtime.Subscribe.InferChannelInput<InputChannel>;
+
+      /**
+       * TODO
+       */
+      topics: InputTopics;
+
+      /**
+       * TODO
+       */
+      signingKey: string;
+
+      /**
+       * TODO
+       */
+      signingKeyFallback?: string | undefined;
+    },
+  ): Promise<TToken> {
+    const channelId =
+      typeof args.channel === "string" ? args.channel : args.channel.name;
+
+    if (!channelId) {
+      throw new Error("Channel ID is required to create a subscription token");
+    }
+
+    const key = await api.getSubscriptionToken({
+      channel: channelId,
+      topics: args.topics,
+      signingKey: args.signingKey,
+      signingKeyFallback: args.signingKeyFallback,
+      apiBaseUrl: this.#apiBaseUrl,
+    });
+
+    const token = {
+      channel: channelId,
+      topics: args.topics,
+      key,
+    } as TToken;
+
+    return token;
+  }
+
+  /**
+   * TODO
+   */
   public close(
     /**
      * TODO
      */
-    reason: string = "Userland closed connection",
+    reason = "Userland closed connection",
   ) {
     if (!this.#running) {
       return;

packages/realtime/src/util.ts
@@ -30,3 +30,167 @@ export const createDeferredPromise = <T>(): DeferredPromiseReturn<T> => {
 
   return { promise, resolve: resolve!, reject: reject! };
 };
+
+/**
+ * Send an HTTP request with the given signing key. If the response is a 401 or
+ * 403, then try again with the fallback signing key
+ */
+export async function fetchWithAuthFallback<TFetch extends typeof fetch>({
+  authToken,
+  authTokenFallback,
+  fetch,
+  options,
+  url,
+}: {
+  authToken: string;
+  authTokenFallback: string | undefined;
+  fetch: TFetch;
+  options?: Parameters<TFetch>[1];
+  url: URL | string;
+}): Promise<Response> {
+  let res = await fetch(url, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+
+  if ([401, 403].includes(res.status) && authTokenFallback) {
+    res = await fetch(url, {
+      ...options,
+      headers: {
+        ...options?.headers,
+        Authorization: `Bearer ${authTokenFallback}`,
+      },
+    });
+  }
+
+  return res;
+}
+
+/**
+ * Given an unknown value, try to parse it as a `boolean`. Useful for parsing
+ * environment variables that could be a selection of different values such as
+ * `"true"`, `"1"`.
+ *
+ * If the value could not be confidently parsed as a `boolean` or was seen to be
+ * `undefined`, this function returns `undefined`.
+ */
+export const parseAsBoolean = (value: unknown): boolean | undefined => {
+  if (typeof value === "boolean") {
+    return value;
+  }
+
+  if (typeof value === "number") {
+    return Boolean(value);
+  }
+
+  if (typeof value === "string") {
+    const trimmed = value.trim().toLowerCase();
+
+    if (trimmed === "undefined") {
+      return undefined;
+    }
+
+    if (["true", "1"].includes(trimmed)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  return undefined;
+};
+
+const publicEnvVarPrefixes = [
+  "", // Also search for the env var itself
+  "PUBLIC_",
+  "NEXT_PUBLIC_",
+  "REACT_APP_",
+  "NUXT_PUBLIC_",
+  "VUE_APP_",
+];
+
+/**
+ * Given a `key`, get the environment variable under that key.
+ */
+export const getEnvVar = (key: string): string | undefined => {
+  return allProcessEnv()[key];
+};
+
+/**
+ * Given a `key`, get the environment variable under that key or a
+ * public-prefixed version of it, such as `NEXT_PUBLIC_${key}`.
+ */
+export const getPublicEnvVar = (key: string): string | undefined => {
+  const env = allProcessEnv();
+
+  for (const prefix of publicEnvVarPrefixes) {
+    const envVar = env[prefix + key];
+
+    if (envVar !== undefined) {
+      return envVar;
+    }
+  }
+};
+
+export type EnvValue = string | undefined;
+export type Env = Record<string, EnvValue>;
+
+/**
+ * The Deno environment, which is not always available.
+ */
+declare const Deno: {
+  env: { toObject: () => Env };
+};
+
+/**
+ * The Netlify environment, which is not always available.
+ */
+declare const Netlify: {
+  env: { toObject: () => Env };
+};
+
+/**
+ * allProcessEnv returns the current process environment variables, or an empty
+ * object if they cannot be read, making sure we support environments other than
+ * Node such as Deno, too.
+ *
+ * Using this ensures we don't dangerously access `process.env` in environments
+ * where it may not be defined, such as Deno or the browser.
+ */
+export const allProcessEnv = (): Env => {
+  // Node, Bun, or Node-like environments
+  try {
+    if (process.env) {
+      return process.env;
+    }
+  } catch (_err) {
+    // noop
+  }
+
+  // Deno
+  try {
+    const env = Deno.env.toObject();
+
+    if (env) {
+      return env;
+    }
+  } catch (_err) {
+    // noop
+  }
+
+  // Netlify
+  try {
+    const env = Netlify.env.toObject();
+
+    if (env) {
+      return env;
+    }
+  } catch (_err) {
+    // noop
+  }
+
+  return {};
+};

packages/realtime/src/hooks.ts
@@ -1,4 +1,3 @@
-import { Inngest } from "inngest";
 import { useEffect, useRef, useState } from "react";
 import { subscribe } from "./subscribe";
 import { type Realtime } from "./types";
@@ -48,18 +47,12 @@ export interface InngestSubscription<TToken extends Realtime.Subscribe.Token> {
 export function useInngestSubscription<
   const TToken extends Realtime.Subscribe.Token | null | undefined,
 >({
-  app = new Inngest({ id: "useInngestSubscription" }),
   token: tokenInput,
   refreshToken,
   key,
   enabled = true,
   bufferInterval = 0,
 }: {
-  /**
-   * TODO
-   */
-  app?: Inngest.Any;
-
   /**
    * TODO
    */
@@ -131,7 +124,7 @@ export function useInngestSubscription<
     const start = async () => {
       try {
         setState(InngestSubscriptionState.Connecting);
-        const stream = await subscribe(app, { ...token });
+        const stream = await subscribe({ ...token });
         if (cancelled) return;
 
         subscriptionRef.current = stream;

packages/realtime/src/index.test.ts
@@ -342,7 +342,6 @@ describe("subscribe", () => {
       test("can subscribe with just strings", () => {
         const _fn = async () => {
           const stream = await subscribe(
-            app,
             {
               channel: "test",
               topics: ["foo", "bar"],
@@ -389,7 +388,7 @@ describe("subscribe", () => {
     describe("type-only channel import", () => {
       test("errors if channel name is incorrect", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             // @ts-expect-error Incorrect channel
             channel: typeOnlyChannel<typeof userChannel>("test"),
             topics: ["created", "updated"],
@@ -399,7 +398,7 @@ describe("subscribe", () => {
 
       test("errors if topic names are incorrect with static channel", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             channel: typeOnlyChannel<typeof staticChannel>("static"),
             // @ts-expect-error Incorrect topic
             topics: ["created", "updated", "test"],
@@ -409,7 +408,7 @@ describe("subscribe", () => {
 
       test("errors if topic names are incorrect with dynamic channel", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             channel: typeOnlyChannel<typeof userChannel>("user/123"),
             // @ts-expect-error Incorrect topic
             topics: ["created", "updated", "test"],
@@ -420,7 +419,6 @@ describe("subscribe", () => {
       test("can subscribe using types only of a static channel", () => {
         const _fn = async () => {
           const stream = await subscribe(
-            app,
             {
               channel: typeOnlyChannel<typeof staticChannel>("static"),
               topics: ["created", "updated"],
@@ -466,7 +464,6 @@ describe("subscribe", () => {
       test("can subscribe using types only of a dynamic channel", () => {
         const _fn = async () => {
           const stream = await subscribe(
-            app,
             {
               channel: typeOnlyChannel<typeof userChannel>("user/123"),
               topics: ["created", "updated"],
@@ -513,7 +510,7 @@ describe("subscribe", () => {
     describe("runtime channel import", () => {
       test("errors if static definition given", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             // @ts-expect-error Definition given
             channel: staticChannel,
             topics: ["created", "updated"],
@@ -523,7 +520,7 @@ describe("subscribe", () => {
 
       test("errors if dynamic definition given", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             // @ts-expect-error Definition given
             channel: userChannel,
             topics: ["created", "updated"],
@@ -533,7 +530,7 @@ describe("subscribe", () => {
 
       test("errors if topic names are incorrect with static channel", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             channel: staticChannel(),
             // @ts-expect-error Incorrect topic
             topics: ["created", "updated", "test"],
@@ -543,7 +540,7 @@ describe("subscribe", () => {
 
       test("errors if topic names are incorrect with dynamic channel", () => {
         const _fn = () => {
-          void subscribe(app, {
+          void subscribe({
             channel: userChannel("123"),
             // @ts-expect-error Incorrect topic
             topics: ["created", "updated", "test"],
@@ -553,7 +550,7 @@ describe("subscribe", () => {
 
       test("can subscribe with runtime import of a static channel", () => {
         const _fn = async () => {
-          const stream = await subscribe(app, {
+          const stream = await subscribe({
             channel: staticChannel(),
             topics: ["created", "updated"],
           });
@@ -586,7 +583,7 @@ describe("subscribe", () => {
 
       test("can subscribe with runtime import of a dynamic channel", () => {
         const _fn = async () => {
-          const stream = await subscribe(app, {
+          const stream = await subscribe({
             channel: userChannel("123"),
             topics: ["created", "updated"],
           });
@@ -626,7 +623,7 @@ describe("subscribe", () => {
             topics: ["foo", "bar"],
           });
 
-          const stream = await subscribe(app, token, (message) => {
+          const stream = await subscribe(token, (message) => {
             assertType<"test">(message.channel);
             assertType<"foo" | "bar">(message.topic);
 
@@ -670,7 +667,7 @@ describe("subscribe", () => {
             topics: ["created", "updated"],
           });
 
-          const stream = await subscribe(app, token, (message) => {
+          const stream = await subscribe(token, (message) => {
             assertType<"static">(message.channel);
             assertType<"created" | "updated">(message.topic);
 
@@ -714,7 +711,7 @@ describe("subscribe", () => {
             topics: ["created", "updated"],
           });
 
-          const stream = await subscribe(app, token, (message) => {
+          const stream = await subscribe(token, (message) => {
             assertType<"static">(message.channel);
             assertType<"created" | "updated">(message.topic);
 
@@ -758,7 +755,7 @@ describe("subscribe", () => {
             topics: ["created", "updated"],
           });
 
-          const stream = await subscribe(app, token, (message) => {
+          const stream = await subscribe(token, (message) => {
             assertType<`user/${string}`>(message.channel);
             assertType<"created" | "updated">(message.topic);
 
@@ -802,7 +799,7 @@ describe("subscribe", () => {
             topics: ["created", "updated"],
           });
 
-          const stream = await subscribe(app, token, (message) => {
+          const stream = await subscribe(token, (message) => {
             assertType<`user/${string}`>(message.channel);
             assertType<"created" | "updated">(message.topic);
 

packages/realtime/src/subscribe/helpers.ts
@@ -1,5 +1,7 @@
-import { Inngest } from "inngest";
-import { Realtime } from "../types";
+import type { Inngest } from "inngest";
+import type { InngestApi } from "inngest/api/api";
+import type { Realtime } from "../types";
+import { getEnvVar, getPublicEnvVar } from "../util";
 import { TokenSubscription } from "./TokenSubscription";
 
 /**
@@ -20,12 +22,12 @@ export const subscribe = async <
   /**
    * TODO
    */
-  app: Inngest.Like,
-
-  /**
-   * TODO
-   */
   token: {
+    /**
+     * TODO
+     */
+    app?: Inngest.Like;
+
     /**
      * TODO
      */
@@ -42,9 +44,27 @@ export const subscribe = async <
    */
   callback?: Realtime.Subscribe.Callback<TToken>,
 ): Promise<TOutput> => {
+  const app: Inngest.Any | undefined = token.app as Inngest.Any | undefined;
+  const api: InngestApi | undefined = app?.["inngestApi"];
+
+  // Allow users to specify public env vars for the target URLs, but do not
+  // allow this for signing keys, as they should never be on a client.
+  const maybeApiBaseUrl =
+    app?.apiBaseUrl ||
+    getPublicEnvVar("INNGEST_BASE_URL") ||
+    getPublicEnvVar("INNGEST_API_BASE_URL");
+
+  const maybeSigningKey =
+    api?.["signingKey"] || getEnvVar("INNGEST_SIGNING_KEY");
+
+  const maybeSigningKeyFallback =
+    api?.["signingKeyFallback"] || getEnvVar("INNGEST_SIGNING_KEY_FALLBACK");
+
   const subscription = new TokenSubscription(
-    app,
     token as Realtime.Subscribe.Token,
+    maybeApiBaseUrl,
+    maybeSigningKey,
+    maybeSigningKeyFallback,
   );
 
   const retStream = subscription.getJsonStream();

commit fc966ca94f699d6534f2fc5c85bbcf5be3c6795a

Fix requiring signing key for dev in `@inngest/realtime` (#960)
## Summary
<!-- Succinctly describe your change, providing context, what you've
changed, and why. -->

Addresses #958 by removing the need for a signing key in a dev env for
`@inngest/realtime`; this was an overzealous check that is harder to
confidently make without the presence of an app.

## Checklist
<!-- Tick these items off as you progress. -->
<!-- If an item isn't applicable, ideally please strikeout the item by
wrapping it in "~~"" and suffix it with "N/A My reason for skipping
this." -->
<!-- e.g. "- [ ] ~~Added tests~~ N/A Only touches docs" -->

- [ ] ~Added a [docs PR](https://github.com/inngest/website) that
references this PR~ N/A Fix
- [ ] Added unit/integration tests
- [x] Added changesets if applicable

## Related
<!-- A space for any related links, issues, or PRs. -->
<!-- Linear issues are autolinked. -->
<!-- e.g. - INN-123 -->
<!-- GitHub issues/PRs can be linked using shorthand. -->
<!-- e.g. "- inngest/inngest#123" -->
<!-- Feel free to remove this section if there are no applicable related
links.-->
- Caused by #949 
- Fixes #958

packages/realtime/src/api.ts
@@ -13,7 +13,7 @@ export const api = {
   }: {
     channel: string;
     topics: string[];
-    signingKey: string;
+    signingKey: string | undefined;
     signingKeyFallback: string | undefined;
     apiBaseUrl: string | undefined;
   }): Promise<string> {

packages/realtime/src/subscribe/TokenSubscription.ts
@@ -109,7 +109,7 @@ export class TokenSubscription {
     this.#debug(
       `Establishing connection to channel "${
         this.#channelId
-      }" with topics ${JSON.stringify(this.#topics)}...`,
+      }" with topics ${JSON.stringify([...this.#topics.keys()])}...`,
     );
 
     if (typeof WebSocket === "undefined") {
@@ -121,12 +121,6 @@ export class TokenSubscription {
       this.#debug(
         "No subscription token key passed; attempting to retrieve one automatically...",
       );
-
-      if (!this.#signingKey) {
-        throw new Error(
-          "No subscription token key passed but have no signing key so cannot retrieve one",
-        );
-      }
 
       key = (
         await this.lazilyGetSubscriptionToken({
@@ -439,12 +433,12 @@ export class TokenSubscription {
       /**
        * TODO
        */
-      signingKey: string;
+      signingKey: string | undefined;
 
       /**
        * TODO
        */
-      signingKeyFallback?: string | undefined;
+      signingKeyFallback: string | undefined;
     },
   ): Promise<TToken> {
     const channelId =

packages/realtime/src/util.ts
@@ -42,7 +42,7 @@ export async function fetchWithAuthFallback<TFetch extends typeof fetch>({
   options,
   url,
 }: {
-  authToken: string;
+  authToken: string | undefined;
   authTokenFallback: string | undefined;
   fetch: TFetch;
   options?: Parameters<TFetch>[1];
@@ -52,7 +52,7 @@ export async function fetchWithAuthFallback<TFetch extends typeof fetch>({
     ...options,
     headers: {
       ...options?.headers,
-      Authorization: `Bearer ${authToken}`,
+      ...(authToken ? { Authorization: `Bearer ${authToken}` } : {}),
     },
   });
 