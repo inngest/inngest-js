c021cab558b70cb4559220ea414aa37982b8ffca
Charly Poly <1252066+charlypoly@users.noreply.github.com>
2025-05-09 16:34:31 +0200

    feat(examples): `step.fetch()` (#977)
    Linked to https://github.com/inngest/website/pull/1114

---
+++ examples/node-step-fetch/src/inngest/functions.ts
@@ -0,0 +1,17 @@
+import { inngest } from "./client";
+
+export const retrieveTextFile = inngest.createFunction(
+  { id: "retrieveTextFile" },
+  { event: "textFile/retrieve" },
+  async ({ step }) => {
+    const response = await step.fetch(
+      "https://example-files.online-convert.com/document/txt/example.txt"
+    );
+
+    await step.run("extract-text", async () => {
+      const text = await response.text();
+      const exampleOccurences = text.match(/example/g);
+      return exampleOccurences?.length;
+    });
+  }
+);
