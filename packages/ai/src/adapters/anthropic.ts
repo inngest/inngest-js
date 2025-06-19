0bd8061a789f822953105bbf39e58ae44a966ecd
Ted Werbel <werbel.ted@gmail.com>
2025-06-19 10:51:05 -0400

    added latest anthropic models

--- packages/ai/src/adapters/anthropic.ts
+++ packages/ai/src/adapters/anthropic.ts
@@ -25,6 +25,12 @@ export namespace AnthropicAiAdapter {
   export type Model =
     // eslint-disable-next-line @typescript-eslint/ban-types
     | (string & {})
+    | "claude-opus-4-0"
+    | "claude-opus-4-20250514"
+    | "claude-sonnet-4-0"
+    | "claude-sonnet-4-20250514"
+    | "claude-3-7-sonnet-latest"
+    | "claude-3-7-sonnet-20250219"
     | "claude-3-5-haiku-latest"
     | "claude-3-5-haiku-20241022"
     | "claude-3-5-sonnet-latest"
