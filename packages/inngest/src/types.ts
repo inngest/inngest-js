b44e966b235ac0a82326d0b9f9a3c57c966fe48d
Lakshmi Kasinathan <lakshmi@inngest.com>
2025-08-13 13:42:30 -0700

    Support conditional batching

--- packages/inngest/src/types.ts
+++ packages/inngest/src/types.ts
@@ -1192,6 +1192,7 @@ export const functionConfigSchema = z.strictObject({
       maxSize: z.number(),
       timeout: z.string(),
       key: z.string().optional(),
+      if: z.string().optional(),
     })
     .optional(),
   rateLimit: z
