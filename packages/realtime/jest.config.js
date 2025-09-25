15a1bd4baf8ddec8b1f7ef8bc22fca7e2e44c4d8
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-09-25 11:56:09 +0000

    Replace jest with vitest

--- packages/realtime/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
-  },
-  roots: ["<rootDir>/src"],
-};
