e1a741f68cc2ddb175048b9007fdc9d9cfcf23b1
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-09-26 10:01:05 +0000

    Run CI without `--no-package-lock` in case we actually needed one
    This was breaking TanStack. Ideally the
    examples don't have one, but sometimes
    it's needed!

--- packages/inngest/scripts/integrationTestRunner.ts
+++ packages/inngest/scripts/integrationTestRunner.ts
@@ -66,7 +66,7 @@ async function setupExample(examplePath: string): Promise<void> {
     );
   } else {
     await execAsync(
-      "npm install --no-save --legacy-peer-deps --no-package-lock ../../packages/inngest/inngest.tgz",
+      "npm install --no-save --legacy-peer-deps ../../packages/inngest/inngest.tgz",
       {
         cwd: examplePath,
       },
