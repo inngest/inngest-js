e76e1371dc58e91efbd75084eee5318b9d936478
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-10-03 20:15:22 +0000

    Add `.fromSchema()` tests

--- packages/middleware-validation/src/middleware.test.ts
+++ packages/middleware-validation/src/middleware.test.ts
@@ -58,7 +58,7 @@ describe("validationMiddleware", () => {
     expect(result).toEqual("success");
   });
 
-  test("should validate a correct event with a Zod schema", async () => {
+  test("should validate a correct event with a Zod schema (fromZod)", async () => {
     const inngest = new Inngest({
       id: "test",
       schemas: new EventSchemas().fromZod({
@@ -86,7 +86,33 @@ describe("validationMiddleware", () => {
     expect(result).toEqual("success");
   });
 
-  test("should not allow an event through with an incorrect Zod schema", async () => {
+  test("should validate a correct event with a Zod schema (fromSchema)", async () => {
+    const inngest = new Inngest({
+      id: "test",
+      schemas: new EventSchemas().fromSchema({
+        test: z.object({
+          message: z.string(),
+        }),
+      }),
+      middleware: [validationMiddleware()],
+    });
+
+    const t = new InngestTestEngine({
+      function: inngest.createFunction(
+        { id: "test" },
+        { event: "test" },
+        () => "success",
+      ),
+      events: [{ name: "test", data: { message: "hello" } }],
+    });
+
+    const { result, error } = await t.execute();
+
+    expect(error).toBeUndefined();
+    expect(result).toEqual("success");
+  });
+
+  test("should not allow an event through with an incorrect Zod schema (fromZod)", async () => {
     const inngest = new Inngest({
       id: "test",
       schemas: new EventSchemas().fromZod({
@@ -114,8 +140,34 @@ describe("validationMiddleware", () => {
     expect(result).toBeUndefined();
   });
 
+  test("should not allow an event through with an incorrect Zod schema (fromSchema)", async () => {
+    const inngest = new Inngest({
+      id: "test",
+      schemas: new EventSchemas().fromSchema({
+        test: z.object({
+          message: z.string(),
+        }),
+      }),
+      middleware: [validationMiddleware()],
+    });
+
+    const t = new InngestTestEngine({
+      function: inngest.createFunction(
+        { id: "test" },
+        { event: "test" },
+        () => "success",
+      ),
+      events: [{ name: "test", data: { message: 123 } }],
+    });
+
+    const { result, error } = await t.execute();
+
+    expect(JSON.stringify(error)).toContain("failed validation");
+    expect(result).toBeUndefined();
+  });
+
   describe("inngest/function.invoked", () => {
-    test("should test against multiple schemas for `inngest/function.invoked`", async () => {
+    test("should test against multiple schemas for `inngest/function.invoked` (fromZod)", async () => {
       const inngest = new Inngest({
         id: "test",
         schemas: new EventSchemas().fromZod({
@@ -129,6 +181,35 @@ describe("validationMiddleware", () => {
               b: z.boolean(),
             }),
           },
+        }),
+        middleware: [validationMiddleware()],
+      });
+
+      const t = new InngestTestEngine({
+        function: inngest.createFunction(
+          { id: "test" },
+          { event: "b" },
+          () => "success",
+        ),
+        events: [{ name: "inngest/function.invoked", data: { b: true } }],
+      });
+
+      const { result, error } = await t.execute();
+
+      expect(error).toBeUndefined();
+      expect(result).toEqual("success");
+    });
+
+    test("should test against multiple schemas for `inngest/function.invoked` (fromSchema)", async () => {
+      const inngest = new Inngest({
+        id: "test",
+        schemas: new EventSchemas().fromSchema({
+          a: z.object({
+            a: z.boolean(),
+          }),
+          b: z.object({
+            b: z.boolean(),
+          }),
         }),
         middleware: [validationMiddleware()],
       });
@@ -199,7 +280,7 @@ describe("validationMiddleware", () => {
       expect(result).toBeUndefined();
     });
 
-    test("should succeed if an event has a schema", async () => {
+    test("should succeed if an event has a schema (fromZod)", async () => {
       const inngest = new Inngest({
         id: "test",
         schemas: new EventSchemas().fromZod({
@@ -227,7 +308,33 @@ describe("validationMiddleware", () => {
       expect(result).toEqual("success");
     });
 
-    test("should succeed if an `inngest/function.invoked` event has a schema", async () => {
+    test("should succeed if an event has a schema (fromSchema)", async () => {
+      const inngest = new Inngest({
+        id: "test",
+        schemas: new EventSchemas().fromSchema({
+          test: z.object({
+            message: z.string(),
+          }),
+        }),
+        middleware: [validationMiddleware({ disallowSchemalessEvents: true })],
+      });
+
+      const t = new InngestTestEngine({
+        function: inngest.createFunction(
+          { id: "test" },
+          { event: "test" },
+          () => "success",
+        ),
+        events: [{ name: "test", data: { message: "hello" } }],
+      });
+
+      const { result, error } = await t.execute();
+
+      expect(error).toBeUndefined();
+      expect(result).toEqual("success");
+    });
+
+    test("should succeed if an `inngest/function.invoked` event has a schema (fromZod)", async () => {
       const inngest = new Inngest({
         id: "test",
         schemas: new EventSchemas().fromZod({
@@ -256,9 +363,37 @@ describe("validationMiddleware", () => {
       expect(error).toBeUndefined();
       expect(result).toEqual("success");
     });
+
+    test("should succeed if an `inngest/function.invoked` event has a schema (fromSchema)", async () => {
+      const inngest = new Inngest({
+        id: "test",
+        schemas: new EventSchemas().fromSchema({
+          test: z.object({
+            message: z.string(),
+          }),
+        }),
+        middleware: [validationMiddleware({ disallowSchemalessEvents: true })],
+      });
+
+      const t = new InngestTestEngine({
+        function: inngest.createFunction(
+          { id: "test" },
+          { event: "test" },
+          () => "success",
+        ),
+        events: [
+          { name: "inngest/function.invoked", data: { message: "hello" } },
+        ],
+      });
+
+      const { result, error } = await t.execute();
+
+      expect(error).toBeUndefined();
+      expect(result).toEqual("success");
+    });
   });
 
-  test("handles a literal Zod schema", async () => {
+  test("handles a literal Zod schema (fromZod)", async () => {
     const inngest = new Inngest({
       id: "test",
       schemas: new EventSchemas().fromZod([
@@ -287,7 +422,7 @@ describe("validationMiddleware", () => {
     expect(result).toEqual("success");
   });
 
-  test("handles a nested Zod schema", async () => {
+  test("handles a nested Zod schema (fromZod)", async () => {
     const inngest = new Inngest({
       id: "test",
       schemas: new EventSchemas().fromZod({
@@ -317,7 +452,35 @@ describe("validationMiddleware", () => {
     expect(result).toEqual("success");
   });
 
-  test("validates all events in a batch", async () => {
+  test("handles a Zod schema (fromSchema)", async () => {
+    const inngest = new Inngest({
+      id: "test",
+      schemas: new EventSchemas().fromSchema({
+        test: z.object({
+          message: z.object({
+            content: z.string(),
+          }),
+        }),
+      }),
+      middleware: [validationMiddleware()],
+    });
+
+    const t = new InngestTestEngine({
+      function: inngest.createFunction(
+        { id: "test" },
+        { event: "test" },
+        () => "success",
+      ),
+      events: [{ name: "test", data: { message: { content: "hello" } } }],
+    });
+
+    const { result, error } = await t.execute();
+
+    expect(error).toBeUndefined();
+    expect(result).toEqual("success");
+  });
+
+  test("validates all events in a batch (fromZod)", async () => {
     const inngest = new Inngest({
       id: "test",
       schemas: new EventSchemas().fromZod({
@@ -348,13 +511,42 @@ describe("validationMiddleware", () => {
     expect(result).toBeUndefined();
   });
 
+  test("validates all events in a batch (fromSchema)", async () => {
+    const inngest = new Inngest({
+      id: "test",
+      schemas: new EventSchemas().fromSchema({
+        test: z.object({
+          message: z.string(),
+        }),
+      }),
+      middleware: [validationMiddleware()],
+    });
+
+    const t = new InngestTestEngine({
+      function: inngest.createFunction(
+        { id: "test" },
+        { event: "test" },
+        () => "success",
+      ),
+      events: [
+        { name: "test", data: { message: "hello" } },
+        { name: "test", data: { message: 123 } },
+      ],
+    });
+
+    const { result, error } = await t.execute();
+
+    expect(JSON.stringify(error)).toContain("failed validation");
+    expect(result).toBeUndefined();
+  });
+
   describe("onSendEvent", () => {
     describe("inngest.send()", () => {
       afterEach(() => {
         fetchMock.mockReset();
       });
 
-      test("should validate an event before sending it", async () => {
+      test("should validate an event before sending it (fromZod)", async () => {
         const inngest = new Inngest({
           id: "test",
           schemas: new EventSchemas().fromZod({
@@ -386,7 +578,37 @@ describe("validationMiddleware", () => {
         expect(result).toBeUndefined();
       });
 
-      test("should not validate an event before sending it if disabled", async () => {
+      test("should validate an event before sending it (fromSchema)", async () => {
+        const inngest = new Inngest({
+          id: "test",
+          schemas: new EventSchemas().fromSchema({
+            test: z.object({
+              message: z.string(),
+            }),
+          }),
+          middleware: [validationMiddleware()],
+        });
+
+        const t = new InngestTestEngine({
+          function: inngest.createFunction(
+            { id: "test" },
+            { event: "test" },
+            () =>
+              inngest.send({
+                name: "test",
+                data: { message: 123 as unknown as string },
+              }),
+          ),
+          events: [{ name: "test", data: { message: "hello" } }],
+        });
+
+        const { result, error } = await t.execute();
+
+        expect(JSON.stringify(error)).toContain("failed validation");
+        expect(result).toBeUndefined();
+      });
+
+      test("should not validate an event before sending it if disabled (fromZod)", async () => {
         fetchMock.postOnce(`${baseUrl}/e/${eventKey}`, {
           status: 200,
           ids: ["123"],
@@ -416,6 +638,35 @@ describe("validationMiddleware", () => {
           }),
         ).resolves.not.toThrow();
       });
+
+      test("should not validate an event before sending it if disabled (fromSchema)", async () => {
+        fetchMock.postOnce(`${baseUrl}/e/${eventKey}`, {
+          status: 200,
+          ids: ["123"],
+        });
+
+        const inngest = new Inngest({
+          id: "test",
+          fetch: fetchMock as typeof fetch,
+          baseUrl,
+          eventKey,
+          schemas: new EventSchemas().fromSchema({
+            test: z.object({
+              message: z.string(),
+            }),
+          }),
+          middleware: [
+            validationMiddleware({ disableOutgoingValidation: true }),
+          ],
+        });
+
+        await expect(
+          inngest.send({
+            name: "test",
+            data: { message: 123 as unknown as string },
+          }),
+        ).resolves.not.toThrow();
+      });
     });
 
     describe("step.sendEvent()", () => {
@@ -423,7 +674,7 @@ describe("validationMiddleware", () => {
         fetchMock.mockReset();
       });
 
-      test("should validate an event before sending it", async () => {
+      test("should validate an event before sending it (fromZod)", async () => {
         const inngest = new Inngest({
           id: "test",
           schemas: new EventSchemas().fromZod({
@@ -459,7 +710,41 @@ describe("validationMiddleware", () => {
         expect(result).toBeUndefined();
       });
 
-      test("should not validate an event before sending it if disabled", async () => {
+      test("should validate an event before sending it (fromSchema)", async () => {
+        const inngest = new Inngest({
+          id: "test",
+          schemas: new EventSchemas().fromSchema({
+            test: z.object({
+              message: z.string(),
+            }),
+          }),
+          middleware: [validationMiddleware()],
+          logger: { error: () => undefined } as Logger,
+        });
+
+        const fn = inngest.createFunction(
+          { id: "test" },
+          { event: "test" },
+          async ({ step }) => {
+            await step.sendEvent("id", {
+              name: "test",
+              data: { message: 123 as unknown as string },
+            });
+          },
+        );
+
+        const t = new InngestTestEngine({
+          function: fn,
+          events: [{ name: "test", data: { message: "hello" } }],
+        });
+
+        const { result, error } = await t.execute();
+
+        expect(JSON.stringify(error)).toContain("failed validation");
+        expect(result).toBeUndefined();
+      });
+
+      test("should not validate an event before sending it if disabled (fromZod)", async () => {
         fetchMock.post(`${baseUrl}/e/${eventKey}`, {
           status: 200,
           ids: ["123"],
@@ -476,6 +761,46 @@ describe("validationMiddleware", () => {
                 message: z.string(),
               }),
             },
+          }),
+          middleware: [
+            validationMiddleware({ disableOutgoingValidation: true }),
+          ],
+        });
+
+        const fn = inngest.createFunction(
+          { id: "test" },
+          { event: "test" },
+          async ({ step }) => {
+            await step.sendEvent("id", {
+              name: "test",
+              data: { message: 123 as unknown as string },
+            });
+          },
+        );
+
+        const t = new InngestTestEngine({
+          function: fn,
+          events: [{ name: "test", data: { message: "hello" } }],
+        });
+
+        await expect(t.execute()).resolves.not.toThrow();
+      });
+
+      test("should not validate an event before sending it if disabled (fromSchema)", async () => {
+        fetchMock.post(`${baseUrl}/e/${eventKey}`, {
+          status: 200,
+          ids: ["123"],
+        });
+
+        const inngest = new Inngest({
+          id: "test",
+          fetch: fetchMock as typeof fetch,
+          baseUrl,
+          eventKey,
+          schemas: new EventSchemas().fromSchema({
+            test: z.object({
+              message: z.string(),
+            }),
           }),
           middleware: [
             validationMiddleware({ disableOutgoingValidation: true }),
