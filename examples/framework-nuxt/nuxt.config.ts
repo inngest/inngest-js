37f75e9a4121f8cc8834daffa6376528a1b85d4a
Aaron Harper <aaron@inngest.com>
2025-12-28 09:14:57 -0500

    Update other packages

--- examples/framework-nuxt/nuxt.config.ts
+++ examples/framework-nuxt/nuxt.config.ts
@@ -1,4 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  // Delete these aliases when copying this example to a new project
+  alias: {
+    inngest: "../../packages/inngest/src/index.ts",
+    "inngest/nuxt": "../../packages/inngest/src/nuxt.ts",
+  },
 })
