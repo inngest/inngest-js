304c5497235a021cc2673dc48e2e7771c2332860
Dan Farrelly <daniel.j.farrelly@gmail.com>
2026-02-05 09:47:44 -0500

    Re-fetch token when enabled prop is changed.
    This can happen if someone is conditionally enabling the hook in the calling component, ex. via feature flip which is loaded through a hook itself. This hook only previously happened on mount, but would not re-run if the hook was enabled, leaving the entire hook to never re-initialize and fetch a token if the state is changed.

--- packages/realtime/src/hooks.ts
+++ packages/realtime/src/hooks.ts
@@ -115,7 +115,7 @@ export function useInngestSubscription<
         setState(InngestSubscriptionState.Error);
       }
     }
-  }, []);
+  }, [enabled]);
 
   // Subscription management
   useEffect(() => {
