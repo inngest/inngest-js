6d7390009459361551134755332760b141150f5c
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2026-02-05 17:46:42 +0000

    Get force step plan flag from headers

--- packages/inngest/src/components/InngestCommHandler.ts
+++ packages/inngest/src/components/InngestCommHandler.ts
@@ -1483,6 +1483,25 @@ export class InngestCommHandler<
               ? { fn: fns[0], onFailure: false }
               : Object.values(this.fns)[0];
           fnId = fn?.fn.id();
+
+          // Grab "force step plan" flag from headers
+          let die = false;
+          const dieHeader = await actions.headers(
+            "getting step plan force control for forced execution",
+            headerKeys.InngestForceStepPlan,
+          );
+          if (dieHeader) {
+            const parsed = parseAsBoolean(dieHeader);
+            if (typeof parsed === "boolean") {
+              die = parsed;
+            } else {
+              this.log(
+                "warn",
+                `Received invalid value for ${headerKeys.InngestForceStepPlan} header: ${dieHeader}. Expected a boolean value. Defaulting to "false".`,
+              );
+            }
+          }
+
           body = {
             event: {},
             events: [],
@@ -1491,7 +1510,7 @@ export class InngestCommHandler<
             sdkDecided: true,
             ctx: {
               attempt: 0,
-              disable_immediate_execution: false,
+              disable_immediate_execution: die,
               use_api: true,
               max_attempts: 3,
               run_id: await actions.headers(
