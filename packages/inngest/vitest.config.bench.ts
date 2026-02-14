17739ce40128d22eacc49f23ae9586b67476b091
Aaron Harper <aaron@inngest.com>
2026-02-14 17:55:38 -0500

    Add codspeed

--- packages/inngest/vitest.config.bench.ts
+++ packages/inngest/vitest.config.bench.ts
@@ -1,8 +1,10 @@
+import codspeedPlugin from "@codspeed/vitest-plugin";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
+    codspeedPlugin(),
     tsConfigPaths({
       projects: ["./"],
     }),
