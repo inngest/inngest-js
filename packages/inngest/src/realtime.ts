5050983acbcbeed16917973848d70565ca990bd3
Jacob Heric <jacob@jacobheric.com>
2026-03-22 07:26:16 -0400

    sort imports

--- packages/inngest/src/realtime.ts
+++ packages/inngest/src/realtime.ts
@@ -4,5 +4,5 @@ export {
   getSubscriptionToken,
   subscribe,
 } from "./components/realtime/subscribe/index.ts";
-export { staticSchema } from "./components/triggers/triggers.ts";
 export type { Realtime } from "./components/realtime/types.ts";
+export { staticSchema } from "./components/triggers/triggers.ts";
