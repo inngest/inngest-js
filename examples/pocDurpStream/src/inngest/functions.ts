0ba40af1472a2f097829a0ff5f0ac5b021759d6c
Linell Bonnette <tlbonnette@gmail.com>
2026-03-24 11:22:14 -0700

    feat: automatic redirect following

--- examples/pocDurpStream/src/inngest/functions.ts
+++ examples/pocDurpStream/src/inngest/functions.ts
@@ -1,13 +1,15 @@
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "demo/event.sent" },
+  {
+    id: "hello-world",
+    triggers: { event: "demo/event.sent" },
+  },
   async ({ event, step }) => {
     const greeting = await step.run("greet", () => {
       return `Hello ${event.name}!`;
     });
 
     return { greeting };
-  }
+  },
 );
