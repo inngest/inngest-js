e32403cdf8d40e2b690717ff2d020301f0d200d9
Aaron Harper <aaron@inngest.com>
2026-04-10 11:38:33 -0400

    Add default checkpointing maxRuntime

--- packages/inngest/src/components/InngestFunction.ts
+++ packages/inngest/src/components/InngestFunction.ts
@@ -5,6 +5,7 @@ import {
   type Cancellation,
   type CheckpointingOptions,
   type ConcurrencyOption,
+  type DefaultMaxRuntime,
   defaultCheckpointingOptions,
   type FunctionConfig,
   type Handler,
@@ -293,6 +294,7 @@ export class InngestFunction<
     requestedRunStep: string | undefined,
     internalFnId: string | undefined,
     disableImmediateExecution: boolean,
+    defaultMaxRuntime: DefaultMaxRuntime,
   ): InternalCheckpointingOptions | undefined {
     if (requestedRunStep || !internalFnId || disableImmediateExecution) {
       return;
@@ -312,13 +314,16 @@ export class InngestFunction<
     }
 
     if (userCfg === true) {
-      return defaultCheckpointingOptions;
+      return {
+        ...defaultCheckpointingOptions,
+        maxRuntime: defaultMaxRuntime,
+      };
     }
 
     return {
       bufferedSteps:
         userCfg.bufferedSteps ?? defaultCheckpointingOptions.bufferedSteps,
-      maxRuntime: userCfg.maxRuntime ?? defaultCheckpointingOptions.maxRuntime,
+      maxRuntime: userCfg.maxRuntime ?? defaultMaxRuntime,
       maxInterval:
         userCfg.maxInterval ?? defaultCheckpointingOptions.maxInterval,
     };
