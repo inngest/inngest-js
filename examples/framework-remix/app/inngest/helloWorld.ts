a1318ec3623dafcdbc34e9600661914a624f05dd
Dan Farrelly <daniel.j.farrelly@gmail.com>
2026-05-19 12:48:05 -0400

    Upgrade Remix to React Router 7

--- examples/framework-remix/app/inngest/helloWorld.ts
+++ examples/framework-remix/app/inngest/helloWorld.ts
@@ -1,10 +1,11 @@
 import { inngest } from "./client";
 
 export default inngest.createFunction(
-  { id: "hello-world", triggers: [{ event: "demo/event.sent" }] },
+  { id: "hello-world" },
+  { event: "demo/event.sent" },
   async ({ event, step }) => {
     return {
       message: `Hello ${event.name}!`,
     };
-  }
+  },
 );
